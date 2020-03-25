5572996ef6a0d80de60d82ce035a5d110a2b86cf
Youn <younhun@mit.edu>
2020-03-25 15:23:24 -0400

    added example parameters to read_simulate.py

--- read_simulate.py
+++ read_simulate.py
@@ -2,6 +2,7 @@
   read_simulate.py
   Run to simulate reads from genomes specified by accession numbers.
 """
+import os
 import argparse
 from scripts.fetch_genomes import fetch_sequences
 from util.logger import logger
@@ -9,6 +10,14 @@ from util.logger import logger
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Simulate reads from genomes.")
+    parser.add_argument('-s', '--seed', required=False, type=int, default=31415,
+                        help='<Optional> Seed for randomness (for reproducibility).')
+    parser.add_argument('-o', '--out_dir', required=True,
+                        help='<Required> Directory to save the reads.')
+    parser.add_argument('-p', '--out_prefix', required=False, default='sampled_read',
+                        help='<Optional> File prefix for the read files.')
+    parser.add_argument('-e', '--extension', required=False, default='txt',
+                        help='<Optional> File extension.')
 
     # =============
     # TODO: add arguments to parser.
@@ -16,12 +25,19 @@ def parse_args():
     return parser.parse_args()
 
 
-def save_to_fastq(sampled_reads):
-    # TODO
-    raise NotImplementedError("TODO implement!")
+def save_to_fastq(sampled_reads, out_dir, out_prefix, extension):
+    for t in range(len(sampled_reads)):
+        filename = '{}_{}.{}'.format(out_prefix, t, extension)
+        out_path = os.path.join(out_dir, filename)
+        save_timeslice_to_fastq(sampled_reads[t], out_path)
 
 
-def sample_reads(args):
+def save_timeslice_to_fastq(reads, out_path):
+    # TODO implement.
+    pass
+
+
+def sample_reads(param_1, param_2, seed):
     # TODO
     raise NotImplementedError("TODO implement!")
 
@@ -32,9 +48,13 @@ def main():
     logger.debug("Downloading genomes from NCBI...")
     fetch_sequences()
     logger.debug("Sampling reads...")
-    sampled_reads = sample_reads(args)
+    sampled_reads = sample_reads(
+        param_1=args.p1,  # Change this as necessary.
+        param_2=args.p2,  # Change this as necessary.
+        seed=args.seed
+    )
     logger.debug("Saving samples to FastQ file {}.".format(args.out_file))
-    save_to_fastq(args, sampled_reads)
+    save_to_fastq(sampled_reads, args.out_dir, args.out_prefix)
     logger.info("Reads finished sampling.")
 
 
