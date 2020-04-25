Fixed major EM bug (sigmoid derivative not computed correctly).
Added informative metadata to sampled reads.

algs/em.py
@@ -129,10 +129,13 @@ class EMSolver(AbstractModelSolver):
             Q = (Q / Q.sum(dim=0)[None, :]).sum(dim=1) / Z_t.view(F)
 
             sigmoid = y[t]
-            sigmoid_jacobian = torch.ger(sigmoid, sigmoid) - \
+            sigmoid_jacobian = torch.ger(sigmoid, 1-sigmoid) - \
                                torch.diag(sigmoid).mm(
                                    torch.ones(S, S, device=self.device) - torch.eye(S, device=self.device)
                                )
+            # print("sigmoid: ", sigmoid)
+            # print("outer product", )
+            # print("jacobian: ", sigmoid_jacobian)
 
             x_gradient[t] = x_gradient[t] + sigmoid_jacobian.mv(
                 self.model.get_fragment_frequencies().t().mv(Q)
@@ -144,9 +147,11 @@ class EMSolver(AbstractModelSolver):
             x_gradient = x_gradient * gradient_clip / grad_t_norm
 
         # ==== Re-center to zero to prevent drift.
-        x_gradient = x_gradient - (torch.ones(size=x_gradient.size(), device=self.device) * x_gradient.mean())
+        updated_x = x + self.lr * x_gradient
+        updated_x = updated_x - (updated_x.mean() * torch.ones(size=updated_x.size(), device=self.device))
 
-        return x + self.lr * x_gradient
+        return updated_x
+
 
     def get_frag_likelihoods(self, t: int):
         """

run_inference.py
@@ -11,7 +11,7 @@ import torch
 
 from model.generative import GenerativeModel
 from model.bacteria import Population
-from model.reads import SequenceRead, FastQErrorModel
+from model.reads import SequenceRead, FastQErrorModel, NoiselessErrorModel
 from algs import em, vi, bbvi
 
 from typing import List
@@ -82,7 +82,7 @@ def perform_inference(reads: List[List[SequenceRead]],
     ##############################
     # Construct generative model
     mu = torch.zeros(len(population.strains))
-    tau_1 = 1
+    tau_1 = 100
     tau = 1
     window_size = len(reads[0][0].seq)
 
@@ -96,6 +96,7 @@ def perform_inference(reads: List[List[SequenceRead]],
                                bacteria_pop=population,
                                read_length=window_size,
                                read_error_model=my_error_model)
+    print(my_model.get_fragment_space())
 
     # logger.debug(str(my_model.get_fragment_space()))
     logger.debug("Strain keys:")
@@ -104,8 +105,8 @@ def perform_inference(reads: List[List[SequenceRead]],
 
     if method == "em":
         logger.info("Solving using Expectation-Maximization.")
-        solver = em.EMSolver(my_model, reads, torch_device=default_device, lr=1e-3)
-        abundances = solver.solve(iters=10000, print_debug_every=100, thresh=1e-5)
+        solver = em.EMSolver(my_model, reads, torch_device=default_device, lr=1e-4)
+        abundances = solver.solve(iters=10000, print_debug_every=100, thresh=1e-7, gradient_clip=1e2)
         save_abundances(
             population=population,
             time_points=time_points,