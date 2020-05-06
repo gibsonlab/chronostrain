Implemented VSMC. (Still need to verify ELBO up to sign. Temporarily "Fixed" the issue by removing minus sign in optimizer.)

algs/base.py
@@ -36,7 +36,11 @@ class AbstractModelSolver(metaclass=ABCMeta):
 # ===================================================================
 
 # Helper function for both EM and VI
-def compute_read_likelihoods(model: GenerativeModel, reads: List[List[SequenceRead]], device) -> List[torch.Tensor]:
+def compute_read_likelihoods(
+        model: GenerativeModel,
+        reads: List[List[SequenceRead]],
+        logarithm: bool,
+        device) -> List[torch.Tensor]:
     fragment_space = model.bacteria_pop.get_fragment_space(model.read_length)
 
     start_time = current_time_millis()
@@ -46,7 +50,8 @@ def compute_read_likelihoods(model: GenerativeModel, reads: List[List[SequenceRe
         # Each is an (F x N) matrix.
         return torch.tensor([
             [
-                math.exp(model.error_model.compute_log_likelihood(f, r))
+                model.error_model.compute_log_likelihood(f, r) if logarithm
+                else math.exp(model.error_model.compute_log_likelihood(f, r))
                 for r in reads[k]
             ] for f in fragment_space.get_fragments()
         ], device=device, dtype=torch.double)

algs/bbvi.py
@@ -8,10 +8,11 @@ from typing import List, Tuple
 import torch
 from torch.distributions.multivariate_normal import MultivariateNormal
 from torch.distributions.categorical import Categorical
+from torch.nn.functional import softmax
 
 from model.reads import SequenceRead
 
-from algs.base import AbstractModelSolver
+from algs.base import AbstractModelSolver, compute_read_likelihoods
 from model.generative import GenerativeModel
 from util.io.logger import logger
 from util.benchmarking import RuntimeEstimator
@@ -44,35 +45,61 @@ class MeanFieldPosterior:
         self.device = device
 
         # ================= Learnable parameters:
-        # The initial mean of the GP.
-        self.mean = torch.nn.Parameter(torch.zeros(
-            strains, device=device, dtype=torch.double
-        ), requires_grad=True)
+        # The mean parameters of the GP.
+        # t = 0: the mean of the GP.
+        # t > 0: Describes the conditional mean shift A, as in
+        #     E[X_{t+1} | X_{t} = y] = E[X_{t+1}] + TRANSITION[t+1]*(y - E[X_{t}])
+        #                            = TRANSITION[t+1]*y - A
+        self.means = [
+            torch.nn.Parameter(
+                torch.zeros(strains, device=device, dtype=torch.double),
+                requires_grad=True
+            )
+            for _ in range(times)
+        ]
 
-        # Represents the transition matrix Sigma_{t+1,t} * inv(Sigma_{t,t}).
-        self.transitions = [torch.nn.Parameter(torch.eye(
-            strains, strains, device=device, dtype=torch.double
-        ), requires_grad=True) for _ in range(times)]
+        # Represents the transition matrix Sigma_{t+1,t} * inv(Sigma_{t,t})
+        # These describe the means of the conditional distribution X_{t+1} | X_{t}.
+        self.transitions = [
+            torch.nn.Parameter(
+                torch.eye(strains, strains, device=device, dtype=torch.double),
+                requires_grad=True
+            )
+            for _ in range(times-1)
+        ]
 
-        # Represents the precision matrices inv(Sigma_{t+1,t+1} - Sigma_{t+1,t} inv(Sigma_{t,t}) * Sigma_{t,t+1}).
-        self.precisions = [torch.nn.Parameter(torch.eye(
-            strains, device=device, dtype=torch.double
-        ), requires_grad=True) for _ in range(times)]
+        # Represents the time-t covariances.
+        # Describes the conditional covariances Cov(X_{t+1} | X_{t}) -->
+        # think of "Sigma_{i,j}" as the (time-i, time-j) block of the complete Covariance matrix.
+        # t > 1: Sigma_{t+1,t+1} - Sigma_{t+1,t}*inv(Sigma_{t,t})*Sigma_{t,t+1}
+        # t = 1: Sigma_{1,1}
+        self.covariances = [
+            torch.nn.Parameter(
+                torch.eye(strains, device=device, dtype=torch.double),
+                requires_grad=True
+            )
+            for _ in range(times)
+        ]
 
         # The categorical weights for each time point.
-        self.frag_weights = [torch.nn.Parameter(torch.ones(
-            fragments, device=device, dtype=torch.double
-        ), requires_grad=True) for _ in range(times)]
+        self.frag_weights = [
+            torch.nn.Parameter(
+                torch.ones(size=[read_counts[t], fragments], device=device, dtype=torch.double),
+                requires_grad=True
+            ) for t in range(times)
+        ]
 
     def params(self) -> List[torch.nn.Parameter]:
         """
         Return a list of all learnable parameters (e.g. Parameter instances with requires_grad=True).
         """
-        return [self.mean] + self.transitions + self.precisions + self.frag_weights
+        return self.means + self.transitions + self.covariances + self.frag_weights
 
-    def sample(self, num_samples=1) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
+    def sample(self, num_samples=1, apply_softmax=False) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
         # Indexing: (time) x (sample idx) x (distribution dimension)
         X = self.rand_sample_X(num_samples)
+        if apply_softmax:
+            X = [softmax(x_t, dim=1) for x_t in X]
         F = self.rand_sample_F(num_samples)
         return X, F
 
@@ -80,44 +107,65 @@ class MeanFieldPosterior:
         return self.log_likelihood_X(X) + self.log_likelihood_F(F)
 
     def rand_sample_X(self, num_samples) -> List[torch.Tensor]:
-        # Dimension indexing: (T x N x S)
+        # Dimension indexing: (T instances of N x S tensors)
         X = [torch.empty(1) for _ in range(self.times)]
-        prev_x = self.mean.view(self.strains).repeat(num_samples, 1)
+        prev_x = None
         for t in range(self.times):
-            X[t] = MultivariateNormal(
-                loc=prev_x.mm(self.transitions[t]),
-                precision_matrix=self.precisions[t]
-            ).sample().to(self.device)
+            if t == 0:
+                X[0] = MultivariateNormal(
+                    loc=self.means[0].repeat(num_samples, 1),
+                    covariance_matrix=self.covariances[0]
+                ).sample().to(self.device)
+            else:
+                X[t] = MultivariateNormal(
+                    loc=prev_x.mm(self.transitions[t]) - self.means[t].repeat(num_samples, 1),
+                    covariance_matrix=self.covariances[t]
+                ).sample().to(self.device)
             X[t].requires_grad = False
             prev_x = X[t]
-        #
-        # # Dimension indexing: (N x T x S)
-        # X = torch.stack(X).transpose(0,1)
         return X
 
     def rand_sample_F(self, num_samples) -> List[torch.Tensor]:
-        # Indexing: (T x N x R)
+        # Indexing: (T instances of N x R tensors)
         F = [
-            Categorical(self.frag_weights[t])
-                .sample([num_samples, self.read_counts[t]])
-                .to(self.device)
+            (Categorical(self.frag_weights[t])
+             .sample([num_samples])
+             .to(self.device))
             for t in range(self.times)
         ]
         for t in range(self.times):
             F[t].requires_grad = False
-        #
-        # # Indexing: (N x T x R)
-        # F = torch.stack(F).transpose(0,1)
         return F
 
     def log_likelihood_X(self, X: List[torch.Tensor]) -> torch.Tensor:
         N = X[0].size(0)
         ans = torch.zeros(N, dtype=torch.double, device=self.device)
-        prev_x = self.mean.view(1, self.strains)
+
+        print("COVARIANCE MATRIX:")
+        print(self.covariances[0])
+        print("HERE!")
+
+        logger.info("Posterior sample:")
+        sample_x, sample_f = self.sample(num_samples=1, apply_softmax=True)
+        logger.info("X: {}".format(sample_x))
+        logger.info("F: {}".format(sample_f))
+
+        print("**********************")
+
+
+
         for t in range(self.times):
-            dist = MultivariateNormal(loc=prev_x.mm(self.transitions[t]), precision_matrix=self.precisions[t])
+            if t == 0:
+                dist = MultivariateNormal(
+                    loc=self.means[0].repeat(N, 1),
+                    covariance_matrix=self.covariances[0]
+                )
+            else:
+                dist = MultivariateNormal(
+                    loc=X[t-1].mm(self.transitions[t]) - self.means[t].repeat(N, 1),
+                    covariance_matrix=self.covariances[t]
+                )
             ans = ans + dist.log_prob(X[t])
-            prev_x = X[t]
         return ans
 
     def log_likelihood_F(self, F: List[torch.Tensor]) -> torch.Tensor:
@@ -149,6 +197,7 @@ class BBVISolver(AbstractModelSolver):
             read_counts=[len(reads) for reads in data],
             device=device
         )
+        self.read_log_likelihoods = compute_read_likelihoods(self.model, self.data, logarithm=True, device=device)
 
     def elbo_estimate(self, num_samples=1000):
         """
@@ -162,38 +211,41 @@ class BBVISolver(AbstractModelSolver):
             X=X_samples,
             F=F_samples
         )
-        posterior_ll_grad_disabled = posterior_ll.clone()
-        posterior_ll_grad_disabled.requires_grad = False
+        posterior_ll_grad_disabled = posterior_ll.detach()
 
         generative_ll = self.model.log_likelihood_torch(
             X=X_samples,
             F=F_samples,
-            R=self.data,
+            read_log_likelihoods=self.read_log_likelihoods,
             device=self.device
         )
 
-        elbo_monte_carlo = posterior_ll * (generative_ll - posterior_ll_grad_disabled)
-        return elbo_monte_carlo.mean()
+        elbo_samples = posterior_ll * (generative_ll - posterior_ll_grad_disabled)
+        return elbo_samples.mean()
 
     def solve(self,
               optim_class=torch.optim.SGD,
               optim_args=None,
               iters=4000,
               num_samples=1000,
-              print_debug_every=200
-              ):
+              print_debug_every=200):
+
         if optim_args is None:
-            optim_args = {'lr': 1e-4}
+            optim_args = {'lr': 1e-7}
         optimizer = optim_class(
             self.posterior.params(),
             **optim_args
         )
 
-        time_est = RuntimeEstimator(total_iters=iters, horizon=10)
+        logger.debug("BBVI algorithm started. (Gradient method, Target iterations={})".format(
+            iters
+        ))
+        time_est = RuntimeEstimator(total_iters=iters, horizon=5)
         for i in range(1, iters+1, 1):
             time_est.stopwatch_click()
 
             loss = -self.elbo_estimate(num_samples=num_samples)
+            print("ELBO loss = {}".format(loss))
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
@@ -205,5 +257,5 @@ class BBVISolver(AbstractModelSolver):
                 logger.debug("Iteration {i} | time left: {t} min. | Last ELBO = {loss}".format(
                     i=i,
                     t=time_est.time_left() // 60,
-                    loss=loss.item()
+                    loss=-loss.item()
                 ))

model/reads.py
@@ -2,6 +2,7 @@
  reads.py
  Contains classes for the error model of reads.
 """
+import math
 from abc import ABCMeta, abstractmethod
 import torch
 from model.fragments import Fragment
@@ -108,8 +109,13 @@ class AbstractTrainableErrorModel(AbstractErrorModel):
 # ======================================================================================
 
 class NoiselessErrorModel(AbstractErrorModel):
+    def __init__(self, mismatch_likelihood: float = 1e-20):
+        super().__init__()
+        self.mismatch_log_likelihood = math.log(mismatch_likelihood)
+        self.match_log_likelihood = math.log(1 - mismatch_likelihood)
+
     def compute_log_likelihood(self, fragment: Fragment, read: SequenceRead) -> float:
-        return 0. if fragment.seq == read.seq else float("-inf")
+        return self.match_log_likelihood if fragment.seq == read.seq else self.mismatch_log_likelihood
 
     def sample_noisy_read(self, fragment: str, metadata: str = "") -> SequenceRead:
         return SequenceRead(fragment, quality=torch.ones(len(fragment))*1000, metadata=metadata)
@@ -301,7 +307,6 @@ class BasicErrorModel(AbstractErrorModel):
         quality_score_vector = self.q_dist.sample_qvec()
 
         noisy_fragment_chars = ['' for _ in range(self.read_len)]
-        noisy_fragment_quality = [0]*self.read_len
 
         # Generate base pair reads from the fragment, conditioned on fragment base pairs and
         # the quality score for that base pair.
@@ -322,10 +327,9 @@ class BasicErrorModel(AbstractErrorModel):
 
             # Save the noisy output.
             noisy_fragment_chars[k] = BasicErrorModel.bases[noisy_letter]
-            noisy_fragment_quality[k] = q_score
 
         noisy_fragment_string = ''.join(noisy_fragment_chars)
-        seq_read = SequenceRead(noisy_fragment_string, noisy_fragment_quality, metadata=metadata)
+        seq_read = SequenceRead(noisy_fragment_string, quality_score_vector, metadata=metadata)
         return seq_read
 
 
@@ -373,7 +377,8 @@ class FastQErrorModel(AbstractErrorModel):
             fragment.seq[k] == read.seq[k] for k in range(len(fragment.seq))
         ]).to(dtype=torch.double)
 
-        return ((1 - error_prob) * matches + error_prob * (1 - matches)).log().sum().item()
+        return ((torch.ones(1, device=error_prob.device) - error_prob) * matches + (error_prob/3)
+                * (torch.ones(1, device=error_prob.device) - matches)).log().sum().item()
 
     def sample_noisy_read(self, fragment: str, metadata="") -> SequenceRead:
         qvec = self.q_dist.sample_qvec()

run_inference.py
@@ -11,13 +11,13 @@ import torch
 
 from model.generative import GenerativeModel
 from model.bacteria import Population
-from model.reads import SequenceRead, FastQErrorModel
-from algs import em, bbvi
+from model.reads import SequenceRead, FastQErrorModel, NoiselessErrorModel
+from algs import em, bbvi, vsmc
 from visualizations import plot_abundances as plotter
 
 from typing import List
 from util.io.logger import logger
-from util.io.model_io import get_all_accessions_csv, load_fastq_reads, save_abundances, load_abundances
+from util.io.model_io import load_fastq_reads, save_abundances, load_abundances
 
 # ============================= Constants =================================
 default_device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -59,6 +59,8 @@ def parse_args():
                              'strain by time point. For benchmarking.')
     parser.add_argument('-trim', '--marker_trim_len', required=False, type=int,
                         help='<Optional> An integer to trim markers down to. For testing/debugging.')
+    parser.add_argument('--disable_quality', action="store_true",
+                        help='<Flag> Turn off effect of quality scores.')
 
     return parser.parse_args()
 
@@ -103,22 +105,32 @@ def perform_em(
         logger.debug("Abundance squared-norm difference: {}".format(diff))
 
 
-def perform_bbvi(
+def perform_vsmc(
         model: GenerativeModel,
         reads: List[List[SequenceRead]]):
-    logger.info("Solving using Black-Box (Monte-Carlo) Variational Inference.")
-    solver = bbvi.BBVISolver(model=model, data=reads, device=default_device)
-    solver.solve()
+
+    # ==== Run the solver.
+    solver = vsmc.VSMCSolver(model=model, data=reads, torch_device=default_device)
+    solver.solve(
+        optim_class=torch.optim.Adam,
+        optim_args={'lr': 1e-2, 'betas': (0.9, 0.999), 'eps': 1e-7, 'weight_decay': 0.},
+        iters=1000,
+        num_samples=10000,
+        print_debug_every=20
+    )
     posterior = solver.posterior
 
+    # ==== Output the learned posterior.
     logger.info("Learned posterior:")
-    logger.info(posterior.params())
+    logger.info(posterior.means[0])
+    logger.info(posterior.covariances[0])
 
-    logger.info("Posterior sample:")
-    sample_x, sample_f = posterior.sample()
-    logger.info("X: ", sample_x)
-    logger.info("F: ", sample_f)
-    raise NotImplementedError("TODO check if this works.")
+    logger.info("softmax: {}".format(posterior.means[0].data.softmax(dim=0)))
+    logger.info("time 2 (AFTER SOFTMAX): {}".format(
+        (posterior.means[0] + posterior.means[1]).softmax(dim=0)
+    ))
+
+    raise NotImplementedError("TODO output plots.")
 
 
 def plot_result(
@@ -192,12 +204,16 @@ def main():
         raise ValueError("Specified sample times must be distinct.")
 
     # ==== Create model instance.
-    mu = torch.zeros(len(population.strains))
+    mu = torch.zeros(len(population.strains), device=default_device)
     tau_1 = 100
     tau = 1
     window_size = len(reads[0][0].seq)  # Assumes reads are all of the same length.
 
-    error_model = FastQErrorModel(read_len=window_size)
+    if args.disable_quality:
+        logger.info("Flag --disable_quality turned on; Quality scores are diabled.")
+        error_model = NoiselessErrorModel(mismatch_likelihood=1e-100)
+    else:
+        error_model = FastQErrorModel(read_len=window_size)
     model = GenerativeModel(
         times=args.time_points,
         mu=mu,
@@ -228,28 +244,12 @@ def main():
             plot_out_filename=args.plots_file,
             ground_truth_path=args.true_abundance_path
         )
-    elif args.method == 'bbvi':
-        perform_bbvi(
+    elif args.method == 'vsmc':
+        logger.info("Solving using Variational Sequential Monte-Carlo.")
+        perform_vsmc(
             model=model,
             reads=reads
         )
-    elif args.method == 'vi':
-        raise NotImplementedError("TODO: test!")
-        # logger.info("Solving using second-order variational inference.")
-        # posterior = vi.SecondOrderVariationalPosterior(
-        #     means=mu,
-        #     covariances=torch.eye(len(population.strains)),
-        #     frag_freqs=my_model.get_fragment_frequencies())
-        # solver = vi.SecondOrderVariationalGradientSolver(my_model, reads, posterior)
-        # abundances = solver.solve()
-        # save_abundances(
-        #     population=population,
-        #     time_points=time_points,
-        #     abundances=abundances,
-        #     out_filename=out_filename,
-        #     out_dir=out_dir
-        # )
-        # return abundances
     else:
         raise ValueError("{} is not an implemented method.".format(args.method))
 