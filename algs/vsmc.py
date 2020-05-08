Successfully changed dimension of Gaussians to S-1 from S. TODO: identified bug in VSMC as gradients being wrong only for first time point. Need to fix.

algs/vsmc.py
@@ -7,7 +7,6 @@ import torch
 from typing import List
 
 from torch.distributions import MultivariateNormal, Categorical
-from torch.nn.functional import softmax
 
 from model.generative import GenerativeModel
 from model.reads import SequenceRead
@@ -15,20 +14,7 @@ from algs.base import AbstractModelSolver, compute_read_likelihoods
 from util.benchmarking import RuntimeEstimator
 
 from util.io.logger import logger
-
-
-# ------------- torch function
-def multi_logit(x: torch.Tensor, dim=0) -> torch.Tensor:
-    """
-    Applies softmax along the specified dimension, after padding the secondary dimension on the right
-    (columns of a Matrix, or entries of a vector) with a zero.
-    If applied to a scalar tensor, effectively computes (p,q) = softmax(x, 0) = (logit(x), 1-logit(x)).
-    """
-    return softmax(
-        torch.nn.functional.pad(x, pad=[0, 1]),  # Add a column of zeros.
-        dim=dim
-    )
-# ------------------------------
+from util.torch import multi_logit
 
 
 class VariationalSequentialPosterior:
@@ -54,7 +40,7 @@ class VariationalSequentialPosterior:
         #
         self.means = [
             torch.nn.Parameter(
-                torch.zeros(num_strains, device=device, dtype=torch.double),
+                torch.zeros(num_strains-1, device=device, dtype=torch.double),
                 requires_grad=True
             )
             for _ in range(num_times)
@@ -64,8 +50,8 @@ class VariationalSequentialPosterior:
         # These describe the means of the conditional distribution X_{t+1} | X_{t}.
         self.transitions = [
             torch.nn.Parameter(
-                torch.eye(num_strains, num_strains, device=device, dtype=torch.double),
-                requires_grad=False  # TODO change to "True" to enable training.
+                torch.eye(num_strains-1, num_strains-1, device=device, dtype=torch.double),
+                requires_grad=True
             )
             for _ in range(num_times - 1)
         ]
@@ -75,10 +61,10 @@ class VariationalSequentialPosterior:
         # think of "Sigma_{i,j}" as the (time-i, time-j) block of the complete Covariance matrix.
         # t > 1: Sigma_{t+1,t+1} - Sigma_{t+1,t}*inv(Sigma_{t,t})*Sigma_{t,t+1}
         # t = 1: Sigma_{1,1}
-        self.covariances = [
+        self.cond_covar_cholesky = [
             torch.nn.Parameter(
-                torch.eye(num_strains, device=device, dtype=torch.double),
-                requires_grad=False  # TODO change to "True" to enable training.
+                torch.eye(num_strains-1, device=device, dtype=torch.double),
+                requires_grad=True
             )
             for _ in range(num_times)
         ]
@@ -87,8 +73,8 @@ class VariationalSequentialPosterior:
         """
         Return a list of all learnable parameters (e.g. Parameter instances with requires_grad=True).
         """
-        # return self.means + self.transitions + self.covariances
-        return self.means
+        return self.means + self.transitions + self.cond_covar_cholesky
+        # return self.means
 
     def get_means_detached(self):
         return [mean.detach() for mean in self.means]
@@ -98,11 +84,10 @@ class VariationalSequentialPosterior:
         prev_covariance = None
         for t in range(self.times):
             if t == 0:
-                covariance = self.covariances[0]
+                covariance = self.cond_covar_cholesky[0].t().mm(self.cond_covar_cholesky[0])
             else:
-                covariance = self.covariances[t] + self.transitions[t-1].mm(
-                    prev_covariance.mm(self.transitions[t-1].t())
-                )
+                covariance = self.cond_covar_cholesky[t].t().mm(self.cond_covar_cholesky[t]) \
+                             + self.transitions[t-1].mm(prev_covariance.mm(self.transitions[t-1].t()))
             variances.append(covariance.diagonal().detach())
             prev_covariance = covariance
         return variances
@@ -148,7 +133,9 @@ class VSMCSolver(AbstractModelSolver):
                 # Initial distribution of X_0 (no resampling required).
                 distribution = MultivariateNormal(
                     loc=self.posterior.means[0].expand(num_samples, -1),
-                    covariance_matrix=self.posterior.covariances[0]
+                    covariance_matrix=(self.posterior.cond_covar_cholesky[0]
+                                       .t()
+                                       .mm(self.posterior.cond_covar_cholesky[0]))
                 )
             else:
                 # Resampling (double-check that gradient is turned off.)
@@ -156,8 +143,12 @@ class VSMCSolver(AbstractModelSolver):
 
                 # distribution of X_t.
                 distribution = MultivariateNormal(
-                    loc=(X_prev[children] - self.posterior.means[t-1].expand(num_samples, -1)).mm(self.posterior.transitions[t-1]) + self.posterior.means[t].expand(num_samples, -1),
-                    covariance_matrix=self.posterior.covariances[t]
+                    loc=((X_prev[children] - self.posterior.means[t-1].expand(num_samples, -1))
+                         .mm(self.posterior.transitions[t-1])
+                         + self.posterior.means[t].expand(num_samples, -1)),
+                    covariance_matrix=(self.posterior.cond_covar_cholesky[t]
+                                       .t()
+                                       .mm(self.posterior.cond_covar_cholesky[t]))
                 )
             X = distribution.sample().detach()
 
@@ -178,8 +169,8 @@ class VSMCSolver(AbstractModelSolver):
     def generative_log_prob(self, t, X, X_prev):
         """
         :param t: the time index (0 thru T-1)
-        :param X: (N x S) tensor of time (t) samples.
-        :param X_prev: (N x S) tensor of time (t-1) samples.
+        :param X: (N x S-1) tensor of time (t) samples.
+        :param X_prev: (N x S-1) tensor of time (t-1) samples.
         :return: The joint log-likelihood p(X_t, Reads_t | X_{t-1}).
         """
         # Gaussian part
@@ -188,12 +179,12 @@ class VSMCSolver(AbstractModelSolver):
             center = self.model.mu.repeat(N, 1)
         else:
             center = X_prev
-        covariance = self.model.time_scale(t) * torch.eye(self.model.num_strains(), device=self.device)
+        covariance = self.model.time_scale(t) * torch.eye(self.model.num_strains() - 1, device=self.device)
         distribution = MultivariateNormal(loc=center, covariance_matrix=covariance)
         gaussian_log_probs = distribution.log_prob(X)
 
         # Reads likelihood calculation, conditioned on the Gaussian part.
-        data_log_probs = (softmax(X, dim=1)
+        data_log_probs = (multi_logit(X, dim=1)
                           .mm(self.model.get_fragment_frequencies().t())
                           .mm(self.read_likelihoods[t])
                           .log()
@@ -223,13 +214,9 @@ class VSMCSolver(AbstractModelSolver):
             time_est.stopwatch_click()
 
             # Maximize elbo by minimizing -elbo.
-            elbo = self.elbo_surrogate_estimate(num_samples=num_samples)
+            elbo = -self.elbo_surrogate_estimate(num_samples=num_samples)
             optimizer.zero_grad()
             elbo.backward()
-            # print("\t****** grad: ", self.posterior.means[0].grad)
-
-            # elbo = -self.elbo_surrogate_estimate(num_samples=num_samples, mean=self.posterior.means[0])
-            # print(" ============= ELBO before step: ", elbo.data)
 
             optimizer.step()
 

run_inference.py
@@ -273,8 +273,8 @@ def create_model(population: Population,
     @param disable_quality: A flag to indicate whether or not to use NoiselessErrorModel.
     @return A Generative model object.
     """
-    mu = torch.zeros(len(population.strains), device=default_device)
-    tau_1 = 100
+    mu = torch.zeros(len(population.strains) - 1, device=default_device)
+    tau_1 = 1000
     tau = 1
 
     if disable_quality:

visualizations/plot_abundances.py
@@ -1,4 +1,3 @@
-import math
 from typing import List
 
 import torch
@@ -8,6 +7,7 @@ import pandas as pd
 
 from model.bacteria import Population
 from util.io.model_io import load_abundances
+from util.torch import multi_logit
 
 
 def plot_abundances_comparison(
@@ -72,29 +72,25 @@ def plot_posterior_abundances(
         _, true_abundances, accessions = load_abundances(truth_path)
         truth_acc_dict = {acc: i for i, acc in enumerate(accessions)}
 
-    mean_softmax = [torch.nn.functional.softmax(means[t], dim=0) for t in range(len(times))]
+    mean_softmax = [multi_logit(means[t], dim=0) for t in range(len(times))]
     stdevs = [variances[t].sqrt() for t in range(len(times))]
 
     for s, strain in enumerate(population.strains):
         val = [mean_softmax[t][s].item() for t in range(len(times))]
-        delta = -torch.ones(size=means[0].size(), device=means[0].device)
-        delta[s] = 1
-        upper = [
-            torch.nn.functional.softmax(means[t] + (stdevs[t] * delta[s]), dim=0)[s].item()
-            for t in range(len(times))
-        ]
-        lower = [
-            torch.nn.functional.softmax(means[t] - (stdevs[t] * delta[s]), dim=0)[s].item()
-            for t in range(len(times))
-        ]
-        # print("Upper: ", upper)
-        # print("Lower: ", lower)
-        # upper = [means[t][s].item() + math.sqrt(variances[t][s].item()) for t in range(len(times))]
-        # lower = [means[t][s].item() - math.sqrt(variances[t][s].item()) for t in range(len(times))]
+        # delta = -torch.ones(size=means[0].size(), device=means[0].device)
+        # delta[s] = 1
+        # upper = [
+        #     multi_logit(means[t] + 2 * (stdevs[t] * delta), dim=0)[s].item()
+        #     for t in range(len(times))
+        # ]
+        # lower = [
+        #     multi_logit(means[t] - 2 * (stdevs[t] * delta), dim=0)[s].item()
+        #     for t in range(len(times))
+        # ]
 
         p = plt.plot(times, val, linestyle='solid', label=strain.name)
         color = p[-1].get_color()
-        plt.fill_between(times, lower, upper, facecolor=color, alpha=0.2)
+        # plt.fill_between(times, lower, upper, facecolor=color, alpha=0.2)
 
         if true_abundances is not None:
             truth = [true_abundances[t, truth_acc_dict[strain.name]].item() for t in range(len(times))]