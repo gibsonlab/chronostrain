78d661879ad575353ff407ab9e05d1af361c2f3e
Youn <zephyr0223@gmail.com>
2020-05-14 11:41:30 -0400

    added em_suite and em_suite_lowquality from Google Drive

--- tests/em_perf/em_suite_lowquality.sh
+++ tests/em_perf/em_suite_lowquality.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-TESTNAME="em_perf"
+TESTNAME="em_perf_lowquality"
 SPARSE_DEPTH="10"
 
-echo "----------------------------------- testname ${TESTNAME} -----------------------------------"
+echo "----------------------------------- Quality [ON], testname ${TESTNAME} -----------------------------------"
 for depth in 10 30 50 70 90 100 200 300 400 500 600 700 800 900
 do
   for trial in 1 2 3 4 5 6 7 8 9 10
@@ -18,13 +18,31 @@ do
   done
 done
 
+TESTNAME_qoff="em_perf_argmax_lowquality"
+
+echo "----------------------------------- Quality [OFF], testname ${TESTNAME_qoff} -----------------------------------"
+for depth in 10 30 50 70 90 100 200 300 400 500 600 700 800 900
+do
+  for trial in 1 2 3 4 5 6 7 8 9 10
+  do
+    python3 plot_abundance_output.py \
+    --abundance_path "data/output/test_${TESTNAME_qoff}/depth_$depth-trial_$trial/EM_result_${TESTNAME_qoff}.csv" \
+    --output_path "data/output/test_${TESTNAME_qoff}/depth_$depth-trial_$trial/plot.png" \
+    --ground_truth_path "tests/em_perf/true_abundances.csv" \
+    --font_size 18 \
+    --thickness 3
+  done
+done
+
 echo "--------------------------------- Plotting comparisons. ----------------------------------------------"
 trial_params=""
+qoff_trial_params=""
 for depth in 10 30 50 70 90 100 200 300 400 500 600 700 800 900
 do
   for trial in 1 2 3 4 5 6 7 8 9 10
   do
     trial_params="$trial_params -t qon $depth data/output/test_$TESTNAME/depth_$depth-trial_$trial/EM_result_$TESTNAME.csv"
+    qoff_trial_params="${qoff_trial_params} -t qoff $depth data/output/test_${TESTNAME_qoff}/depth_$depth-trial_$trial/EM_result_${TESTNAME_qoff}.csv"
   done
 done
 
@@ -33,4 +51,5 @@ python3 plot_performances.py \
 --output_path "data/output/test_$TESTNAME/performance_plot.png" \
 --font_size 18 \
 --thickness 3 \
-$trial_params
+--draw_legend \
+$trial_params $qoff_trial_params
