more fixes for softmax (reverting from multi_logit)

algs/em.py
@@ -7,13 +7,14 @@ from model.reads import SequenceRead
 from model.generative import GenerativeModel
 
 import torch
+from torch.nn.functional import softmax
 
-from util.torch import multi_logit
+# from util.torch import multi_logit
 
 torch.set_default_dtype(torch.float64)
 
 # ===========================================================================================
-# =============== Expectation-Maximization (for getting a MAP estimator) ====================
+# =============== Expectation-Maximization (for computing a MAP estimator) ==================
 # ===========================================================================================
 
 
@@ -33,7 +34,7 @@ class EMSolver(AbstractModelSolver):
         :param generative_model: The underlying generative model with prior parameters.
         :param data: the observed data, a time-indexed list of read collections.
         :param torch_device: the torch device to operate on. (Recommended: CUDA if available.)
-        :param lr: the learning rate (default: 1e-3
+        :param lr: the learning rate (default: 1e-3)
         """
         super().__init__(generative_model, data)
         self.read_likelihoods = compute_read_likelihoods(self.model, self.data, logarithm=False, device=torch_device)
@@ -91,7 +92,7 @@ class EMSolver(AbstractModelSolver):
             time_est.increment(secs_elapsed)
 
             diff = torch.norm(
-                multi_logit(updated_brownian_motion, dim=1) - multi_logit(brownian_motion, dim=1),
+                softmax(updated_brownian_motion, dim=1) - softmax(brownian_motion, dim=1),
                 p='fro'
             )
 
@@ -110,9 +111,10 @@ class EMSolver(AbstractModelSolver):
             k += 1
         logger.info("Finished {k} iterations.".format(k=k))
 
-        abundances = [multi_logit(gaussian, dim=0) for gaussian in brownian_motion]
-        normalized_abundances = torch.stack(abundances).to(self.device)
-        return normalized_abundances
+        return softmax(brownian_motion, dim=1).to(self.device)
+        # abundances = [softmax(gaussian, dim=) for gaussian in brownian_motion]
+        # normalized_abundances = torch.stack(abundances).to(self.device)
+        # return normalized_abundances
 
     def do_noisy_mapping(self):
         noisy_mappings = []
@@ -150,7 +152,8 @@ class EMSolver(AbstractModelSolver):
                     x_gradient[t] = variance_scaling_prev * (x[t] - x[t-1]) + variance_scaling_next * (x[t] - x[t+1])
 
         # ====== Sigmoidal part
-        y = multi_logit(x, dim=1)
+        # y = multi_logit(x, dim=1)
+        y = softmax(x, dim=1)
         for t in range(T):
             # Scale each row by Z_t, and normalize.
             Z_t = self.model.strain_abundance_to_frag_abundance(y[t].view(S, 1))

algs/vi.py
@@ -8,7 +8,7 @@ from model.generative import GenerativeModel
 from model.reads import SequenceRead
 from algs.base import AbstractModelSolver, compute_read_likelihoods
 
-from util.torch import multi_logit
+from torch.nn.functional import softmax
 from util.benchmarking import RuntimeEstimator
 from util.io.logger import logger
 
@@ -54,7 +54,7 @@ class MeanFieldPosterior(AbstractVariationalPosterior):
         S = self.model.num_strains()
 
         W_f = self.model.get_fragment_frequencies()[f]  # S-dim vector.
-        sigma = multi_logit(X, dim=1)  # N x S
+        sigma = softmax(X, dim=1)  # N x S
         Wf_dot_sigma = sigma.mv(W_f)  # N-dim vector.
 
         sigma_deriv = self.sigmoid_derivative(X)  # N x S x S
@@ -153,7 +153,7 @@ class MeanFieldPosterior(AbstractVariationalPosterior):
         return (
                 (1 / self.model.time_scale(t) ** 2) * (X_t - X_prev).norm(p='fro', dim=1)
                 +
-                (multi_logit(X_t, dim=1).mm(self.model.get_fragment_frequencies().t())).log().mv(
+                (softmax(X_t, dim=1).mm(self.model.get_fragment_frequencies().t())).log().mv(
                     self.phi[t].sum(dim=1)
                 )
         )  # length N
@@ -167,12 +167,11 @@ class MeanFieldPosterior(AbstractVariationalPosterior):
 
         :param t: the time index of current sample.
         :param X_t: (N x S) tensor of proposal samples.
-        :param X_prev: (N x S) tensor of parent samples (with row indices matching X_t).
         :param weights: a length N vector of sequential sample weights.
         :return the difference (in L2 norm) of phi.
         """
         N = X_t.size(0)
-        log_statistic = (multi_logit(X_t, dim=1)
+        log_statistic = (softmax(X_t, dim=1)
                          .mm(self.model.get_fragment_frequencies().t())
                          .log())  # N x F
         tilt = (weights.view(1, N).mm(log_statistic)
@@ -278,7 +277,7 @@ class MeanFieldPosterior(AbstractVariationalPosterior):
     def sigmoid_derivative(self, X: torch.Tensor) -> torch.Tensor:
         N = X.size(0)
         S = self.model.num_strains()
-        sigmoid = multi_logit(X, dim=1).view(N, S, 1)
+        sigmoid = softmax(X, dim=1).view(N, S, 1)
         deriv = sigmoid.matmul(sigmoid.transpose(1, 2))
         for n in range(N):
             deriv[n] = torch.diag(sigmoid[n].view(S)) - deriv[n]
@@ -300,7 +299,7 @@ class MeanFieldPosterior(AbstractVariationalPosterior):
         """
         N = X.size(0)
         S = self.model.num_strains()
-        sigmoid = multi_logit(X, dim=1).view(N, S, 1)
+        sigmoid = softmax(X, dim=1).view(N, S, 1)
         hess = sigmoid.matmul(sigmoid.transpose(1, 2)).view(N, S, S, 1).matmul(
             sigmoid.expand([S, -1, -1, -1]).permute([1, 0, 3, 2])
         )  # N x S x S x S

algs/vsmc.py
@@ -15,7 +15,7 @@ from algs.base import AbstractModelSolver, compute_read_likelihoods
 from util.benchmarking import RuntimeEstimator
 
 from util.io.logger import logger
-from util.torch import multi_logit
+from torch.nn.functional import softmax
 
 
 class VariationalSequentialPosterior(AbstractVariationalPosterior):
@@ -97,7 +97,7 @@ class VariationalSequentialPosterior(AbstractVariationalPosterior):
         # Indexing: (time) x (sample idx) x (distribution dimension)
         X = self.rand_sample_X(num_samples)
         if apply_softmax:
-            X = [multi_logit(x_t, dim=1) for x_t in X]
+            X = [softmax(x_t, dim=1) for x_t in X]
         return X
 
     def rand_sample_X(self, num_samples) -> List[torch.Tensor]:

model/generative.py
@@ -12,7 +12,7 @@ from model.reads import AbstractErrorModel, SequenceRead
 from util.io.logger import logger
 
 from torch.distributions.multivariate_normal import MultivariateNormal
-from util.torch import multi_logit
+from torch.nn.functional import softmax
 
 
 class GenerativeModel:
@@ -98,7 +98,7 @@ class GenerativeModel:
         gaussian_log_probs = MultivariateNormal(loc=center, covariance_matrix=covariance).log_prob(X)
 
         # Reads likelihood calculation, conditioned on the Gaussian part.
-        data_log_probs = (multi_logit(X, dim=1)
+        data_log_probs = (softmax(X, dim=1)
                           .mm(self.get_fragment_frequencies().t())
                           .mm(read_likelihoods)
                           .log()
@@ -194,7 +194,7 @@ class GenerativeModel:
         :return: A T x S tensor; each row is an abundance profile for a time point.
         """
         gaussians = self._sample_brownian_motion()
-        return multi_logit(gaussians, dim=1)
+        return softmax(gaussians, dim=1)
 
     def strain_abundance_to_frag_abundance(self, strain_abundances: torch.Tensor) -> torch.Tensor:
         """

visualizations/plot_abundances.py
@@ -9,7 +9,7 @@ import pandas as pd
 from algs.vi import AbstractVariationalPosterior
 from model.bacteria import Population
 from util.io.model_io import load_abundances
-from util.torch import multi_logit
+from torch.nn.functional import softmax
 
 
 def plot_abundances_comparison(
@@ -134,7 +134,7 @@ def plot_posterior_abundances(
         _, true_abundances, accessions = load_abundances(truth_path, torch_device=torch.device("cpu"))
         truth_acc_dict = {acc: i for i, acc in enumerate(accessions)}
 
-    abundance_samples = [multi_logit(x_t, dim=1) for x_t in posterior.sample(num_samples=num_samples)]
+    abundance_samples = [softmax(x_t, dim=1) for x_t in posterior.sample(num_samples=num_samples)]
     data = pd.DataFrame(np.array(
         [
             (times[t], strain.name, abundance_t[i, s].item(), 'Learned')