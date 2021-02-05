a484e8b5a847a165ddaa7ed40f9df2e9f4166990
zgromko <zgro97@msn.com>
2021-02-05 13:51:17 -0800

    Added primer parsing, refactored utils into database, added V4 regions to tests/ncbi_refs

--- database/base.py
+++ database/base.py
@@ -1,14 +1,17 @@
 from abc import abstractmethod, ABCMeta
 
 from model.bacteria import Strain, Marker, MarkerMetadata
-from util.io.fetch_genomes import fetch_sequences, SubsequenceMetadata
+from util.io.fetch_genomes import fetch_filenames
 
 import os
 import re
+import json
 from typing import List
 from util.io.logger import logger
+from dataclasses import dataclass
 
 _DEFAULT_DATA_DIR = "data"
+_MAXIMUM_MARKER_LEN = 2000
 
 
 class AbstractStrainDatabase(metaclass=ABCMeta):
@@ -48,26 +51,21 @@ class SimpleCSVStrainDatabase(AbstractStrainDatabase):
         super().__init__()
 
     def __load__(self):
-        strain_info_map = fetch_sequences(refs_file_json=self.json_refs)
+        strain_info_map = self.fetch_sequences()
         for strain_accession in strain_info_map.keys():
 
-            input_file_path = os.path.join(_DEFAULT_DATA_DIR, strain_accession + ".fasta")
-            with open(input_file_path) as file:
-                lines = [re.sub('[^AGCT]+', '', line.split(sep=" ")[-1]) for line in file]
-            genome = ''.join(lines)
-            if self.trim_debug is not None:
-                genome = genome[:self.trim_debug]
+            sequence_loader = strain_info_map[strain_accession]['sequenceLoader']
+            genome = sequence_loader.get_full_genome(self.trim_debug)
             markers = []
-            if 'subsequences' in strain_info_map[strain_accession].keys():
-                for subsequence in strain_info_map[strain_accession]['subsequences'].keys():
-                    markers.append(Marker(
-                        name = subsequence,
-                        seq = strain_info_map[strain_accession]['subsequences'][subsequence].get_subsequence(genome),
-                        metadata = MarkerMetadata(parent=strain_accession,
-                            subsequence_name = strain_info_map[strain_accession]['subsequences'][subsequence].name,
-                            parent_genome_length = len(genome)
-                        )
-                    ))
+            for subsequence_data in sequence_loader.get_marker_subsequences():
+                markers.append(Marker(
+                    name = subsequence_data.ID,
+                    seq = subsequence_data.get_subsequence(genome),
+                    metadata = MarkerMetadata(parent=strain_accession,
+                        subsequence_name = subsequence_data.name,
+                        parent_genome_length = sequence_loader.get_genome_length()
+                    )
+                ))
             if len(markers) > 0:
                 self.strain_to_markers[strain_accession] = markers
 
@@ -83,3 +81,214 @@ class SimpleCSVStrainDatabase(AbstractStrainDatabase):
             Strain(name=s_id, markers=markers, genome_length=markers[0].metadata.parent_genome_length)
             for (s_id, markers) in self.strain_to_markers.items()
         ]
+
+
+    def fetch_sequences(self):
+        """
+        Read JSON file, and download FASTA from accessions if doesn't exist.
+        :return: a dictionary mapping accessions to strain-accession-filename-subsequences
+                 wrappers.
+        """
+        strains_map = {}
+
+        with open(self.json_refs, "rb") as f:
+            strain_infos = json.load(f, object_hook = lambda info: StrainInfo(info))
+            for info in strain_infos:
+                info.validate()
+                fasta_filename, genbank_filename = fetch_filenames(info['accession'])
+                strains_map[info['accession']] = {
+                    "strain": info['name'],
+                    "accession": info['accession'],
+                    "file": fasta_filename,
+                    "sequenceLoader": SubsequenceLoader(fasta_filename, genbank_filename, info['markers'])
+                }
+
+        logger.info("Found {} records.".format(len(strains_map.keys())))
+        return strains_map
+    
+
+class SubsequenceLoader:
+
+
+    def __init__(self, fasta_filename, genbank_filename, marker_info: list):
+        self.fasta_filename = fasta_filename
+        self.genbank_filename = genbank_filename
+        self.marker_info = marker_info
+        self.full_genome = None # Lazy loading in get_full_genome() and get_genome_length()
+        self.genome_length = None
+
+
+    def get_full_genome(self, trim_debug=None):
+        if self.full_genome is None:
+            with open(self.fasta_filename) as file:
+                lines = [re.sub('[^AGCT]+', '', line.split(sep=" ")[-1]) for line in file]
+            self.full_genome = ''.join(lines)
+            if trim_debug is not None:
+                    self.full_genome = self.full_genome[:trim_debug]
+        return self.full_genome
+
+
+    def get_genome_length(self):
+        if self.genome_length is None:    
+            self.genome_length = len(self.get_full_genome())
+        return self.genome_length
+
+
+    def get_marker_subsequences(self):
+        locus_tags_to_names = {}
+        names_to_primers = {}
+        for marker in self.marker_info:
+            if marker['type'] == 'tag':
+                locus_tags_to_names[marker['id']] = marker['name']
+            elif marker['type'] == 'primer':
+                names_to_primers[marker['name']] = (marker['forward'], marker['reverse'])
+        return self.get_subsequences_from_locus_tags(locus_tags_to_names) + self.get_subsequences_from_primers(names_to_primers)
+
+
+    def parse_index_tag(self, index_tag: str, sequence_name, locus_tag):
+        '''
+        Parses a genome location tag, e.g. "complement(312..432)" to create SubsequenceMetadata
+        and pads indices by 100bp
+        '''
+        indices = re.findall(r'[0-9]+', index_tag)
+        if not len(indices) == 2:
+            logger.warning('Encountered match to malformed tag: ' + index_tag)
+            return None
+        
+        max_index = self.get_genome_length()
+        return SubsequenceMetadata(
+            name=sequence_name, start_index=max(0, int(indices[0])-100), ID=locus_tag,
+            end_index=min(int(indices[1])+100, max_index), complement='complement' in index_tag
+        )
+        
+
+    def get_subsequences_from_locus_tags(self, tags_to_names: dict):
+        '''
+        :param filename: Name of local genbank file
+        :param tags_to_names: Dictionary of locus tags to the name of their subsequence
+        :return: List of SubsequenceMetadata deriving from all matched tags in the genbank file
+        '''
+        chunk_designation = None
+        potential_index_tag = ''
+        locus_tags = set(tags_to_names.keys())
+        subsequences = []
+        
+        with open(self.genbank_filename, 'rb') as genbank_file:
+            for line in genbank_file:
+                # Split on >1 space
+                split_line = re.split(r'\s{2,}', line.decode('utf-8').strip())
+                
+                if len(split_line) == 2:
+                    chunk_designation = split_line[0]
+                    if chunk_designation == 'gene':
+                        # The first line of a gene chunk is the index tag
+                        potential_index_tag = split_line[1]
+                    
+                if chunk_designation == 'gene':
+                    if 'locus_tag' in split_line[-1]:
+                        # Tags are declared by: /locus_tag=""
+                        tag = split_line[-1].split('"')[1]
+                        if tag in locus_tags:
+                            subsequences.append(self.parse_index_tag(potential_index_tag, tags_to_names[tag], tag))
+                        
+        return [sequence for sequence in subsequences if sequence is not None]
+
+
+    def parse_fasta_regex(self, sequence):
+        fasta_translation = {
+            'R':'[AG]',  'Y':'[CT]',   'K':'[GT]',
+            'M':'[AC]',  'S':'[CG]',   'W':'[AT]',
+            'B':'[CGT]', 'D':'[AGT]',  'H':'[ACT]',
+            'V':'[ACG]', 'N':'[ACGT]', 'A':'A',
+            'C':'C',     'G':'G',      'T':'T'
+        }
+        sequence_regex = ''
+        for char in sequence:
+            sequence_regex += fasta_translation[char]
+        return sequence_regex
+
+
+    def complement_regex(self, regex):
+        complement_translation = {'A': 'T', 'T':'A', 'G':'C', 'C':'G'}
+        complemented_regex = ''
+        for char in regex:
+            if char in complement_translation.keys():
+                complemented_regex += complement_translation[char]
+            else:
+                complemented_regex += char
+        return complemented_regex
+
+
+    def find_primer_match(self, forward_regex, reverse_regex):
+        best_match = (None, _MAXIMUM_MARKER_LEN)
+        forward_matches = list(re.finditer(forward_regex, self.get_full_genome()))
+        reverse_matches = list(re.finditer(reverse_regex, self.get_full_genome()))
+
+        for forward_match in forward_matches:
+            for reverse_match in reverse_matches:
+                match_length = reverse_match.end()-forward_match.start()
+                if match_length < best_match[1] and match_length > 0:
+                    best_match = ((forward_match.start(), reverse_match.end()),match_length)
+        return best_match[0]
+
+
+    def get_subsequences_from_primers(self, names_to_primers: dict):
+        subsequences = []
+
+        for marker_name in names_to_primers.keys():
+            forward_primer_regex = self.parse_fasta_regex(names_to_primers[marker_name][0])
+            reverse_primer_regex = self.parse_fasta_regex(names_to_primers[marker_name][1])
+            match_indices = self.find_primer_match(forward_primer_regex, reverse_primer_regex)
+            if match_indices is not None:
+                subsequences.append(SubsequenceMetadata(
+                    name = marker_name, ID = '-'.join(names_to_primers[marker_name]),
+                    start_index = match_indices[0], end_index = match_indices[1],
+                    complement = False
+                ))
+                continue
+            forward_primer_regex = self.complement_regex(forward_primer_regex)
+            reverse_primer_regex = self.complement_regex(reverse_primer_regex)
+            match_indices = self.find_primer_match(forward_primer_regex, reverse_primer_regex)
+            if match_indices is not None:
+                subsequences.append(SubsequenceMetadata(
+                    name = marker_name, ID = '-'.join(names_to_primers[marker_name]),
+                    start_index = match_indices[0], end_index = match_indices[1],
+                    complement = True
+                ))
+
+        return subsequences
+
+
+class StrainInfo(dict):
+    def __getattr__(self, key):
+        return self[key]
+ 
+    def __setattr__(self, key, value):
+        if key not in ['name', 'accession', 'markers']:
+            logger.error('Invalid attribute name in refs JSON')
+            raise KeyError
+        self[lower(key)] = value
+
+    def validate(self):
+        if 'name' not in self.keys() or 'accession' not in self.keys() or 'markers' not in self.keys():
+            logger.error('Missing name, accession, or markers for entry in refs JSON: ' + str(self))
+            class StrainInfoIncompleteError(Exception):
+                pass
+            raise StrainInfoIncompleteError
+
+
+@dataclass
+class SubsequenceMetadata:
+    name: str
+    ID: str
+    start_index: int
+    end_index: int
+    complement: bool # Indicates whether the intended sequence is the complement of the given sequence
+    
+    def get_subsequence(self, genome: str):
+        complement_translation = {'A': 'T', 'T':'A', 'G':'C', 'C':'G'}
+        complement_func = (lambda base: complement_translation[base]) if self.complement else lambda base: base
+        subsequence = ''
+        for base in genome[self.start_index:self.end_index]:
+            subsequence += complement_func(base)
+        return subsequence
--- util/io/fetch_genomes.py
+++ util/io/fetch_genomes.py
@@ -1,10 +1,8 @@
 import os
 import re
-import json
 import urllib.request as urllib
 from util.io.logger import logger
 from util.io.filesystem import convert_size, get_filesize_bytes
-from dataclasses import dataclass
 
 _base_dir = "data"
 _fasta_filename = "{accession}.fasta"
@@ -51,122 +49,5 @@ def fetch_filenames(accession):
     return (get_fasta_filename(accession), get_genbank_filename(accession))
 
 
-def fetch_sequences(refs_file_json: str):
-    """
-    Read JSON file, and download FASTA from accessions if doesn't exist.
-    :return: a dictionary mapping accessions to strain-accession-filename-subsequences
-             wrappers.
-    """
-    strains_map = {}
 
-    with open(refs_file_json, "rb") as f:
-        strain_infos = json.load(f, object_hook = lambda info: StrainInfo(info))
-        for info in strain_infos:
-            info.validate()
-            fasta_filename, genbank_filename = fetch_filenames(info['accession'])
-            strains_map[info['accession']] = {
-                "strain": info['name'],
-                "accession": info['accession'],
-                "file": fasta_filename,
-                "subsequences": parse_genbank_tags(genbank_filename, info['tags'])
-            }
-
-    logger.info("Found {} records.".format(len(strains_map.keys())))
-    return strains_map
-    
-
-def parse_subsequence_tag(genbank_tag: str, sequence_name, max_index):
-    '''
-    Parses a genome location tag, e.g. "complement(312..432)" to create SubsequenceMetadata
-    and pads indices by 100bp
-    '''
-    indices = re.findall(r'[0-9]+', genbank_tag)
-    if not len(indices) == 2:
-        logger.warning('Encountered match to malformed tag: ' + genbank_tag)
-        return None
-    
-    return SubsequenceMetadata(
-        name=sequence_name, start_index=max(0, int(indices[0])-100), 
-        end_index=min(int(indices[1])+100, max_index), complement='complement' in genbank_tag
-    )
-    
-
-def parse_genbank_tags(filename, tags_to_names):
-    '''
-    :param filename: Name of local genbank file
-    :param tags_to_names: Dictionary of locus tags to the name of their subsequence
-    :return: Dictionary mapping input locus tags to SubsequenceMetadata
-    '''
-    try:
-        genbank_file = open(filename, 'rb')
-    except IOError:
-        logger.warning("Unable to read file: " + filename)
-        return {}
-		
-    parent_genome_length = float('inf')
-    chunk_designation = None
-    potential_index_tag = ''
-    locus_tags = set(tags_to_names.keys())
-    tag_to_index = {}
-    
-    for line in genbank_file:
-        # Split on >1 space
-        split_line = re.split(r'\s{2,}', line.decode('utf-8').strip())
         
-        if len(split_line) == 2:
-            potential_index_tag = ''
-            chunk_designation = split_line[0]
-            if chunk_designation == 'source':
-                # Tag data begins with the full genome declaration as follows
-                # source    start_index..end_index
-                parent_genome_length = int(re.findall(r'[0-9]+', split_line[1])[1])
-            elif chunk_designation == 'gene':
-                potential_index_tag = split_line[1]
-            
-        if chunk_designation == 'gene':
-            if 'locus_tag' in split_line[-1]:
-                # Tags are declared by: /locus_tag=""
-                tag = split_line[-1].split('"')[1]
-                if tag in locus_tags:
-                    tag_index = parse_subsequence_tag(potential_index_tag, tags_to_names[tag], parent_genome_length)
-                    if tag_index is not None:
-                        tag_to_index[tag] = tag_index
-                    
-    genbank_file.close()
-    return tag_to_index
-    
-
-class StrainInfo(dict):
-    def __getattr__(self, key):
-        return self[key]
- 
-    def __setattr__(self, key, value):
-        if key not in ['name', 'accession', 'tags']:
-            logger.error('Invalid attribute name in refs JSON')
-            raise KeyError
-        self[lower(key)] = value
-
-    def validate(self):
-        if 'name' not in self.keys() or 'accession' not in self.keys() or 'tags' not in self.keys():
-            logger.error('Missing name, accession, or tags for entry in refs JSON: ' + str(self))
-            class StrainInfoIncompleteError(Exception):
-                pass
-            raise StrainInfoIncompleteError
-
-
-@dataclass
-class SubsequenceMetadata:
-    name: str
-    start_index: int
-    end_index: int
-    complement: bool # Indicates whether the intended sequence is the complement of the given sequence
-    
-    def get_subsequence(self, genome: str):
-        complement_translation = {'A': 'T', 'T':'A', 'G':'C', 'C':'G'}
-        complement_func = (lambda base: complement_translation[base]) if self.complement else lambda base: base
-        subsequence = ''
-        for base in genome[self.start_index:self.end_index]:
-            subsequence += complement_func(base)
-        return subsequence
-        
-
