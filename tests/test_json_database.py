9f9c4b6729373cc2b6a3d336dd764df1e757cd38
Youn <younhun@mit.edu>
2021-03-05 16:12:29 -0500

    Added basic unit test for JSON database parsing.

---
+++ tests/test_json_database.py
@@ -0,0 +1,99 @@
+from chronostrain.database.json import *
+
+
+def test_deserialization():
+    json_dict = {
+        "name": "test_name",
+        "accession": "test_accession",
+        "markers": [
+            {"name": "MARKER_1", "type": "primer", "forward": "FORWARD_PRIMER", "reverse": "REVERSE_PRIMER"},
+            {"name": "MARKER_2", "type": "tag", "locus_id": "TEST_LOCUS_ID"}
+        ]
+    }
+
+    strain_entry = StrainEntry.deserialize(json_dict, 0)
+    assert strain_entry.index == 0
+    assert strain_entry.name == json_dict["name"]
+    assert strain_entry.accession == json_dict["accession"]
+    assert len(strain_entry.marker_entries) == 2
+
+    marker_entry = strain_entry.marker_entries[0]
+    marker_entry_json = json_dict["markers"][0]
+    assert isinstance(marker_entry, PrimerMarkerEntry)
+    assert marker_entry.index == 0
+    assert marker_entry.name == marker_entry_json["name"]
+    assert marker_entry.forward == marker_entry_json["forward"]
+    assert marker_entry.reverse == marker_entry_json["reverse"]
+    assert marker_entry.parent == strain_entry
+
+    marker_entry = strain_entry.marker_entries[1]
+    marker_entry_json = json_dict["markers"][1]
+    assert marker_entry.index == 1
+    assert isinstance(marker_entry, TagMarkerEntry)
+    assert marker_entry.name == marker_entry_json["name"]
+    assert marker_entry.locus_id == marker_entry_json["locus_id"]
+    assert marker_entry.parent == strain_entry
+
+
+def build_regex_test_case(forward_primer, reverse_primer, genome) -> SubsequenceLoader:
+    json_dict = {
+        "name": "test_name",
+        "accession": "test_accession",
+        "markers": [
+            {"name": "MARKER_1", "type": "primer", "forward": forward_primer, "reverse": reverse_primer}
+        ]
+    }
+    strain_entry = StrainEntry.deserialize(json_dict, 0)
+
+    seq_loader = SubsequenceLoader(
+        fasta_filename="n/a",
+        genbank_filename="n/a",
+        marker_entries=strain_entry.marker_entries,
+        marker_max_len=500
+    )
+    seq_loader.full_genome = genome
+    return seq_loader
+
+
+def test_regex():
+    genome = "random_string_1_AACCGGTT_test_string_GATATATA_random_string_2"
+    forward_primer = "AACCGGTT"
+    reverse_primer = "TATATATC"
+    seq_loader = build_regex_test_case(forward_primer, reverse_primer, genome)
+    result = seq_loader._regex_match_primers(forward_primer, reverse_primer)
+    assert result == (16, 45)
+
+    genome = "random_string_1_AACCGGTT_test_string_GATATATA_random_string_2_GATATATA"
+    forward_primer = "AACCGGTT"
+    reverse_primer = "TATATATC"
+    seq_loader = build_regex_test_case(forward_primer, reverse_primer, genome)
+    result = seq_loader._regex_match_primers(forward_primer, reverse_primer)
+    assert result == (16, 45)
+
+    genome = "AACCGGTT_random_string_1_AACCGGTT_test_string_GATATATA_random_string_2_GATATATA"
+    forward_primer = "AACCGGTT"
+    reverse_primer = "TATATATC"
+    seq_loader = build_regex_test_case(forward_primer, reverse_primer, genome)
+    result = seq_loader._regex_match_primers(forward_primer, reverse_primer)
+    assert result == (25, 54)
+
+    genome = "random_string_1_AACCGGTT_test_string_GATATATA_random_string_2"
+    forward_primer = "RRCCGGTT"
+    reverse_primer = "TATATATC"
+    seq_loader = build_regex_test_case(forward_primer, reverse_primer, genome)
+    result = seq_loader._regex_match_primers(forward_primer, reverse_primer)
+    assert result == (16, 45)
+
+    genome = "random_string_1_AACCGGTT_test_string_GATATATA_random_string_2_GATATATA"
+    forward_primer = "RRCCGGTT"
+    reverse_primer = "TATATAYY"
+    seq_loader = build_regex_test_case(forward_primer, reverse_primer, genome)
+    result = seq_loader._regex_match_primers(forward_primer, reverse_primer)
+    assert result == (16, 45)
+
+    genome = "AACCGGTT_random_string_1_AACCGGTT_test_string_GATATATA_random_string_2_GATATATA"
+    forward_primer = "RRCCGGTT"
+    reverse_primer = "TATATAYY"
+    seq_loader = build_regex_test_case(forward_primer, reverse_primer, genome)
+    result = seq_loader._regex_match_primers(forward_primer, reverse_primer)
+    assert result == (25, 54)
