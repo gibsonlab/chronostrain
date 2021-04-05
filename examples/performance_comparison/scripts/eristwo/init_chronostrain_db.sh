0e71cba856d4b241b990985f9cc76ae093bb8c2a
Youn <younhun@mit.edu>
2021-04-05 19:30:58 -0400

    Updated helper/ directory change.

--- examples/performance_comparison/scripts/eristwo/init_chronostrain_db.sh
+++ examples/performance_comparison/scripts/eristwo/init_chronostrain_db.sh
@@ -5,4 +5,4 @@
 source settings.sh
 CHRONOSTRAIN_LOG_FILEPATH=LOG_FILEPATH="${CHRONOSTRAIN_DATA_DIR}/logs/db_init/db_init.log"
 export CHRONOSTRAIN_LOG_FILEPATH
-python $BASE_DIR/scripts/initialize_chronostrain_database.py
+python $BASE_DIR/scripts/helpers/initialize_chronostrain_database.py
