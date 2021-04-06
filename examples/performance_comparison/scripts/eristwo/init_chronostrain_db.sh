1e2ec8c4acccab766fe2560b6c4893ae55dbc48d
Youn <younhun@mit.edu>
2021-04-05 20:21:50 -0400

    fixed log filepath.

--- examples/performance_comparison/scripts/eristwo/init_chronostrain_db.sh
+++ examples/performance_comparison/scripts/eristwo/init_chronostrain_db.sh
@@ -3,6 +3,6 @@
 # Database initialization. (pre-download fasta and markers.)
 
 source settings.sh
-CHRONOSTRAIN_LOG_FILEPATH=LOG_FILEPATH="${CHRONOSTRAIN_DATA_DIR}/logs/db_init/db_init.log"
+CHRONOSTRAIN_LOG_FILEPATH="${CHRONOSTRAIN_DATA_DIR}/logs/db_init/db_init.log"
 export CHRONOSTRAIN_LOG_FILEPATH
 python $BASE_DIR/scripts/helpers/initialize_chronostrain_database.py
