7c3c7b28448510ae10ffafc5c65554945fa7514c
Youn <younhun@mit.edu>
2021-04-05 20:24:11 -0400

    fixed circular import.

--- chronostrain/config/chronostrain_config.py
+++ chronostrain/config/chronostrain_config.py
@@ -52,7 +52,7 @@ class DatabaseConfig(AbstractConfig):
 
         return class_name, kwargs, datadir
 
-    def get_database(self, force_refresh: bool = False) -> chronostrain.database.AbstractStrainDatabase:
+    def get_database(self, force_refresh: bool = False):
         module_name, class_name = self.class_name.rsplit(".", 1)
         class_ = getattr(importlib.import_module(module_name), class_name)
         db_kwargs = self.kwargs.copy()
