Numerical fixes and readgen seeding. - readgen.py now samples a random multinomial read count profile (instead of a deterministic frequency proportional to relative abund) - Fixed some numerical issues with bbvi.py. - Fixed numerical instability with BasicFastQErrorModel's log_likelihood calculation.

chronostrain/algs/bbvi.py
@@ -251,7 +251,8 @@ class BBVISolver(AbstractModelSolver):
                  model: GenerativeModel,
                  data: TimeSeriesReads,
                  cache_tag: CacheTag,
-                 correlation_type: str = "time"):
+                 correlation_type: str = "time",
+                 read_likelihood_numerical_thresh: float = 1e-15):
         super().__init__(model, data, cache_tag)
         self.correlation_type = correlation_type
         if correlation_type == "time":
@@ -269,10 +270,11 @@ class BBVISolver(AbstractModelSolver):
         for t_idx, read_likelihood_matrix in enumerate(self.read_likelihoods):
             sums = read_likelihood_matrix.sum(dim=0)
 
-            zero_indices = {i.item() for i in torch.where(sums == 0)[0]}
+            zero_indices = {i.item() for i in torch.where(sums <= read_likelihood_numerical_thresh)[0]}
             if len(zero_indices) > 0:
-                logger.warn("[t = {}] Discarding reads with overall likelihood zero: {}".format(
+                logger.warn("[t = {}] Discarding reads with overall likelihood < {}: {}".format(
                     self.model.times[t_idx],
+                    read_likelihood_numerical_thresh,
                     ",".join([str(read_idx) for read_idx in zero_indices])
                 ))
 
@@ -336,10 +338,6 @@ class BBVISolver(AbstractModelSolver):
                 self.fragment_posterior.phi[t_idx].sum(dim=1)  # length F
             )  # length N
 
-            # leftover_reads = 100000
-            # expectation_model_log_fragment_probs += model_frag_likelihoods_t.mv(
-            #     (leftover_reads / self.model.get_fragment_space().size()) * torch.ones(size=(self.model.get_fragment_space().size(),))
-            # )  # length N
         elbo_samples = (model_gaussian_log_likelihoods
                         + expectation_model_log_fragment_probs
                         - posterior_gaussian_log_likelihoods)

chronostrain/model/io/reads.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from typing import List, Optional
-import torch
+import numpy as np
 
 from Bio import SeqIO
 from Bio.Seq import Seq
@@ -47,7 +47,7 @@ class TimeSliceReads(object):
     def load(file_path: Path, time_point: float):
         reads = []
         for record in SeqIO.parse(file_path, "fastq"):
-            quality = torch.tensor(record.letter_annotations["phred_quality"], dtype=torch.int)
+            quality = np.array(record.letter_annotations["phred_quality"], dtype=np.int)
             read = SequenceRead(
                 read_id=record.id,
                 seq=str(record.seq),

chronostrain/model/reads/fastq_basic.py
@@ -36,7 +36,7 @@ class BasicFastQErrorModel(AbstractErrorModel):
         # NOTE: Ignore quality score distributions (assume negligible/constant likelihood for all q-score vectors.)
         # This only uses phred scores to compute Pr(Read | Fragment, Quality).
         error_prob = self.phred_error_prob(read.quality)
-        matches: np.ndarray = (fragment.seq == read.seq)
+        matches: np.ndarray = (fragment.seq == read.seq) & (read.quality > 0)
 
         p_matches = 1 - error_prob[np.where(matches)]
         p_errors = (1/3) * error_prob[np.where(~matches)]

scripts/readgen.py
@@ -12,6 +12,7 @@ from chronostrain.database import AbstractStrainDatabase
 from chronostrain.util.external.art import art_illumina
 
 from random import seed, randint
+import numpy as np
 
 
 def parse_args():
@@ -64,6 +65,19 @@ class Seed(object):
         return r
 
 
+def sample_read_counts(n_reads: int, rel_abund: Dict[str, float]) -> Dict[str, int]:
+    """
+    :param rel_abund: A dictionary mapping strain IDs to its relative abundance fraction.
+    :return: A dictionary mapping strain IDS to read counts, sampled as a multinomial.
+    """
+    strains = list(rel_abund.keys())
+    counts = np.random.multinomial(n=n_reads, pvals=[rel_abund[strain] for strain in strains])
+    return {
+        strains[i]: counts[i]
+        for i in range(len(strains))
+    }
+
+
 def main():
     args = parse_args()
     strain_db = cfg.database_cfg.get_database()
@@ -74,14 +88,19 @@ def main():
     timepoint_indexed_files = []
     seed = Seed(args.seed)
     for t, abundance_t in parse_abundance_profile(args.abundance_path):
+        # Sample a random multinomial profile.
+        np.random.seed(seed.next_value())
+        read_counts_t = sample_read_counts(args.num_reads, abundance_t)
+
+        # Generate the read path.
         out_path_t = out_dir / "reads_{t}.fastq".format(t=t)
         tmpdir = out_dir / "tmp_{t}".format(t=t)
         Path(tmpdir).mkdir(parents=True, exist_ok=True)
 
+        # Invoke art sampler on each time point.
         sample_reads_from_rel_abundances(
             final_reads_path=out_path_t,
-            abundances=abundance_t,
-            num_reads=args.num_reads,
+            abundances=read_counts_t,
             strain_db=strain_db,
             tmp_dir=tmpdir,
             profile_first=args.profiles[0],
@@ -132,7 +151,6 @@ def parse_abundance_profile(abundance_path: str) -> List[Tuple[float, Dict]]:
 
 def sample_reads_from_rel_abundances(final_reads_path: Path,
                                      abundances: Dict[str, float],
-                                     num_reads: int,
                                      strain_db: AbstractStrainDatabase,
                                      tmp_dir: Path,
                                      profile_first: Path,
@@ -164,12 +182,12 @@ def sample_reads_from_rel_abundances(final_reads_path: Path,
     """
     if n_cores == 1:
         strain_read_paths = []
-        for entry_index, (accession, rel_abund) in enumerate(abundances.items()):
+        for entry_index, (accession, read_count) in enumerate(abundances.items()):
             strain = strain_db.get_strain(strain_id=accession)
 
             output_path = art_illumina(
                 reference_path=strain.metadata.file_path,
-                num_reads=int(rel_abund * num_reads),
+                num_reads=read_count,
                 output_dir=tmp_dir,
                 output_prefix="{}_".format(accession),
                 profile_first=profile_first,
@@ -184,14 +202,14 @@ def sample_reads_from_rel_abundances(final_reads_path: Path,
     elif n_cores > 1:
         configs = [(
             strain_db.get_strain(accession).metadata.file_path,
-            int(rel_abund * num_reads),
+            read_count,
             tmp_dir,
             "{}_".format(accession),
             profile_first,
             profile_second,
             read_len,
             seed.next_value()
-        ) for entry_index, (accession, rel_abund) in enumerate(abundances.items())]
+        ) for entry_index, (accession, read_count) in enumerate(abundances.items())]
 
         thread_pool = Pool(n_cores)
         strain_read_paths = thread_pool.starmap(art_illumina, configs)