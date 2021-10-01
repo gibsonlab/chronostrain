7e00fc328162bc77bef7d5aa224c7a47d7f87b72
Younhun Kim <younhun@mit.edu>
2021-09-30 23:22:58 -0400

    Added type annotation to quality.py.

--- chronostrain/util/quality.py
+++ chronostrain/util/quality.py
@@ -1,21 +1,23 @@
+from typing import List
+
 import numpy as np
 from Bio.SeqIO.QualityIO import phred_quality_from_solexa
 
 
-def _parse_ascii_using_offset(qstr: str, offset: int):
+def _parse_ascii_using_offset(qstr: str, offset: int) -> List[int]:
     """
     Parse ASCII code into ints using `ord`, minus the specified offset.
     Mirrors the implementation found in Bio.SeqIO.QualityIO.
     """
-    return np.array([ord(letter) - offset for letter in qstr], dtype=float)
+    return [ord(letter) - offset for letter in qstr]
 
 
 # === Offsets: ord('@') = 64, ord('!') = 33.
-def _phred_fastq_solexa(qstr):
+def _phred_fastq_solexa(qstr) -> List[float]:
     return [phred_quality_from_solexa(q) for q in _parse_ascii_using_offset(qstr, ord("@"))]
 
 
-def _phred_fastq_illumina(qstr):
+def _phred_fastq_illumina(qstr) -> List[int]:
     """
     Corresponds to the 'fastq-illumina' option in Bio.SeqIO.QualityIO.
     As stated in the Bio.SeqIO.QualityIO documentation, this is for newer Illumina 1.3-1.7 FASTQ files.
@@ -23,21 +25,21 @@ def _phred_fastq_illumina(qstr):
     return _parse_ascii_using_offset(qstr, ord("@"))
 
 
-def _phred_fastq_sanger(qstr):
+def _phred_fastq_sanger(qstr) -> List[int]:
     """
     Sanger-style phred scores, also used by Illumina 1.8+ (according to Bio.SeqIO.QualityIO docs).
     """
     return _parse_ascii_using_offset(qstr, ord('!'))
 
 
-def ascii_to_phred(qstr: str, quality_format: str):
+def ascii_to_phred(qstr: str, quality_format: str) -> np.array:
     if quality_format == 'fastq':
-        return _phred_fastq_sanger(qstr)
+        return np.array(_phred_fastq_sanger(qstr), dtype=float)
     elif quality_format == 'fastq-sanger':
-        return _phred_fastq_sanger(qstr)
+        return np.array(_phred_fastq_sanger(qstr), dtype=float)
     elif quality_format == 'fastq-solexa':
-        return _phred_fastq_solexa(qstr)
+        return np.array(_phred_fastq_solexa(qstr), dtype=float)
     elif quality_format == 'fastq-illumina':
-        return _phred_fastq_illumina(qstr)
+        return np.array(_phred_fastq_illumina(qstr), dtype=float)
     else:
         raise ValueError("Unknown quality_format input `{}`.".format(quality_format))
