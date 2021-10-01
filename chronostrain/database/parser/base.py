1884645aa2655a9d786ddbef6cf427895a26844a
Younhun Kim <younhun@mit.edu>
2021-09-30 23:28:18 -0400

    Fixed typo in phred.py.

--- chronostrain/database/parser/base.py
+++ chronostrain/database/parser/base.py
@@ -1,8 +1,6 @@
 from abc import abstractmethod
-from pathlib import Path
 from typing import Iterator
 
-from chronostrain import cfg
 from chronostrain.model import Strain
 
 
--- chronostrain/model/reads/phred.py
+++ chronostrain/model/reads/phred.py
@@ -35,7 +35,7 @@ class PhredErrorModel(AbstractErrorModel):
         error_log10_prob = -0.1 * read.quality
         matches: np.ndarray = (fragment.seq == read.seq) & (read.quality > 0)
         # TODO: check whether this filters out `N`s.
-        mismatches: np.ndarray = (fragment.seq != read.seq) & (read.quality < 0)
+        mismatches: np.ndarray = (fragment.seq != read.seq) & (read.quality > 0)
 
         """
         Phred model: Pr(measured base = 'A', true base = 'G' | q) = ( 1/3 * 10^{-q/10} )
