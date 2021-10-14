ed94269b4a97179dd96d19ca8f1c4bbfb426b539
Youn <younhun@mit.edu>
2021-10-14 11:12:53 -0400

    Updated filter.sh in umb/

--- examples/umb/scripts/filter.sh
+++ examples/umb/scripts/filter.sh
@@ -7,6 +7,7 @@ SEED=31415
 
 # =========== Read filtering. ===============
 echo "Filtering reads."
-python ${PROJECT_DIR}/scripts/filter.py \
+cd $PROJECT_DIR/scripts
+python filter.py \
 -r "${READS_DIR}" \
 -o "${READS_DIR}/filtered"
