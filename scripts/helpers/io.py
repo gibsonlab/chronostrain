0eb27f25b4a85e8c8a5715e46a4fcfb49916e488
Youn <younhun@mit.edu>
2021-10-14 10:55:17 -0400

    Changed the way input.csv is parsed.

--- scripts/helpers/io.py
+++ scripts/helpers/io.py
@@ -1,40 +1,42 @@
 import csv
 from pathlib import Path
-from typing import Tuple, List, Iterable
+from typing import Dict, Tuple, List, Iterable
 
 
 def get_input_paths(base_dir: Path, input_filename) -> Tuple[List[Iterable[Path]], List[int], List[float]]:
-    time_points: List[float] = []
-    read_depths: List[int] = []
-    read_paths: List[List[Path]] = []
+    time_points_to_reads: Dict[float, List[Tuple[int, Path]]] = {}
 
     input_specification_path = base_dir / input_filename
     try:
         with open(input_specification_path, "r") as f:
             input_specs = csv.reader(f, delimiter=',', quotechar='"')
             for row in input_specs:
-                time_point_str = row[0]
-                read_depth_str = row[1]
-                filenames = [base_dir / f for f in row[2:]]
+                time_point = float(row[0])
+                num_reads = int(row[1])
+                read_path = Path(row[2])
 
-                time_points.append(float(time_point_str))
-                read_depths.append(int(read_depth_str))
-                read_paths.append(filenames)
+                if not read_path.exists():
+                    raise FileNotFoundError(f"Specified input file `{str(read_path)}` does not exist.")
+
+                if time_point not in time_points_to_reads:
+                    time_points_to_reads[time_point] = []
+
+                time_points_to_reads[time_point].append((num_reads, read_path))
     except FileNotFoundError:
-        raise FileNotFoundError("Missing required file `input_files.csv` in directory {}.".format(base_dir)) from None
+        raise FileNotFoundError(f"Missing required file `{input_filename}` in directory {base_dir}.") from None
 
-    if len(read_paths) != len(time_points):
-        raise ValueError("There must be exactly one set of reads for each time point specified.")
-
-    if len(read_depths) != len(time_points):
-        raise ValueError("Input specification must include a read depth for each time point.")
-
-    if len(time_points) != len(set(time_points)):
-        raise ValueError("Specified sample times must be distinct.")
-
-    for read_paths_t in read_paths:
-        for f in read_paths_t:
-            if not f.exists():
-                raise FileNotFoundError("Specified input file `{}` does not exist.".format(str(f)))
+    time_points = sorted(time_points_to_reads.keys(), reverse=False)
+    read_depths = [
+        sum([
+            n_reads for n_reads, _ in time_points_to_reads[t]
+        ])
+        for t in time_points
+    ]
+    read_paths = [
+        [
+            read_path for _, read_path in time_points_to_reads[t]
+        ]
+        for t in time_points
+    ]
 
     return read_paths, read_depths, time_points
