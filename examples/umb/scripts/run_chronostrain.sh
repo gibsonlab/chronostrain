8e7cda5d883725a3907dddaced92dcba1582f3e8
Younhun Kim <younhun@mit.edu>
2021-10-29 13:57:11 -0400

    Updated examples/umb/scripts/run_chronostrain.sh to remove the read_len argument.

--- examples/umb/scripts/run_chronostrain.sh
+++ examples/umb/scripts/run_chronostrain.sh
@@ -11,7 +11,6 @@ echo "Running inference."
 cd $PROJECT_DIR/scripts
 python inference_with_variant_search.py \
 --reads_dir "${READS_DIR}/filtered" \
---read_length $READ_LEN \
 --out_dir $CHRONOSTRAIN_OUTPUT_DIR \
 --quality_format "fastq" \
 --input_file "input_files.csv" \
