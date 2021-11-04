Various fixes for numerical precision. This update fixes "-inf" appearing in data likelihoods/BBVI. - Added epsilon-smoothing to bbvi's update_phi implementation, which helps with division-by-zeros when renormalizing phi. - Added a simple, jit-enabled logmatmulexp() implementation for numerically-stable version of logsumexp on matrix multiplication. - Got rid of read trimming in Datalikelihoods class. (updated bbvi_visualizations accordingly to not depend on this). - SparseMatrix's to_dense() method now has an optional mode where the empty entries are filled with a specified value.

chronostrain/algs/inference/bbvi.py
@@ -488,41 +488,45 @@ class BBVISolver(AbstractModelSolver):
                         - posterior_gaussian_log_likelihoods)
         return elbo_samples.mean()
 
-    def update_phi(self, x_samples: torch.Tensor):
+    def update_phi(self, x_samples: torch.Tensor, eps_smoothing: float = 1e-30):
         if cfg.model_cfg.use_sparse:
-            return self.update_phi_sparse(x_samples)
+            return self.update_phi_sparse(x_samples, eps_smoothing)
         else:
-            return self.update_phi_dense(x_samples)
+            return self.update_phi_dense(x_samples, eps_smoothing)
 
-    def update_phi_dense(self, x_samples: torch.Tensor):
+    def update_phi_dense(self, x_samples: torch.Tensor, eps_smoothing: float):
         """
         This step represents the explicit solution of maximizing the ELBO of Q_phi (the mean-field portion of
         the read-to-fragment posteriors), given a particular solution of (samples from) Q_X.
         :param x_samples:
+        :param eps_smoothing: A small epsilon parameter to smooth out estimates (e.g. normalize eps + phi,
+            instead of phi which may contain columns of all zeros due to numerical precision.)
         :return:
         """
         W = self.model.fragment_frequencies
         self.fragment_posterior.phi = []
 
         for t in range(self.model.num_times()):
-            phi_t = self.data_likelihoods.matrices[t] * torch.exp(
+            phi_t = self.data_likelihoods.matrices[t].exp() * torch.exp(
                 torch.mean(
                     torch.log(W @ softmax(x_samples[t], dim=1).transpose(0, 1)),
                     dim=1
                 )
-            ).unsqueeze(1)
+            ).unsqueeze(1) + eps_smoothing
             self.fragment_posterior.phi.append(normalize(phi_t, dim=0))
 
-    def update_phi_sparse(self, x_samples: torch.Tensor):
+    def update_phi_sparse(self, x_samples: torch.Tensor, eps_smoothing: float):
         """
         Same as update_phi_dense, but accounts for the fact that the W and read_likelihoods[t] matrices are sparse.
         :param x_samples:
+        :param eps_smoothing: A small epsilon parameter to smooth out estimates (e.g. normalize eps + phi,
+            instead of phi which may contain columns of all zeros due to numerical precision.)
         :return:
         """
         self.fragment_posterior.phi = []
 
         for t in range(self.model.num_times()):
-            phi_t: SparseMatrix = self.data_likelihoods.matrices[t].scale_row(
+            phi_t: SparseMatrix = self.data_likelihoods.matrices[t].exp().scale_row(
                 torch.exp(torch.mean(
                     torch.log(
                         # (F' x S) @ (S x N)
@@ -533,7 +537,7 @@ class BBVISolver(AbstractModelSolver):
                 dim=0
             )
 
-            self.fragment_posterior.phi.append(phi_t.normalize(dim=0))
+            self.fragment_posterior.phi.append(phi_t.add_constant(eps_smoothing).normalize(dim=0))
 
     def solve(self,
               optim_class=torch.optim.Adam,

chronostrain/algs/subroutines/likelihoods/base.py
@@ -32,62 +32,12 @@ class DataLikelihoods(object):
 
         log_likelihoods_tensors = self._likelihood_computer().compute_likelihood_tensors()
         self.matrices = [
-            ll_tensor.exp() for ll_tensor in log_likelihoods_tensors
-        ]
-
-        # TODO: remove this. Filtering by alignment now solves this issue.
-        # self.retained_indices = self._trim()
-        self.retained_indices = [
-            list(range(len(self.data[t_idx])))
-            for t_idx in range(self.model.num_times())
+            ll_tensor for ll_tensor in log_likelihoods_tensors
         ]
 
     @abstractmethod
     def _likelihood_computer(self) -> 'AbstractLogLikelihoodComputer':
         raise NotImplementedError()
-
-    def _trim(self) -> List[List[int]]:
-        """
-        Trims the likelihood matrices using the specified lower bound. Reads are removed if there are no fragments
-        with likelihood greater than the lower bound. (This should ideally not happen if a stringent alignment-based
-        filter was applied.)
-
-        :return: List of the index of kept reads (exceeding the lower bound threshold).
-        """
-        read_indices = []
-        for t_idx in range(self.model.num_times()):
-            read_likelihoods_t = self.matrices[t_idx]
-            sums = read_likelihoods_t.sum(dim=0)
-
-            zero_indices = {i.item() for i in torch.where(sums <= self.read_likelihood_lower_bound)[0]}
-            if len(zero_indices) > 0:
-                logger.debug(
-                    "[t = {}] For numerical stability, "
-                    "discarding {} of {} reads with overall likelihood < {}: {}".format(
-                        self.model.times[t_idx],
-                        len(zero_indices),
-                        len(sums),
-                        self.read_likelihood_lower_bound,
-                        ",".join(str(self.data[t_idx][read_idx].id) for read_idx in zero_indices)
-                    )
-                )
-
-                leftover_indices = [
-                    read_idx
-                    for read_idx in range(len(self.data[t_idx]))
-                    if read_idx not in zero_indices
-                ]
-                read_indices.append(leftover_indices)
-
-                if isinstance(read_likelihoods_t, SparseMatrix):
-                    self.matrices[t_idx] = read_likelihoods_t.slice_columns(
-                        leftover_indices
-                    )
-                else:
-                    self.matrices[t_idx] = read_likelihoods_t[:, leftover_indices]
-            else:
-                read_indices.append(list(range(len(self.data[t_idx]))))
-        return read_indices
 
     @abstractmethod
     def conditional_likelihood(self, X: torch.Tensor) -> float:

chronostrain/algs/subroutines/likelihoods/dense.py
@@ -7,6 +7,7 @@ from chronostrain.model import Fragment, SequenceRead
 from chronostrain.model.io import TimeSeriesReads
 from chronostrain.config import cfg
 from chronostrain.model.generative import GenerativeModel
+from chronostrain.util.math import logmatmulexp
 
 from .base import DataLikelihoods, AbstractLogLikelihoodComputer
 from ..cache import ReadsPopulationCache
@@ -25,7 +26,14 @@ class DenseDataLikelihoods(DataLikelihoods):
         total_ll = 0.
         for t in range(self.model.num_times()):
             # (T x S) * (S x F) * (F x N)
-            total_ll += torch.log(y[t] @ (self.model.fragment_frequencies.t() @ self.matrices[t])).sum()
+            # total_ll += torch.log(y[t] @ (self.model.fragment_frequencies.t() @ self.matrices[t].exp())).sum()
+            total_ll += logmatmulexp(
+                y[t].log().view(1, -1),
+                logmatmulexp(
+                    self.model.fragment_frequencies.t().log(),
+                    self.matrices[t]
+                )
+            ).sum()
         return total_ll
 
 

chronostrain/algs/subroutines/likelihoods/sparse.py
@@ -8,6 +8,7 @@ from joblib import Parallel, delayed
 from chronostrain.database import StrainDatabase
 from chronostrain.model import Fragment, Marker, SequenceRead, AbstractMarkerVariant
 from chronostrain.util.alignments.multiple import MarkerMultipleFragmentAlignment
+from chronostrain.util.math import logmatmulexp
 from chronostrain.util.sequences import SeqType
 from chronostrain.util.sparse import SparseMatrix
 from chronostrain.model.io import TimeSeriesReads
@@ -68,19 +69,16 @@ class SparseDataLikelihoods(DataLikelihoods):
         y = torch.softmax(X, dim=1)
         total_ll = 0.
         for t_idx in range(self.model.num_times()):
-            log_likelihood_t = torch.mm(  # result is (T x N)
-                y[t_idx].view(1, -1),  # (T x S)
-                self.model.fragment_frequencies.t().sparse_mul(
-                    self.projectors[t_idx].t()
-                ).sparse_mul(
-                    self.matrices[t_idx]
-                ).to_dense()
-                # (S x F) x (F x F') x (F' x F) x (F x N) -> (S x N)
-            ).log().sum()
+            log_likelihood_t = logmatmulexp(
+                y[t_idx].log().view(1, -1),
+                logmatmulexp(
+                    self.model.fragment_frequencies.t().sparse_mul(self.projectors[t_idx].t()).log().to_dense(float("-inf")),
+                    self.matrices[t_idx].to_dense(float("-inf"))
+                )  # TODO: replace this with a direct logmatmulexp_sparse implementation.
+            )
 
+            log_likelihood_t = log_likelihood_t.sum()
             total_ll += log_likelihood_t
-            # if True:
-            #     print("t_idx = {}, ll = {}".format(t_idx, log_likelihood_t))
         return total_ll
 
 
@@ -271,15 +269,6 @@ class SparseLogLikelihoodComputer(AbstractLogLikelihoodComputer):
                         for subseq, insertions, deletions in variant.subseq_from_read(read):
                             frag = self.model.fragments.get_fragment(subseq)
                             ll = self.read_frag_ll(frag, read, insertions, deletions, reverse_complemented=revcomp)
-                            # print("(T = {}) ({}) read={}, frag={}: {} ({} ins, {} dels)".format(
-                            #     t_idx,
-                            #     read.id,
-                            #     read.nucleotide_content(),
-                            #     frag.nucleotide_content(),
-                            #     ll,
-                            #     np.sum(insertions),
-                            #     np.sum(deletions)
-                            # ))
                             read_to_frag_likelihoods[read.id].append((frag, ll))
         return read_to_frag_likelihoods
 

chronostrain/util/sparse/sparse_tensor.py
@@ -4,7 +4,7 @@ A sparse 2-d matrix in COO format. Uses torch_sparse as a backend.
 Note: since torch_sparse largely already uses C-compiled computations or JIT whenever possible, calls using
 torch_sparse need not be optimized. For custom model-specific operations, we use @torch.jit ourselves.
 """
-from typing import Tuple, List
+from typing import Tuple, List, Union
 import torch
 import torch_sparse
 
@@ -74,6 +74,14 @@ class SparseMatrix(object):
         """
         return SparseMatrix(self.indices, torch.exp(self.values), (self.rows, self.columns), force_coalesce=False)
 
+    def log(self) -> 'SparseMatrix':
+        """
+        Component-wise logarithm, applie to only the non-empty entries.
+        Note: Be careful of what the empty values mean. If the empty values before the call to log() are meant to
+        be zeroes, then this is not consistent with the behavior in this implementation, since log(0) = -inf.
+        """
+        return SparseMatrix(self.indices, torch.log(self.values), (self.rows, self.columns), force_coalesce=False)
+
     def add(self, y: 'SparseMatrix') -> 'SparseMatrix':
         """
         :param y: The other matrix in the summand.
@@ -91,19 +99,28 @@ class SparseMatrix(object):
             force_coalesce=True
         )
 
+    def add_constant(self, x: Union[int, float], inplace: bool = True) -> 'SparseMatrix':
+        if inplace:
+            self.values = self.values + x
+            return self
+        else:
+            return SparseMatrix(self.indices, self.values + x, (self.columns, self.rows), force_coalesce=False)
+
     def t(self) -> 'SparseMatrix':
         result_indices, result_values = torch_sparse.transpose(self.indices, self.values, self.rows, self.columns)
-        return SparseMatrix(result_indices, result_values, (self.columns, self.rows))
+        return SparseMatrix(result_indices, result_values, (self.columns, self.rows), force_coalesce=False)
 
-    def to_dense(self) -> torch.Tensor:
-        return torch.sparse_coo_tensor(
-            self.indices,
-            self.values,
-            (self.rows, self.columns)
-        ).to_dense()
-
-    def sparse_scalar_sum(self, scalar: float) -> 'SparseMatrix':
-        return SparseMatrix(self.indices, self.values + scalar, (self.rows, self.columns))
+    def to_dense(self, zero_fill=0.) -> torch.Tensor:
+        if zero_fill == 0.:
+            return torch.sparse_coo_tensor(
+                self.indices,
+                self.values,
+                (self.rows, self.columns)
+            ).to_dense()
+        else:
+            x = torch.ones((self.rows, self.columns), dtype=self.values.dtype, device=self.values.device) * zero_fill
+            x[self.indices[0], self.indices[1]] = self.values
+            return x
 
     def scale_row(self, vec: torch.Tensor, dim: int = 0) -> 'SparseMatrix':
         """
@@ -123,7 +140,8 @@ class SparseMatrix(object):
         return SparseMatrix(
             self.indices,
             self.values * vec[self.indices[dim, :]],
-            (self.rows, self.columns)
+            (self.rows, self.columns),
+            force_coalesce=False
         )
 
     def sum(self, dim: int):
@@ -198,7 +216,7 @@ class SparseMatrix(object):
         """
         # Deleting entries
         cols_to_keep = torch.tensor(cols_to_keep, device=self.indices.device).unsqueeze(1)
-        mask = torch.sum(cols_to_keep == self.indices[1, :], dim=0).bool()
+        mask = torch.sum(torch.eq(cols_to_keep, self.indices[1, :]), dim=0).bool()
         indices = self.indices[:, mask]
         values = self.values[mask]
 
@@ -207,4 +225,4 @@ class SparseMatrix(object):
         adj = torch.cumsum(~range_mask, 0)
         indices[1, :] = indices[1, :] - torch.index_select(adj, 0, indices[1, :])
 
-        return SparseMatrix(indices, values, (self.rows, cols_to_keep.size(-2)))
+        return SparseMatrix(indices, values, (self.rows, cols_to_keep.size(-2)), force_coalesce=False)