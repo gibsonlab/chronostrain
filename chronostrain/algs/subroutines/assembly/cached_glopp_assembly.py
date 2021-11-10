Various fixes. - fixed glopp_with_corr precision matrix having a lower bound instead of a negative upper bound. - simplified per-strand counting, only uses # of reads per strand, unnormalized. (glopp_with_corr will standardize data). - Added a step in inference_with_variants_simple.py where it outputs the strain variants.

chronostrain/algs/subroutines/assembly/cached_glopp_assembly.py
@@ -53,6 +53,10 @@ class CachedGloppVariantAssembly(object):
         self.bam_path = self.absolute_dir / "alignments.bam"
         self.vcf_path = self.absolute_dir / "variants.vcf"
 
+    @property
+    def num_times(self) -> int:
+        return len(self.reads)
+
     def count_variants(self) -> np.ndarray:
         """
         :return: An (N x 5) array of variant counts.
@@ -152,7 +156,7 @@ class CachedGloppVariantAssembly(object):
 
     def parse_marker_contigs(self,
                              glopp_phasing_path: Path,
-                             glopp_partition_path: Path) -> FloppMarkerAssembly:
+                             glopp_partition_path: Path,) -> FloppMarkerAssembly:
         """
         :param glopp_phasing_path: The glopp phasing output file to parse.
         :param glopp_partition_path: The glopp partition output file to parse.
@@ -165,7 +169,6 @@ class CachedGloppVariantAssembly(object):
 
         contig_positions_arr: List[np.ndarray] = []  # contig-indexed list of contig-specific positions.
         contig_assemblies: List[np.ndarray] = []  # contig-indexed list of assembled sequences.
-        contig_counts_arr: List[np.ndarray] = []  # contig-indexed list of read counts per variant.
         contig_read_counts_arr: List[np.ndarray] = []  # contig-indexed list of read counts per strand.
 
         parsed_ploidy: int = -1
@@ -209,18 +212,18 @@ class CachedGloppVariantAssembly(object):
             abs_pos_idx = -1
             for contig_idx, contig_pos in enumerate(contig_positions_arr):
                 contig_assembly = np.empty(shape=(len(contig_pos), parsed_ploidy), dtype=NucleotideDtype)
-                contig_counts = np.zeros(shape=(len(contig_pos), parsed_ploidy, len(self.reads)), dtype=int)
-                read_counts = np.zeros(shape=parsed_ploidy, dtype=int)
+                read_counts = np.zeros(shape=(parsed_ploidy, self.num_times), dtype=int)
                 for contig_pos_idx, pos in enumerate(contig_pos):
                     hap_line = next(phasing_file)
                     abs_pos_idx += 1
                     tokens = hap_line.strip().split("\t")
-                    for k in range(parsed_ploidy):
-                        contig_assembly[contig_pos_idx, k] = allele_parser(abs_pos_idx, int(tokens[k + 1]))
+                    for strand_idx in range(parsed_ploidy):
+                        contig_assembly[contig_pos_idx, strand_idx] = allele_parser(
+                            abs_pos_idx, int(tokens[strand_idx + 1])
+                        )
                 intermediate_line = next(phasing_file)
                 assert intermediate_line.startswith("--") or intermediate_line.startswith("**")
                 contig_assemblies.append(contig_assembly)
-                contig_counts_arr.append(contig_counts)
                 contig_read_counts_arr.append(read_counts)
 
         # Parse the partition file: get the per-timepoint, per-base read counts.
@@ -255,8 +258,8 @@ class CachedGloppVariantAssembly(object):
                     read_last_pos = all_positions[int(last_pos_tok) - 1]
 
                     # For each contig, determine whether it overlaps ("hits") the read.
-                    for contig_positions, contig_assembly, contig_counts, read_counts \
-                            in zip(contig_positions_arr, contig_assemblies, contig_counts_arr, contig_read_counts_arr):
+                    for contig_positions, contig_assembly, read_counts \
+                            in zip(contig_positions_arr, contig_assemblies, contig_read_counts_arr):
                         # "Hit" = there exists some position on the contig that is between the read's first/last pos.
                         read_variants = read_full_seq[contig_positions]
                         assembly_variants = contig_assembly[:, strand_idx]
@@ -268,24 +271,21 @@ class CachedGloppVariantAssembly(object):
 
                         # increment overall read count for the strand.
                         if len(hit_positions) > 0:
-                            read_counts[strand_idx] += 1
-
-                        # increment per-position count.
-                        contig_counts[hit_positions, strand_idx, t_idx] += 1
+                            read_counts[strand_idx, t_idx] += 1
         return FloppMarkerAssembly(
             self.alignment,
             [
-                FloppMarkerContig(marker,
-                                  c_idx,
-                                  contig_pos,
-                                  contig_assembly,
-                                  contig_counts,
-                                  read_counts)
-                for c_idx, (contig_pos, contig_assembly, contig_counts, read_counts) in enumerate(
+                FloppMarkerContig(
+                    marker=marker,
+                    contig_idx=c_idx,
+                    positions=contig_pos,
+                    assembly=contig_assembly,
+                    read_counts=read_counts
+                )
+                for c_idx, (contig_pos, contig_assembly, read_counts) in enumerate(
                     zip(
                         contig_positions_arr,
                         contig_assemblies,
-                        contig_counts_arr,
                         contig_read_counts_arr
                     )
                 )

chronostrain/algs/subroutines/assembly/classes.py
@@ -29,12 +29,6 @@ class FloppMarkerAssembly(object):
     def num_contigs(self) -> int:
         return len(self.contigs)
 
-    def mean_counts(self) -> np.ndarray:
-        return np.concatenate([
-            contig.mean_counts
-            for contig in self.contigs
-        ], axis=0)
-
     def get_contig_of(self, concatenated_strand_index: int) -> Tuple['FloppMarkerContig', int]:
         contig_idx = np.where(concatenated_strand_index < self.cumulative_strand_counts)[0][0]
         if contig_idx == 0:
@@ -64,15 +58,13 @@ class FloppMarkerContig(object):
                  contig_idx: int,
                  positions: np.ndarray,
                  assembly: np.ndarray,
-                 counts: np.ndarray,
-                 num_reads_per_strand: np.ndarray):
+                 read_counts: np.ndarray):
         """
         :param marker: The marker that this object is representing.
         :param positions: The list of integer-valued positions that the assembly matrix's index represents.
             (Should be sorted in increasing order.)
         :param assembly: An (N x k) array of resolved assembly for this contig.
-        :param counts: An (N x k x T) array of marginal counts the number of reads mapped to the k-th "haplotype"
-            at position N at time t.
+        :param read_counts: An (k x T) array of read counts per each strand, per timepoint.
         """
         self.marker = marker
         self.positions = positions
@@ -82,25 +74,20 @@ class FloppMarkerContig(object):
         if len(positions) != assembly.shape[0]:
             raise ValueError("The number of specified positions must match the length of `assembly`.")
 
-        if assembly.shape[0] != counts.shape[0] or assembly.shape[1] != counts.shape[1]:
-            raise ValueError("The shape of `assembly` must match the first two dims of `counts`.")
         self.assembly = assembly
-        self.counts = counts
-        self.mean_counts = np.mean(self.counts, axis=0)  # (k x T).
-        self.num_reads_per_strand = num_reads_per_strand
+        self.read_counts = read_counts
 
         # Trim zero count strands.
-        num_zero_count_strands = np.sum(self.num_reads_per_strand == 0)
+        num_reads_per_strand = np.sum(read_counts, axis=1)
+        num_zero_count_strands = np.sum(num_reads_per_strand == 0)
         if num_zero_count_strands > 0:
             logger.debug(
                 f"{marker.id}, Contig {contig_idx} - Trimming {num_zero_count_strands} strands with zero count."
             )
 
-            support_indices = np.where(self.num_reads_per_strand > 0)[0]
-            self.mean_counts = self.mean_counts[support_indices, :]
+            support_indices = np.where(num_reads_per_strand > 0)[0]
             self.assembly = self.assembly[:, support_indices]
-            self.counts = self.counts[:, support_indices, :]
-            self.num_reads_per_strand = self.num_reads_per_strand[support_indices]
+            self.read_counts = self.read_counts[support_indices, :]
 
     @property
     def leftmost_pos(self) -> int:
@@ -122,4 +109,4 @@ class FloppMarkerContig(object):
         return self.assembly[:, idx]
 
     def num_reads_of_strand(self, strand_idx: int) -> int:
-        return self.num_reads_per_strand[strand_idx]
+        return self.read_counts[strand_idx, :].sum()

chronostrain/algs/variants/glopp_with_corr.py
@@ -5,7 +5,7 @@
 """
 import itertools
 from collections import defaultdict
-from typing import List, Tuple, Dict, Iterator, Union, Iterable
+from typing import List, Tuple, Dict, Iterator, Union, Iterable, Optional
 
 import numpy as np
 
@@ -29,11 +29,18 @@ class GloppContigStrandSpecification(object):
         self.contig = contig
         self.strand_idx = strand_idx
 
+    def __str__(self):
+        return self.__repr__()
 
-def upper_triangular_lower_bounded(x: np.ndarray,
-                                   lower_bound: float,
-                                   k: int = 0
-                                   ) -> Tuple[np.ndarray, np.ndarray]:
+    def __repr__(self):
+        return f"[Marker {self.contig.marker.id}, Contig {self.contig.contig_idx}, Strand {self.strand_idx}]"
+
+
+def upper_triangular_bounded(x: np.ndarray,
+                             lower_bound: float,
+                             upper_bound: float,
+                             k: int = 0
+                             ) -> Tuple[np.ndarray, np.ndarray]:
     """
     Computes the indices satisfying the condition x < upper_bound only on the upper triangular part of
     the provided array (with diagonal offset k).
@@ -43,7 +50,7 @@ def upper_triangular_lower_bounded(x: np.ndarray,
     """
     r, c = np.triu_indices_from(x, k=k)
     values = x[r, c]
-    valid_indices = np.where(values > lower_bound)
+    valid_indices = np.where((values < upper_bound) & (values > lower_bound))
     return r[valid_indices], c[valid_indices]
 
 
@@ -56,10 +63,11 @@ class GloppVariantSolver(AbstractVariantBBVISolver):
                  bbvi_lr: int,
                  bbvi_num_samples: int,
                  quality_lower_bound: float,
-                 glasso_shrinkage: float = 0.8,
+                 glasso_shrinkage: float = 0.1,
                  glasso_standardize: bool = True,
                  glasso_alpha: float = 1e-3,
-                 seed_with_database: bool = False):
+                 seed_with_database: bool = False,
+                 num_strands: Optional[int] = None):
         """
         :param glasso_standardize: Indicates whether to standardize each feature across samples.
         :param glasso_alpha: The `alpha` regularization parameter in the glasso algorithm.
@@ -78,6 +86,7 @@ class GloppVariantSolver(AbstractVariantBBVISolver):
         self.glasso_shrinkage = glasso_shrinkage
         self.glasso_standardize = glasso_standardize
         self.glasso_alpha = glasso_alpha
+        self.num_strands = num_strands
 
         self.reference_markers_to_assembly: Dict[Marker, FloppMarkerAssembly] = self.construct_marker_assemblies()
 
@@ -96,7 +105,7 @@ class GloppVariantSolver(AbstractVariantBBVISolver):
         for marker_multi_align in self.multi_alignments:
             marker_assembly: FloppMarkerAssembly = CachedGloppVariantAssembly(
                 self.reads, marker_multi_align, quality_lower_bound=self.quality_lower_bound
-            ).run(num_variants=None)  # TODO
+            ).run(num_variants=self.num_strands)
 
             marker_assemblies[marker_assembly.marker] = marker_assembly
         return marker_assemblies
@@ -109,7 +118,7 @@ class GloppVariantSolver(AbstractVariantBBVISolver):
     def propose_variants(self) -> Iterator[StrainVariant]:
         variants: List[FloppStrainVariant] = list(
             self.construct_variants_using_assembly(
-                precision_lower_bound=0.5
+                precision_upper_bound=-1
             )
         )
 
@@ -123,10 +132,13 @@ class GloppVariantSolver(AbstractVariantBBVISolver):
 
         yield from variants
 
-    def construct_variants_using_assembly(self, precision_lower_bound: float) -> Iterator[FloppStrainVariant]:
+    def construct_variants_using_assembly(self, precision_upper_bound: float) -> Iterator[FloppStrainVariant]:
         precision_matrix = self.compute_precision_matrix()
-        rows, cols = upper_triangular_lower_bounded(
-            precision_matrix, lower_bound=precision_lower_bound
+
+        rows, cols = upper_triangular_bounded(
+            precision_matrix,
+            upper_bound=precision_upper_bound,
+            lower_bound=-np.inf
         )
 
         G = nx.Graph()
@@ -136,6 +148,7 @@ class GloppVariantSolver(AbstractVariantBBVISolver):
             G.add_edge(v, w)
 
         for clique in nx.find_cliques(G):
+            print(clique)
             yield self.strain_variant_from_clique(
                 G,
                 clique,
@@ -145,8 +158,13 @@ class GloppVariantSolver(AbstractVariantBBVISolver):
         # TODO 2: Edit sparse.py (sparse likelihood calculation) to take these variants into account.
 
     def compute_precision_matrix(self) -> np.ndarray:
+        # marker_contig_counts = np.stack([
+        #     contig_spec.contig.mean_counts[contig_spec.strand_idx]
+        #     for contig_spec in self.strand_specs
+        # ], axis=1)  # (T x M)
+
         marker_contig_counts = np.stack([
-            contig_spec.contig.mean_counts[contig_spec.strand_idx]
+            contig_spec.contig.read_counts[contig_spec.strand_idx]
             for contig_spec in self.strand_specs
         ], axis=1)  # (T x M)
 
@@ -264,9 +282,9 @@ class GloppVariantSolver(AbstractVariantBBVISolver):
         variant_id = "{}:Strands({})".format(
             base_marker.id,
             "|".join(
-                "*" if isinstance(strand, str) else str(strand.strand_idx)
+                "*" if isinstance(strand, str) else f"{str(strand.strand_idx)}/{strand.contig.num_strands}"
                 for strand in strands
-            )
+            ),
         )
         logger.debug("Creating Marker Variant ({})".format(
             variant_id