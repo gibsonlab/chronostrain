9cb60e0402ca477c12f369bcd10382b99f43c9bf
Youn <younhun@mit.edu>
2021-12-03 11:53:03 -0500

    download_samples now gzips the downloaded files.

--- examples/umb/scripts/download_samples.sh
+++ examples/umb/scripts/download_samples.sh
@@ -49,6 +49,7 @@ mkdir -p ${FASTERQ_TMP_DIR}
 		fq_file_1="${SAMPLES_DIR}/${sra_id}_1.fastq"
 		fq_file_2="${SAMPLES_DIR}/${sra_id}_2.fastq"
 
+		echo "[*] Compressing..."
 		gzip $fq_file_1 --force
 		gzip $fq_file_2 --force
 	done
