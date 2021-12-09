cbc5370be19b49909103d75fdff06f7ad1a5c3bd
Youn <younhun@mit.edu>
2021-12-09 12:11:52 -0500

    Fixed typo.

--- examples/umb/scripts/init_chronostrain.sh
+++ examples/umb/scripts/init_chronostrain.sh
@@ -5,7 +5,7 @@ source settings.sh
 export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/init.log"
 
 
-${BASE_DIR}/helpers/create_db_from_uniref.py \
+python ${BASE_DIR}/helpers/create_db_from_uniref.py \
 -u ${BASE_DIR}/files/gene_info_uniref.csv \
 -s ${STRAINGE_STRAIN_LIST} \
 -o ${CHRONOSTRAIN_ECOLI_DB_SPEC}
