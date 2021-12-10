d400ba2bbe428ead92d39ec32df93bae1b1f7aeb
Youn <younhun@mit.edu>
2021-12-10 11:27:33 -0500

    Partial implementation of BLAST lookups for create_db_from_uniref.py.

--- examples/umb/helpers/create_db_from_uniref.py
+++ examples/umb/helpers/create_db_from_uniref.py
@@ -179,6 +179,7 @@ def download_reference(accession: str, gene_names: Set[str]) -> Dict[str, Path]:
 
             gene_out_path = data_dir / "REF_{accession}_{found_gene}.fasta"
             gene_seq = location.extract(chromosome_seq)
+            print(f"Target location: {str(location)}")
             SeqIO.write(
                 SeqRecord(gene_seq, id=f"REF_GENE_{found_gene}", description=f"{accession}_{str(location)}"),
                 gene_out_path,
