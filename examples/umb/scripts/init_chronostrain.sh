a561c9d757decd182e83e9220c39d5443c4925c9
Youn <younhun@mit.edu>
2021-12-10 11:25:11 -0500

    Partial implementation of BLAST lookups for create_db_from_uniref.py.

--- examples/umb/scripts/init_chronostrain.sh
+++ examples/umb/scripts/init_chronostrain.sh
@@ -9,4 +9,5 @@ python ${BASE_DIR}/helpers/create_db_from_uniref.py \
 -u ${BASE_DIR}/files/gene_info_uniref.csv \
 -s ${STRAINGE_STRAIN_LIST} \
 -o ${CHRONOSTRAIN_ECOLI_DB_SPEC} \
--sdb /mnt/d/strainge/strainge_db
+-sdb /mnt/d/strainge/strainge_db \
+-ref "CP009273.1"
