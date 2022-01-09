42cbd8a780d0fa6ded612c23833b73c2d4a8e08e
Youn <younhun@mit.edu>
2022-01-09 16:27:18 -0500

    edited run_chronostrain.sh to have individual patient logs.

--- examples/umb/scripts/run_chronostrain.sh
+++ examples/umb/scripts/run_chronostrain.sh
@@ -2,7 +2,6 @@
 set -e
 
 source settings.sh
-export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/chronostrain.log"
 SEED=31415
 
 # =========== Run chronostrain. ==================
@@ -11,6 +10,7 @@ echo "Running inference."
 for umb_id in UMB05 UMB08 UMB11 UMB12 UMB15 UMB18 UMB20 UMB23 UMB24
 do
 	index_filename="filtered_${umb_id}_${INPUT_INDEX_FILENAME}"
+	export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/chronostrain_${umb_id}.log"
 
 	cd $PROJECT_DIR/scripts
 	python run_bbvi.py \
