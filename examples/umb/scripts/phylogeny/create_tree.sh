4840e8e671fdc9fd217233c89abccd151534287d
Youn <younhun@mit.edu>
2022-01-21 11:27:41 -0500

    Minor fixes - added (currently unused) init_diag function in bbvi.py. - added phylogeny/ subdirectory in examples/umb/scripts.

--- examples/umb/scripts/phylogeny/create_tree.sh
+++ examples/umb/scripts/phylogeny/create_tree.sh
@@ -3,6 +3,7 @@ set -e
 source ../settings.sh
 export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/create_tree.log"
 
+mkdir ${PHYLOGENY_OUTPUT_DIR}/tree
 fasttree -gtr -nt ${PHYLOGENY_OUTPUT_DIR}/alignments/concatenation.fasta > ${PHYLOGENY_OUTPUT_DIR}/tree/tree.nwk
 python ${BASE_DIR}/scripts/phylogeny/tree_annotations.py \
 -o ${PHYLOGENY_OUTPUT_DIR}/tree \
