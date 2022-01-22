aa0fe0971c71bc2e6d2b54d3f83783543a47d31e
Youn <younhun@mit.edu>
2022-01-21 19:18:12 -0500

    Minor fix to previous commit.

--- examples/umb/scripts/init_chronostrain.sh
+++ examples/umb/scripts/init_chronostrain.sh
@@ -8,7 +8,7 @@ export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/init.log"
 python ${BASE_DIR}/helpers/init_chronostrain_db.py \
 -m ${METAPHLAN_PKL_PATH} \
 -o ${CHRONOSTRAIN_ECOLI_DB_JSON} \
--sdb /mnt/d/strainge/strainge_db
+-r /mnt/d/ref_genomes
 
 python ${BASE_DIR}/helpers/prune_chronostrain_db.py \
 --input_json ${CHRONOSTRAIN_ECOLI_DB_JSON} \
