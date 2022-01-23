5bcbdea9b88a0392a6d31c4e501d94c66ae3b759
Youn <younhun@mit.edu>
2022-01-23 17:37:29 -0500

    Edited run_chronostrain.sh to output debug message.

--- examples/umb/scripts/run_chronostrain.sh
+++ examples/umb/scripts/run_chronostrain.sh
@@ -9,21 +9,23 @@ echo "Running inference."
 
 for umb_id in UMB05 UMB08 UMB11 UMB12 UMB15 UMB18 UMB20 UMB23 UMB24
 do
-	index_filename="filtered_${umb_id}_${INPUT_INDEX_FILENAME}"
-	export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/chronostrain_${umb_id}.log"
+    echo "Running inference on ${umb_id}."
+    index_filename="filtered_${umb_id}_${INPUT_INDEX_FILENAME}"
+    export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/chronostrain_${umb_id}.log"
 
-	cd $PROJECT_DIR/scripts
-	python run_bbvi.py \
-	--reads_input "${READS_DIR}/${umb_id}_filtered/${index_filename}" \
-	--out_dir $CHRONOSTRAIN_OUTPUT_DIR/${umb_id} \
-	--quality_format "fastq" \
-	--seed $SEED \
-	--iters $CHRONOSTRAIN_NUM_ITERS \
-	--epochs $CHRONOSTRAIN_NUM_EPOCHS \
-	--decay_lr $CHRONOSTRAIN_DECAY_LR \
-	--num_samples $CHRONOSTRAIN_NUM_SAMPLES \
-	--frag_chunk_size $CHRONOSTRAIN_FRAG_CHUNK_SZ \
-	--learning_rate $CHRONOSTRAIN_LR \
-	--plot_format "pdf"
+    cd $PROJECT_DIR/scripts
+    python run_bbvi.py \
+           --reads_input "${READS_DIR}/${umb_id}_filtered/${index_filename}" \
+           --out_dir $CHRONOSTRAIN_OUTPUT_DIR/${umb_id} \
+           --quality_format "fastq" \
+           --seed $SEED \
+           --iters $CHRONOSTRAIN_NUM_ITERS \
+           --epochs $CHRONOSTRAIN_NUM_EPOCHS \
+           --decay_lr $CHRONOSTRAIN_DECAY_LR \
+           --num_samples $CHRONOSTRAIN_NUM_SAMPLES \
+           --frag_chunk_size $CHRONOSTRAIN_FRAG_CHUNK_SZ \
+           --learning_rate $CHRONOSTRAIN_LR \
+           --plot_format "pdf" \
+           --plot_elbo
 done
 # ================================================
