296a3bde5744c02cac539326600553d04a110298
Youn <younhun@mit.edu>
2022-02-03 13:38:13 -0500

    fixed a bug in process_samples.sh

--- examples/umb/scripts/process_samples.sh
+++ examples/umb/scripts/process_samples.sh
@@ -66,6 +66,8 @@ mkdir -p "${SAMPLES_DIR}/kneaddata"
 
 		# Target fastq files.
 		kneaddata_output_dir="${SAMPLES_DIR}/kneaddata/${sra_id}"
+		mkdir -p kneaddata_output_dir
+
 		trimmed_1_paired_gz="${kneaddata_output_dir}/${sra_id}_paired_1.fastq.gz"
 		trimmed_1_unpaired_gz="${kneaddata_output_dir}/${sra_id}_unmatched_1.fastq.gz"
 		trimmed_2_paired_gz="${kneaddata_output_dir}/${sra_id}_paired_2.fastq.gz"
