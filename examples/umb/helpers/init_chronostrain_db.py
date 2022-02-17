2317fa0f1dc1af3e844813f522772895b28bee5c
Youn <younhun@mit.edu>
2022-02-17 12:01:10 -0500

    Fixed a missing call to mkdir() for reference markers.

--- examples/umb/helpers/init_chronostrain_db.py
+++ examples/umb/helpers/init_chronostrain_db.py
@@ -168,6 +168,7 @@ def create_chronostrain_db(
         if symlink_path.exists():
             logger.info(f"Path {symlink_path} already exists.")
         else:
+            symlink_path.parent.mkdir(exist_ok=True, parents=True)
             symlink_path.symlink_to(fasta_path)
             logger.info(f"Symlink {symlink_path} -> {fasta_path}")
 
