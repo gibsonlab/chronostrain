5d1fa45c622f3c2b263ddcd00afe838733176cd2
Youn <younhun@mit.edu>
2022-02-18 16:05:48 -0500

    fixed typo

--- examples/umb/helpers/init_chronostrain_db.py
+++ examples/umb/helpers/init_chronostrain_db.py
@@ -238,7 +238,7 @@ def create_chronostrain_db(
 
 def prune_entries(strain_entries: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     for strain_entry in strain_entries:
-        if len(strain_entry['markers'] == 0):
+        if len(strain_entry['markers']) == 0:
             logger.info("No markers found for "
                         f"{strain_entry['genus']} {strain_entry['species']} "
                         f"{strain_entry['name']} "
