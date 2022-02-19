ab478e5cdefb7b714804b289b47c3926cd5fac76
Youn <younhun@mit.edu>
2022-02-19 12:08:14 -0500

    Added filter.sh to hmp2.

--- examples/hmp2/scripts/filter.sh
+++ examples/hmp2/scripts/filter.sh
@@ -2,13 +2,13 @@
 set -e
 
 source settings.sh
-export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/filter.log"
 SEED=31415
 
 # =========== Read filtering. ===============
 while IFS=, read -r patient
 do
-	echo "Filtering reads for ${patient}"
+	echo "[*] Filtering reads for ${patient}"
+	export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/${patient}_filter.log"
 	python $PROJECT_DIR/scripts/filter_timeseries.py \
 	--reads_input "${READS_DIR}/${patient}/${INPUT_INDEX_FILENAME}" \
 	-o "${READS_DIR}/${patient}/filtered" \
