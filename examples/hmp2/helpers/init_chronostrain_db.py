bc6291b718f69eb3025a061b9f5c7e110c4b878e
Youn <younhun@mit.edu>
2022-02-20 12:15:43 -0500

    Updated bug in init_chronostrain_db.py introduced after new overlap change.

--- examples/hmp2/helpers/init_chronostrain_db.py
+++ examples/hmp2/helpers/init_chronostrain_db.py
@@ -429,19 +429,16 @@ def blast_hits_into_markers(seq_accessions: List[str], blast_paths: Dict[str, Pa
 
         # Now, hits are guaranteed to be non-overlapping. Instantiate the objects.
         for node in tree:
-            start_loc, end_loc, strand_arr, gene_name_arr, line_arr = node.data
-            gene_ids = [
-                f"{gene_name}#{line_idx}"
-                for strand, gene_name, line_idx in zip(strand_arr, gene_name_arr, line_arr)
-            ]
+            start_loc, end_loc, strand, gene_name, line_idx = node.data
+            gene_id = f"{gene_name}#{line_idx}"
             marker_objs.append({
-                'id': "+".join(gene_ids),
-                'name': "+".join(gene_name_arr),
+                'id': gene_id,
+                'name': gene_name,
                 'type': 'subseq',
                 'source': seq_accession,
                 'start': start_loc,
                 'end': end_loc,
-                'strand': '+' if len(strand_arr) > 1 else strand_arr[0],
+                'strand': strand,
                 'canonical': False
             })
     return marker_objs
