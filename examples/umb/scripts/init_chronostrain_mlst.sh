bc3c98f1ef6a721d0e7a106284ddd55c6887c0b4
Youn <younhun@mit.edu>
2022-03-15 15:42:00 -0400

    changed "pathogenic" to "MLST" (was a misnomer before).

--- examples/umb/scripts/init_chronostrain_mlst.sh
+++ examples/umb/scripts/init_chronostrain_mlst.sh
@@ -2,17 +2,17 @@
 set -e
 
 source settings.sh
-export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/init_pathogenic_genes.log"
+export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/init_MLST_genes.log"
 
 echo "[*] Initializing database."
 python ${BASE_DIR}/helpers/init_chronostrain_db.py \
 --uniprot_tsv ${BASE_DIR}/files/pathogenic_markers.tsv \
--o ${CHRONOSTRAIN_DATA_DIR}/database_ecoli_pathogenic_all.json \
+-o ${CHRONOSTRAIN_DATA_DIR}/database_ecoli_MLST_all.json \
 -r /mnt/d/ref_genomes
 
 echo "[*] Pruning database by hamming similarity."
-MULTIFASTA_FILE="all_strain_markers.fasta"
+MULTIFASTA_FILE="all_MLST_markers.fasta"
 python ${BASE_DIR}/helpers/prune_chronostrain_db.py \
---input_json ${CHRONOSTRAIN_DATA_DIR}/database_ecoli_pathogenic_all.json \
---output_json ${CHRONOSTRAIN_DATA_DIR}/database_ecoli_pathogenic_pruned.json \
+--input_json ${CHRONOSTRAIN_DATA_DIR}/database_ecoli_MLST_all.json \
+--output_json ${CHRONOSTRAIN_DATA_DIR}/database_ecoli_MLST_pruned.json \
 --alignments_path ${REFSEQ_ALIGN_PATH}
