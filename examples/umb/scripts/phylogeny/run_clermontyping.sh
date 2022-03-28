60b99b5a8537f0bcfe7e88b4a78762b16d48c3e8
Youn <younhun@mit.edu>
2022-03-28 12:15:41 -0400

    Extra edits to clermontyping gen script.

--- examples/umb/scripts/phylogeny/run_clermontyping.sh
+++ examples/umb/scripts/phylogeny/run_clermontyping.sh
@@ -4,13 +4,23 @@ source ../settings.sh
 export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/create_clermontyping_input.log"
 
 CLERMONTYPING_SCRIPT=~/ClermonTyping/clermonTyping.sh
-echo "Using ClermonTyping script at: ${CLERMONTYPING_SCRIPT}"
-echo "If ClermonTyping script does not exist, install it and/or change the path in this script (run_clermontyping.sh)."
+echo "[*] Using ClermonTyping script at: ${CLERMONTYPING_SCRIPT}"
+echo "[*] If ClermonTyping script is not found, install it and/or change the path in this script (run_clermontyping.sh)."
 
 python ${BASE_DIR}/scripts/phylogeny/create_clermontyping_input.py \
 -i ${NCBI_REFSEQ_DIR}/index.tsv  \
 -c ${CLERMONTYPING_SCRIPT} \
 -o ${PHYLOGENY_OUTPUT_DIR}/ClermonTyping/clermontyping.sh \
 
+echo "[*] Running ClermonTyping batched analysis."
 cd ${PHYLOGENY_OUTPUT_DIR}/ClermonTyping
 bash clermontyping.sh
+
+# Merge all batched results.
+final_path=${PHYLOGENY_OUTPUT_DIR}/ClermonTyping/umb_phylogroups_complete.txt
+> $final_path  # Clear contents of file.
+for f in ${PHYLOGENY_OUTPUT_DIR}/ClermonTyping/umb_*/umb_*_phylogroups.txt
+do
+	cat $f >> $final_path
+done
+echo "[*] Generated phylogroup path ${final_pathk}."
