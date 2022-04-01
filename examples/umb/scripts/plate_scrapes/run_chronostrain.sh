52997730e5a12028453ef3d7fbe8f43ae21c4308
Youn <younhun@mit.edu>
2022-04-01 15:27:06 -0400

    added plate_scrapes scripts.

---
+++ examples/umb/scripts/plate_scrapes/run_chronostrain.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+source settings.sh
+SEED=31415
+
+# =========== Run chronostrain. ==================
+cd $READS_DIR
+for csv_file in *.csv
+do
+	regex="(.*).csv"
+	if [[ $csv_file =~ $regex ]]
+	then
+		sample_name="${BASH_REMATCH[1]}"
+	else
+		echo "Skipping ${csv_file}."
+		continue
+	fi
+
+	echo "Running inference on ${sample_name}."
+	export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/chronostrain_${sample_name}.log"
+
+	cd $PROJECT_DIR/scripts
+	python run_bbvi.py \
+	--reads_input "${READS_DIR}/${sample_name}_filtered/filtered_${sample_name}.csv" \
+	--out_dir $CHRONOSTRAIN_OUTPUT_DIR/${sample_name} \
+	--seed $SEED \
+	--iters $CHRONOSTRAIN_NUM_ITERS \
+	--epochs $CHRONOSTRAIN_NUM_EPOCHS \
+	--decay_lr $CHRONOSTRAIN_DECAY_LR \
+	--lr_patience 10 \
+	--min_lr 1e-4 \
+	--learning_rate $CHRONOSTRAIN_LR \
+	--num_samples $CHRONOSTRAIN_NUM_SAMPLES \
+	--read_batch_size $CHRONOSTRAIN_READ_BATCH_SZ \
+	--plot_format "pdf" \
+	--plot_elbo
+done
+# ================================================
---
+++ examples/umb/scripts/plate_scrapes/settings.sh
@@ -0,0 +1,49 @@
+# Modify these before running.
+export PROJECT_DIR="/home/bromii/chronostrain"
+export BASE_DIR="${PROJECT_DIR}/examples/umb"
+export DATA_DIR="/mnt/e/umb_data"
+export OUTPUT_DIR="/mnt/d/chronostrain/umb_plate_scrapes"
+export CHRONOSTRAIN_DB_DIR="/mnt/d/chronostrain/umb_database"
+export LOGDIR="${OUTPUT_DIR}/logs"
+
+# ======== Location of this file.
+export SETTINGS_PATH="${BASE_DIR}/scripts/plate_scrapes/settings.sh"
+
+# Location for reads and outputs.
+export READS_DIR="${OUTPUT_DIR}/reads"
+export CHRONOSTRAIN_OUTPUT_DIR="${OUTPUT_DIR}/chronostrain"
+
+# ========= Chronostrain settings
+export CHRONOSTRAIN_NUM_ITERS=50
+export CHRONOSTRAIN_NUM_SAMPLES=200
+export CHRONOSTRAIN_READ_BATCH_SZ=5000
+export CHRONOSTRAIN_NUM_EPOCHS=150
+export CHRONOSTRAIN_DECAY_LR=0.25
+export CHRONOSTRAIN_LR=0.05
+export CHRONOSTRAIN_OUTPUT_FILENAME="abundances.out"
+
+export CHRONOSTRAIN_DB_JSON_ALL="${CHRONOSTRAIN_DB_DIR}/database_all.json"
+export CHRONOSTRAIN_DB_JSON_PRUNED="${CHRONOSTRAIN_DB_DIR}/database_pruned.json"
+export CHRONOSTRAIN_DB_JSON="${CHRONOSTRAIN_DB_JSON_PRUNED}"
+export MULTIFASTA_FILE="strain_markers.fasta"
+
+# ========= (Example-specific configuration. No need to modify below this line, unless you really want it changed.)
+export CHRONOSTRAIN_INI="${BASE_DIR}/files/chronostrain.ini"
+export CHRONOSTRAIN_LOG_INI="${BASE_DIR}/files/logging.ini"
+
+# ========= Done.
+echo "======================================================="
+echo "Loaded shell settings from ${SETTINGS_PATH}."
+echo "Chronostrain config: ${CHRONOSTRAIN_INI}"
+echo "Logging config: ${CHRONOSTRAIN_LOG_INI}"
+echo "Logging dir: ${LOGDIR}"
+echo "======================================================="
+
+check_program()
+{
+	command -v ${1} >/dev/null 2>&1 || {
+		echo >&2 "I require ${1} but it's not installed.  Aborting.";
+		exit 1;
+	}
+}
+export check_program
