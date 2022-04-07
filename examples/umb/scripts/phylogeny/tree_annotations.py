c1f378e3ca09733a8b274da944fe463c25d420a7
Youn <younhun@mit.edu>
2022-04-07 10:47:42 -0400

    tree_annotations now doesnt depend on chronostrain db.

--- examples/umb/scripts/phylogeny/tree_annotations.py
+++ examples/umb/scripts/phylogeny/tree_annotations.py
@@ -1,11 +1,10 @@
 import argparse
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Tuple
 
+import pandas as pd
 import matplotlib.colors as mc
 import seaborn as sns
-from chronostrain.config import cfg
-from chronostrain.model import Strain
 
 
 def parse_args():
@@ -16,13 +15,29 @@ def parse_args():
     # Input specification.
     parser.add_argument('-o', '--output_dir', required=True, type=str,
                         help='<Required> The target output directory.')
+    parser.add_argument('-i', '--index', dest='strain_index_path',
+                        required=True, type=str,
+                        help='<Required> The path to the TSV refseq index file.')
     parser.add_argument('-p', '--phylogroup_path', required=True, type=str,
                         help='<Required> The path to ClermonTyping phylogroup output txt file.')
     return parser.parse_args()
 
 
+def parse_strain_index(index_path: Path) -> List[Tuple[str, str]]:
+    """
+    Return a list of (strain accession, strain name) tuples.
+    """
+    df = pd.read_csv(index_path)
+    strains = []
+    for _, row in df.loc[df['Genus'] == 'Escherichia'].iterrows():
+        strains.append((
+            row['Strain'], row['Accession']
+        ))
+    return strains
+
+
 def create_clade_annotation(target_path: Path,
-                            strains: List[Strain],
+                            strains: List[Tuple[str, str]],
                             strain_clades: Dict[str, str],
                             clade_hex_colors: Dict[str, str]):
     with open(target_path, 'w') as f:
@@ -30,19 +45,19 @@ def create_clade_annotation(target_path: Path,
         print("SEPARATOR COMMA", file=f)
         print("DATA", file=f)
         print("#NODE_ID,TYPE,COLOR,LABEL_OR_STYLE,SIZE_FACTOR", file=f)
-        for strain in strains:
-            clade = strain_clades[strain.id]
+        for strain_id, _ in strains:
+            clade = strain_clades[strain_id]
             color = clade_hex_colors[clade]
-            print(f"{strain.id},range,{color},{clade}", file=f)
+            print(f"{strain_id},range,{color},{clade}", file=f)
 
 
-def create_label_annotation(target_path: Path, strains: List[Strain]):
+def create_label_annotation(target_path: Path, strains: List[Tuple[str, str]]):
     with open(target_path, 'w') as f:
         print("LABELS", file=f)
         print("SEPARATOR COMMA", file=f)
         print("DATA", file=f)
-        for strain in strains:
-            print(f"{strain.id},{strain.name}", file=f)
+        for strain_id, strain_name in strains:
+            print(f"{strain_id},{strain_name}", file=f)
 
 
 def load_clade_assignments(phylogroup_path: Path) -> Dict[str, str]:
@@ -70,8 +85,8 @@ def load_clade_colors() -> Dict[str, str]:
 
 def main():
     args = parse_args()
-    db = cfg.database_cfg.get_database()
-    strains = db.all_strains()
+
+    strains = parse_strain_index(Path(args.strain_index_path))
 
     out_dir = Path(args.output_dir)
     out_dir.mkdir(exist_ok=True, parents=True)
