de7bb8864ac1270776fc86dc45d3b934a5c7c9bd
Youn <younhun@mit.edu>
2022-04-13 08:57:06 -0400

    Re included ll_threshold lower bound (feasible now that we are using bwa mem again)

--- chronostrain/algs/subroutines/likelihoods/sparse.py
+++ chronostrain/algs/subroutines/likelihoods/sparse.py
@@ -210,8 +210,8 @@ class SparseLogLikelihoodComputer(AbstractLogLikelihoodComputer):
                 end_clip=end_clip
             )
 
-            # if ll < ll_threshold:
-            #     return
+            if ll < ll_threshold:
+                return
             read_to_frag_likelihoods[read.id].append((frag, ll))
 
         for aln in self.pairwise_reference_alignments.alignments_by_timepoint(t_idx):
