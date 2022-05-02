a62d1fc02636c57738ee366d4c46551d8f9af6fc
Youn <younhun@mit.edu>
2022-05-02 15:49:37 -0400

    Deleted unused examples. Added "synthetic" example.

---
+++ examples/synthetic/README.md
@@ -0,0 +1,1 @@
+A benchmark using synthetic reads to learn abundances of two high-ID strains (5 SNP difference).
---
+++ examples/synthetic/scripts/settings.sh
@@ -0,0 +1,47 @@
+# Modify these before running.
+export PROJECT_DIR="/home/bromii/chronostrain"
+export BASE_DIR="${PROJECT_DIR}/examples/synthetic"
+export DATA_DIR="/mnt/e/synthetic_data"
+export OUTPUT_DIR="/mnt/d/chronostrain/synthetic/output"
+export CHRONOSTRAIN_DB_DIR="/mnt/d/chronostrain/synthetic/database"
+export LOGDIR="${OUTPUT_DIR}/logs"
+
+# ======== Location of this file.
+export SETTINGS_PATH="${BASE_DIR}/scripts/settings.sh"
+
+# Location for reads and outputs.
+export READS_DIR="/mnt/d/chronostrain/synthetic/reads"
+export CHRONOSTRAIN_OUTPUT_DIR="${OUTPUT_DIR}/chronostrain"
+
+# ========= Chronostrain settings
+export CHRONOSTRAIN_NUM_ITERS=50
+export CHRONOSTRAIN_NUM_SAMPLES=200
+export CHRONOSTRAIN_READ_BATCH_SZ=2500
+export CHRONOSTRAIN_NUM_EPOCHS=150
+export CHRONOSTRAIN_DECAY_LR=0.25
+export CHRONOSTRAIN_LR=0.05
+export CHRONOSTRAIN_CACHE_DIR="${OUTPUT_DIR}/cache"
+export CHRONOSTRAIN_OUTPUT_FILENAME="abundances.out"
+
+# ========= (Example-specific configuration. No need to modify below this line, unless you really want it changed.)
+export CHRONOSTRAIN_DB_JSON="${BASE_DIR}/files/database.json"
+export MULTIFASTA_FILE="markers.fasta"
+export CHRONOSTRAIN_INI="${BASE_DIR}/files/chronostrain.ini"
+export CHRONOSTRAIN_LOG_INI="${BASE_DIR}/files/logging.ini"
+
+# ========= Done.
+echo "======================================================="
+echo "Loaded shell settings from ${SETTINGS_PATH}."
+echo "Chronostrain config: ${CHRONOSTRAIN_INI}"
+echo "Logging config: ${CHRONOSTRAIN_LOG_INI}"
+echo "Logging dir: ${LOGDIR}"
+echo "======================================================="
+
+check_program()
+{
+	command -v ${1} >/dev/null 2>&1 || {
+		echo >&2 "I require ${1} but it's not installed.  Aborting.";
+		exit 1;
+	}
+}
+export check_program
