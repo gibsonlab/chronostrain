fba19f98c4adf6675a5dfc094894bb799b3a0d62
Youn <younhun@mit.edu>
2022-05-04 12:15:39 -0400

    Added performanc evaluation script.

--- examples/synthetic/scripts/evaluate.sh
+++ examples/synthetic/scripts/evaluate.sh
@@ -4,7 +4,10 @@ source settings.sh
 
 
 mkdir -p ${DATA_DIR}/summary
+
 export CHRONOSTRAIN_CACHE_DIR="${DATA_DIR}/cache"
+export CHRONOSTRAIN_LOG_FILEPATH="${DATA_DIR}/summary/logs/evaluate.log"
+
 python ${BASE_DIR}/helpers/eval_performance.py \
 -b ${DATA_DIR} \
 -g ${GROUND_TRUTH} \
