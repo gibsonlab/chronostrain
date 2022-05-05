4d3abc008487e98ea9fe18eb36986d67c0b3f9f8
Youn <younhun@mit.edu>
2022-05-05 17:29:55 -0400

    Added echo messages.

--- examples/synthetic/scripts/run_strainest_all.sh
+++ examples/synthetic/scripts/run_strainest_all.sh
@@ -7,6 +7,8 @@ mkdir -p ${STRAINEST_DB_DIR}
 cd ${STRAINEST_DB_DIR}
 cp ${CHRONOSTRAIN_DB_DIR}/assemblies/CP009273.1_Original/CP009273.1.fasta CP009273.1_Original.fasta
 cp ${CHRONOSTRAIN_DB_DIR}/assemblies/CP009273.1_Substitution/CP009273.1_Substitution.fasta CP009273.1_Substitution.fasta
+
+echo "[*] Building bowtie2 index..."
 bowtie2-build CP009273.1_Original.fasta,CP009273.1_Substitution.fasta -f ${STRAINEST_BOWTIE2_DB_NAME} --quiet
 
 
