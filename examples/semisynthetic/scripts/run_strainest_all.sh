68b86a8f4cb79d42bbc45f613c963748a12b5597
Youn <younhun@mit.edu>
2022-05-13 11:46:26 -0400

    added semisynthetic strainest script.

--- examples/semisynthetic/scripts/run_strainest_all.sh
+++ examples/semisynthetic/scripts/run_strainest_all.sh
@@ -3,7 +3,7 @@ set -e
 source settings.sh
 
 
-cd ${BASE_DIR}/scripts/read_depths
+cd ${BASE_DIR}/scripts
 for n_reads in 10000 50000 100000 500000 1000000
 do
 	for (( trial = 1; trial < ${N_TRIALS}+1; trial++ ));
