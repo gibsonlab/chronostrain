98c52f47453ca619c6aa9f9702b8008e79dcf7c3
Youn <younhun@mit.edu>
2022-05-13 11:35:12 -0400

    Created script for semisynthetic to restrict database to ecoli.

--- examples/semisynthetic/scripts/filter_all.sh
+++ examples/semisynthetic/scripts/filter_all.sh
@@ -4,6 +4,11 @@ source settings.sh
 
 
 cd ${BASE_DIR}/scripts
+
+python ${BASE_DIR}/helpers/init_db.py \
+-i "/mnt/d/chronostrain/umb_database/database_pruned.json" \
+-o ${CHRONOSTRAIN_DB_JSON}
+
 for n_reads in 10000 25000 50000 75000 100000
 do
 	for (( trial = 1; trial < ${N_TRIALS}+1; trial++ ));
