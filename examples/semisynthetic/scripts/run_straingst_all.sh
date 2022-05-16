a152bdc29ccf1670718b10fdd939b8d5480e9079
Youn <younhun@mit.edu>
2022-05-16 10:32:13 -0400

    Fixed StrainGST script.

--- examples/semisynthetic/scripts/run_straingst_all.sh
+++ examples/semisynthetic/scripts/run_straingst_all.sh
@@ -3,7 +3,7 @@ set -e
 source settings.sh
 
 
-cd ${BASE_DIR}/scripts/read_depths
+cd ${BASE_DIR}/scripts
 for n_reads in 10000 25000 50000 75000 100000; do
 	for (( trial = 1; trial < ${N_TRIALS}+1; trial++ )); do
 		bash run_straingst.sh $n_reads $trial 0
