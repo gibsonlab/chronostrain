11a5cc26fdf8d834952adda2ba7edaa946ac90b8
Youn <younhun@mit.edu>
2022-05-16 12:35:47 -0400

    Modified evaluation script.

--- examples/semisynthetic/scripts/evaluate.sh
+++ examples/semisynthetic/scripts/evaluate.sh
@@ -8,4 +8,5 @@ python ${BASE_DIR}/helpers/evaluate.py \
 -i ${REFSEQ_INDEX} \
 -b ${DATA_DIR} \
 -o ${DATA_DIR}/summary/output.csv \
+-g ${RELATIVE_GROUND_TRUTH} \
 -a /mnt/d/chronostrain/umb_database/strain_alignments/concatenation.fasta
