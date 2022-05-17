93ed4f875fdd0346a91dd3589a13ada165b87059
Youn <younhun@mit.edu>
2022-05-17 17:48:40 -0400

    edited strainest script.

--- examples/semisynthetic/scripts/run_strainest_all.sh
+++ examples/semisynthetic/scripts/run_strainest_all.sh
@@ -8,10 +8,11 @@ for n_reads in 10000 25000 50000 75000 100000
 do
 	for (( trial = 1; trial < ${N_TRIALS}+1; trial++ ));
 	do
-		bash run_strainest.sh $n_reads $trial 0
-		bash run_strainest.sh $n_reads $trial 1
-		bash run_strainest.sh $n_reads $trial 2
-		bash run_strainest.sh $n_reads $trial 3
-		bash run_strainest.sh $n_reads $trial 4
+		bash run_strainest.sh $n_reads $trial 0 &
+		bash run_strainest.sh $n_reads $trial 1 &
+		bash run_strainest.sh $n_reads $trial 2 &
+		bash run_strainest.sh $n_reads $trial 3 &
+		bash run_strainest.sh $n_reads $trial 4 &
+		wait
 	done
 done
