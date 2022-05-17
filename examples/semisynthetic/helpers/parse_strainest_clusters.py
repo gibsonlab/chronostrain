f236696a43bdfce6b0c01d246e96ad8f86e873d2
Youn <younhun@mit.edu>
2022-05-17 14:27:10 -0400

    Edited strainest clustering script

--- examples/semisynthetic/helpers/parse_strainest_clusters.py
+++ examples/semisynthetic/helpers/parse_strainest_clusters.py
@@ -1,25 +1,29 @@
 import argparse
+import pandas as pd
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--clusters_file', type=str, required=True,
                         help='<Required> The clusters file output of strainest snvclust.')
+    parser.add_argument('--refseq_index', type=str, required=True)
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
 
+    index_df = pd.read_csv(args.refseq_index, sep='\t')
+
     representative = set()
     with open(args.clusters_file, 'rt') as f:
         for line in f:
             line = line.strip()
             tokens = line.split('\t')
-            strain_id = tokens[1]
+            strain_id = tokens[1].removesuffix('.chrom.fna')
             representative.add(strain_id)
     for strain_id in representative:
-        print(strain_id)
+        print(index_df.loc[index_df['Accession'] == strain_id, 'SeqPath'].item())
 
 
 if __name__ == "__main__":
