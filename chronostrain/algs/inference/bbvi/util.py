0155eb19bb6cb3d74ac710d748f34ccad24be28f
Younhun Kim <younhun@mit.edu>
2022-05-21 01:56:25 -0400

    counting nans wip

--- chronostrain/algs/inference/bbvi/util.py
+++ chronostrain/algs/inference/bbvi/util.py
@@ -13,6 +13,9 @@ def log_softmax(x_samples: torch.Tensor, t: int) -> torch.Tensor:
 
 def log_spherical(x_samples: torch.Tensor, t: int) -> torch.Tensor:
     # x_samples: (T x N x S) tensor.
+    print("DEBUG")
+    print(torch.log(torch.abs(x_samples[t])))
+    print(torch.log(torch.pow(x_samples[t], 2).sum(dim=-1, keepdim=True)))
     return 2 * torch.log(torch.abs(x_samples[t])) - torch.log(torch.pow(x_samples[t], 2).sum(dim=-1, keepdim=True))
 
 
