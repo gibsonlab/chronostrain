5fd2174a15eeba9f6d150eeb5c475cf579da6eb7
Youn <younhun@mit.edu>
2022-05-23 13:06:43 -0400

    Changed log filepath for pass 2 inference.

--- examples/semisynthetic/scripts/run_chronostrain.sh
+++ examples/semisynthetic/scripts/run_chronostrain.sh
@@ -26,10 +26,10 @@ output_dir=${trial_dir}/output/chronostrain
 log_dir=${trial_dir}/logs
 
 mkdir -p $log_dir
-export CHRONOSTRAIN_LOG_FILEPATH="${log_dir}/chronostrain.log"
 export CHRONOSTRAIN_CACHE_DIR="${trial_dir}/cache"
 
 if [ "$pass" == "1" ]; then
+	export CHRONOSTRAIN_LOG_FILEPATH="${log_dir}/chronostrain.log"
 	echo "[*] Running Chronostrain inference (Default mode) for n_reads: ${n_reads}, trial: ${trial}"
 	python $PROJECT_DIR/scripts/run_bbvi.py \
 	--reads_input "${read_dir}/filtered/filtered_input_files.csv" \
@@ -46,6 +46,7 @@ if [ "$pass" == "1" ]; then
 	--plot_format "pdf" \
 	--plot_elbo
 elif [ "$pass" == "2" ]; then
+	export CHRONOSTRAIN_LOG_FILEPATH="${log_dir}/chronostrain.pass2.log"
 	echo "[*] Running Chronostrain inference (Second Pass) for n_reads: ${n_reads}, trial: ${trial}"
 	python $PROJECT_DIR/scripts/run_bbvi.py \
 	--reads_input "${read_dir}/filtered/filtered_input_files.csv" \
