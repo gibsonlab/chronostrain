0f7159ce242e9ba807d8607489f04d3456deb44c
Youn <younhun@mit.edu>
2022-05-26 17:26:11 -0400

    More updates to gt-pro invocation.

--- examples/semisynthetic/scripts/run_strainfacts.sh
+++ examples/semisynthetic/scripts/run_strainfacts.sh
@@ -42,7 +42,7 @@ echo "[*] Running 'GT_Pro genotype' for timepoint ${t_idx}..."
 GT_Pro genotype -f \
 -d ${GT_PRO_DB_DIR}/${GT_PRO_DB_NAME}/${GT_PRO_DB_NAME} \
 -t ${N_CORES} \
--o ${output_dir}/%{n}_reads.tsv \
+-o ${output_dir}/%{n}_reads \
 0_reads.fq 1_reads.fq 2_reads.fq 3_reads.fq 4_reads.fq
 
 
