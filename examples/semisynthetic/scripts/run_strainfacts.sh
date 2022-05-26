32fa1607ee557b1e00111ee38e7c7c103d3b0a37
Youn <younhun@mit.edu>
2022-05-26 16:46:35 -0400

    More updates to gt-pro invocation.

--- examples/semisynthetic/scripts/run_strainfacts.sh
+++ examples/semisynthetic/scripts/run_strainfacts.sh
@@ -29,24 +29,28 @@ cd ${output_dir}
 
 
 # Run metagenotyping
+echo "[*] Preparing GT_Pro inputs..."
 mg_prefix="mg_all"
 metagenotype_all="${mg_prefix}.tsv"
 > $metagenotype_all  # Clear file incase it exists.
 
 for t_idx in 0 1 2 3 4; do
-	echo "[*] Running 'GT_Pro genotype' for timepoint ${t_idx}..."
 	concat_reads=${t_idx}_reads.fq
 	> $concat_reads
 	pigz -dck ${read_dir}/${t_idx}_reads_1.fq.gz >> $concat_reads
 	pigz -dck ${read_dir}/${t_idx}_reads_2.fq.gz >> $concat_reads
-	GT_Pro genotype -f \
-	-d ${GT_PRO_DB_DIR}/${GT_PRO_DB_NAME}/${GT_PRO_DB_NAME} \
-	-t ${N_CORES} \
-	-o ${output_dir}/${t_idx}_reads_1 \
-	$concat_reads
+done
 
-	metagenotype="${t_idx}_reads_1.tsv"
-	rm $concat_reads
+echo "[*] Running 'GT_Pro genotype' for timepoint ${t_idx}..."
+GT_Pro genotype -f \
+-d ${GT_PRO_DB_DIR}/${GT_PRO_DB_NAME}/${GT_PRO_DB_NAME} \
+-t ${N_CORES} \
+-o ${output_dir}/%{n}_reads.tsv \
+0_reads.fq 1_reads.fq 2_reads.fq 3_reads.fq 4_reads.fq
+
+echo "[*] Parsing GT_Pro outputs..."
+for t_idx in 0 1 2 3 4; do
+	metagenotype="${t_idx}_reads.tsv"
 	pigz -d ${metagenotype}.gz
 	GT_Pro parse --dict ${GT_PRO_DB_DIR}/${GT_PRO_DB_NAME}/${GT_PRO_DB_NAME}.snp_dict.noheader.tsv --in $metagenotype \
 	| awk -v t="${t_idx}" '{ print t "\t" $0; }' >> $metagenotype_all
@@ -61,3 +65,6 @@ sfacts fit \
 --optimizer-learning-rate 0.05 \
 --min-optimizer-learning-rate 1e-06 \
 ${mg_prefix}.mgen.nc ${mg_prefix}.world.nc
+
+echo "[*] Cleaning up..."
+rm *.fq
