2e9a2d74175524d8310c7f56dc3b587e989d0ab7
Youn <younhun@mit.edu>
2022-05-26 19:04:04 -0400

    added depth 5000 to all batch scripts.

--- examples/semisynthetic/scripts/run_chronostrain_all.sh
+++ examples/semisynthetic/scripts/run_chronostrain_all.sh
@@ -5,7 +5,7 @@ source settings.sh
 pass=$1
 cd ${BASE_DIR}/scripts
 
-for n_reads in 10000 25000 50000 75000 100000
+for n_reads in 5000 10000 25000 50000 75000 100000
 do
 	for (( trial = 1; trial < ${N_TRIALS}+1; trial++ ));
 	do
--- examples/semisynthetic/scripts/run_strainest_all.sh
+++ examples/semisynthetic/scripts/run_strainest_all.sh
@@ -4,7 +4,7 @@ source settings.sh
 
 
 cd ${BASE_DIR}/scripts
-for n_reads in 10000 25000 50000 75000 100000
+for n_reads in 5000 10000 25000 50000 75000 100000
 do
 	for (( trial = 1; trial < ${N_TRIALS}+1; trial++ ));
 	do
