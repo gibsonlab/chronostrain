Fixed numerical instability in generalized pareto fitting. (re-included custom implementation).

chronostrain/algs/inference/bbvi/solver_full.py
@@ -1,7 +1,5 @@
 from typing import List, Optional, Callable, Type, Dict, Any
 
-import numba
-import numpy as np
 import scipy.special
 import torch
 
@@ -10,10 +8,10 @@ from chronostrain.model.generative import GenerativeModel
 from chronostrain.model.io import TimeSeriesReads
 
 from chronostrain.config import create_logger
+from chronostrain.util.math.psis import psis_smooth_ratios
 
 from .. import AbstractModelSolver
-from .posteriors import *
-from .util import log_softmax, log_matmul_exp, psis_smooth_ratios
+from .util import log_softmax, log_matmul_exp
 from .solver import BBVISolver
 from ..vi import GaussianPosteriorFullCorrelation
 
@@ -38,6 +36,8 @@ class BBVISolverFullPosterior(AbstractModelSolver):
             correlation_type=partial_correlation_type
         )
         self.posterior: GaussianPosteriorFullCorrelation = None
+        self.log_smoothed_ratios: torch.Tensor = None
+        self.k_hat: float = float('inf')
 
     def prior_ll(self, x_samples: torch.Tensor) -> torch.Tensor:
         return self.model.log_likelihood_x(X=x_samples).detach()
@@ -99,6 +99,8 @@ class BBVISolverFullPosterior(AbstractModelSolver):
         log_importance_ratios = log_importance_ratios.cpu().numpy()
         log_importance_ratios = log_importance_ratios - scipy.special.logsumexp(log_importance_ratios)
         log_smoothed_ratios, k_hat = psis_smooth_ratios(log_importance_ratios)
+        log_smoothed_ratios = torch.tensor(log_smoothed_ratios, device=x_samples.device)
+
         logger.debug(f"Estimated Pareto k-hat: {k_hat}")
         if k_hat > 0.7:
             # Extremely large number of MCMC samples are needed for stable gradient estimates!
@@ -111,18 +113,18 @@ class BBVISolverFullPosterior(AbstractModelSolver):
 
         posterior_mean = torch.sum(
             x_samples * torch.exp(
-                torch.unsqueeze(torch.tensor(log_smoothed_ratios, device=x_samples.device), dim=1)
+                torch.unsqueeze(log_smoothed_ratios, dim=1)
             ),
             dim=0
         )
-        posterior_var_scaling = torch.tensor(
-            x_samples * torch.unsqueeze(
-                torch.exp(0.5 * torch.tensor(log_smoothed_ratios, device=x_samples.device)),
-                dim=1  # (N x 1)
-            ),
-            device=x_samples.device
+        posterior_var_scaling = x_samples * torch.unsqueeze(
+            torch.exp(0.5 * log_smoothed_ratios),
+            dim=1  # (N x 1)
         )
         self.posterior = GaussianPosteriorFullCorrelation(
             self.model.num_strains(), self.model.num_times(), posterior_mean, posterior_var_scaling
         )
+
+        self.log_smoothed_ratios = log_smoothed_ratios
+        self.k_hat = k_hat
         logger.debug("Finished computing importance-weighted mean/covariance.")

chronostrain/util/math/generalized_pareto.py
@@ -1,105 +1,52 @@
-"""
-Copyright 2017 Aki Vehtari, Tuomas Sivula
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-1. Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-3. Neither the name of the copyright holder nor the names of its contributors
-may be used to endorse or promote products derived from this software without
-specific prior written permission.
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. """
-
+from typing import Tuple
 
 import numpy as np
+import scipy.special
 
 
-def fit_pareto(x: np.ndarray,
-               loc: float = 0.0):
-    """Estimate the paramaters for the Generalized Pareto Distribution (GPD)
-    Returns empirical Bayes estimate for the parameters of the two-parameter
-    generalized Parato distribution given the data.
-    Parameters
-    ----------
-    x : ndarray
-        One dimensional data array
-    loc: float
-
-    Returns
-    -------
-    k, sigma : float
-        estimated parameter values
-    Notes
-    -----
-    This function returns a negative of Zhang and Stephens's k, because it is
-    more common parameterisation.
+def fit_pareto(x: np.ndarray, loc: float) -> Tuple[float, float]:
     """
-    if x.ndim != 1 or len(x) <= 1:
-        raise ValueError("Invalid input array.")
+    Implementation of Zhang and Stephens (2009) for fitting a generalized Pareto distribution.
 
-    x = np.copy(x) - loc
-    sort = np.argsort(x)
+    :param x: the data to fit across.
+    """
     n = len(x)
-    PRIOR = 3
-    m = 30 + int(np.sqrt(n))
+    x = np.sort(x) - loc
+    m = 20 + int(np.sqrt(n))
 
-    bs = np.arange(1, m + 1, dtype=float)
-    bs -= 0.5
-    np.divide(m, bs, out=bs)
-    np.sqrt(bs, out=bs)
-    np.subtract(1, bs, out=bs)
+    def lx(b_: np.ndarray, x_: np.ndarray) -> np.ndarray:
+        k_ = -np.mean(np.log(1 - np.expand_dims(b_, axis=1) * np.expand_dims(x_, axis=0)), axis=1)
+        return np.log(b_ / k_) + k_ - 1
 
-    bs /= PRIOR * x[sort[int(n/4 + 0.5) - 1]]
-    bs += 1 / x[sort[-1]]
+    b = (1 / x[-1]) + (1 - np.sqrt(m / (np.arange(1, m+1) - 0.5))) / 3 / x[int((n/4) + .5) - 1]  # length m
+    L = n * lx(b, x)
+    w = np.exp(L - scipy.special.logsumexp(L))
 
-    ks = np.negative(bs)
-    temp = ks[:, None] * x
-    np.log1p(temp, out=temp)
-    np.mean(temp, axis=1, out=ks)
+    b = np.sum(b * w)
+    k_est = float(np.mean(np.log(1 - b * x)))  # note: original paper has this sign flipped by convention.
+    sigma_est = float(-k_est / b)
+    return k_est, sigma_est
 
-    L = bs / ks
-    np.negative(L, out=L)
-    np.log(L, out=L)
-    L -= ks
-    L -= 1
-    L *= n
 
-    temp = L - L[:, None]
-    np.exp(temp, out=temp)
-    w = np.sum(temp, axis=1)
-    np.divide(1, w, out=w)
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    from scipy.stats import genpareto
 
-    # remove negligible weights
-    dii = w >= 10 * np.finfo(float).eps
-    if not np.all(dii):
-        w = w[dii]
-        bs = bs[dii]
-    # normalise w
-    w /= w.sum()
+    cut_point = 5
+    k = 0.2
+    sigma = 50.0
 
-    # posterior mean for b
-    b = np.sum(bs * w)
-    # Estimate for k, note that we return a negative of Zhang and
-    # Stephens's k, because it is more common parameterisation.
-    temp = (-b) * x
-    np.log1p(temp, out=temp)
-    k = np.mean(temp)
+    num_samples = 10000
+    true_dist = genpareto(c=k, loc=cut_point, scale=sigma)
+    samples = true_dist.rvs(size=num_samples)
 
-    # estimate for sigma
-    sigma = -k / b * n / (n - 0)
+    k_hat, sigma_hat = fit_pareto(samples, loc=np.min(samples))
+    est_dist = genpareto(c=k_hat, loc=np.min(samples), scale=sigma_hat)
+    print(f"k = {k}, sigma = {sigma}")
+    print(f"k_est = {k_hat}, sigma_est = {sigma_hat}")
 
-    # weakly informative prior for k
-    a = 10
-    k = k * n / (n+a) + a * 0.5 / (n+a)
-    return k, sigma
+    x = np.linspace(0, 100, 10000)
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+    ax.plot(x, true_dist.pdf(x))
+    ax.plot(x, est_dist.pdf(x))
+    plt.show()

chronostrain/util/math/psis.py
@@ -1,8 +1,76 @@
+from typing import Tuple
+
 import numpy as np
-import scipy.special
-import scipy.stats
+import scipy.stats, scipy.special
 
-from chronostrain.util.math import fit_pareto
-
+from .generalized_pareto import fit_pareto
 
 
+def psis_smooth_ratios(log_raw_ratios: np.ndarray, k_min: float = 1/3) -> Tuple[np.ndarray, float]:
+    """
+    Based on citation:
+    2019 Vehtari, Simpson, Gelman, Yao, Gabry. 'Pareto Smoothed Importance Sampling'
+
+    Parts of code lifted from authors' original implementation.
+    (https://github.com/avehtari/PSIS/blob/master/py/psis.py)
+
+    :param log_raw_ratios:
+    :param k_min: Reweighting does not happen if the empirical Pareto k-hat does not exceed this value.
+    :return: A pair of values:
+    1) Pareto-smoothed estimation (log-)weights, to be used in importance sampling calculation.
+    2) the estimated Pareto distribution's empirical k (k-hat).
+    """
+    n = len(log_raw_ratios)
+    if n <= 1:
+        raise ValueError("More than one log-weight needed.")
+
+    # allocate new array for output
+    sort_idxs = np.argsort(log_raw_ratios)
+    wts = np.copy(log_raw_ratios)
+
+    # precalculate constants
+    cutoff_idx = -int(np.ceil(min(0.2 * n, 3 * np.sqrt(n)))) - 1
+
+    # divide log weights into body and right tail
+    log_cutoff_value = max(
+        wts[sort_idxs[cutoff_idx]],
+        np.log(np.finfo(float).tiny)  # smallest possible value
+    )
+    cutoff_value = np.exp(log_cutoff_value)
+
+    tailinds, = np.where(wts > log_cutoff_value)
+    tail_sz = len(tailinds)
+
+    if tail_sz <= 4:
+        raise RuntimeError("Not enough tail samples to perform this estimate.")
+
+    tail_wts = wts[tailinds]
+    k_est, sigma_est = fit_pareto(np.exp(tail_wts), loc=cutoff_value)
+
+    # Regularization from Appendix C of paper
+    k_est = (tail_sz * k_est + 5) / (tail_sz + 10)
+
+    # Only smooth if long-tailed.
+    if k_est >= k_min:
+        # compute ordered statistic for the fit
+        sti = np.arange(0.5, tail_sz)
+        sti /= tail_sz
+
+        # Smoothing formula (inverse-cdf of gen. pareto)
+        qq = scipy.stats.genpareto.ppf(
+            sti,
+            c=k_est,
+            scale=sigma_est
+        ) + cutoff_value
+        np.log(qq, out=qq)
+
+        # place the smoothed tail into the output array
+        tail_ordering = np.argsort(tail_wts)
+        wts[tailinds[tail_ordering]] = qq
+
+        # truncate smoothed values to the largest raw (log-)weight 0
+        wts[wts > 0] = 0
+
+    # renormalize weights
+    wts -= scipy.special.logsumexp(wts)
+    return wts, k_est