fixed sign mismatch bug in solver_dirichlet.py.

chronostrain/algs/inference/bbvi/posteriors/dirichlet.py
@@ -70,11 +70,10 @@ class ReparametrizedDirichletPosterior(AbstractReparametrizedPosterior):
             sample_shape=(self.num_times, num_samples, self.num_strains)
         )
         mean, scaling = self.gaussian_approximation()
-        s = torch.softmax(
+        return torch.softmax(
             torch.unsqueeze(mean, 1) + torch.unsqueeze(scaling, 1) * std_gaussian_samples,
             dim=2
         )
-        return s
 
     def sample(self, num_samples: int = 1) -> torch.Tensor:
         return self.reparametrized_sample(num_samples=num_samples).detach()

chronostrain/algs/inference/bbvi/solver_dirichlet.py
@@ -86,7 +86,7 @@ class ADVIDirichletSolver(AbstractADVISolver):
 
     def model_ll_with_grad(self, dirichlet_samples: torch.Tensor) -> torch.Tensor:
         log_y = torch.log(dirichlet_samples)
-        x = log_y[:, :, :-1] + log_y[:, :, -1].unsqueeze(2)
+        x = log_y[:, :, :-1] - log_y[:, :, -1].unsqueeze(2)
         return self.model.log_likelihood_x(X=x)
 
     def model_ll(self, dirichlet_samples: torch.Tensor) -> torch.Tensor: