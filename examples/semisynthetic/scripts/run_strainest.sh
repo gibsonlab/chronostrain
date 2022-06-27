b3ba21ba1a1c293d737e8fd339645f6a9a55d682
Younhun Kim <younhun@mit.edu>
2022-06-27 11:42:11 -0400

    added strainest mode selection in script. (sensitive vs default)

--- examples/semisynthetic/scripts/run_strainest.sh
+++ examples/semisynthetic/scripts/run_strainest.sh
@@ -11,6 +11,7 @@ check_program strainest
 n_reads=$1
 trial=$2
 time_point=$3
+sensitivity=$4
 
 if [ -z "$n_reads" ]
 then
@@ -28,6 +29,11 @@ if [ -z "$time_point" ]
 then
 	echo "var \"time_point\" is empty"
 	exit 1
+fi
+
+if [ "$sensitivity" != "sensitive" ] || [ "$sensitivity" != "default" ]
+then
+	echo "Invalid value for parameter 'sensitivity'."
 fi
 
 trial_dir=$(get_trial_dir $n_reads $trial)
@@ -64,14 +70,22 @@ samtools sort ${bam_file} -o ${sorted_bam_file}
 samtools index ${sorted_bam_file}
 
 # Run StrainEst
-echo "[*] Running StrainEst..."
-strainest est \
-${STRAINEST_DB_DIR}/snvs_clust.txt \
-${sorted_bam_file} \
-./ \
--t 1 \
--p 0 \
--a 2
+echo "[*] Running StrainEst... ($sensitivity)"
+if [ "$sensitivity" == "sensitive" ]; then
+	strainest est \
+	${STRAINEST_DB_DIR}/snvs_clust.txt \
+	${sorted_bam_file} \
+	./ \
+	-t 1 \
+	-p 0 \
+	-a
+else
+	strainest est \
+	${STRAINEST_DB_DIR}/snvs_clust.txt \
+	${sorted_bam_file} \
+	./ \
+	-t 1
+fi
 
 # ====== Record runtime
 end_time=$(date +%s%N)
@@ -86,17 +100,17 @@ rm ${bam_file}
 rm ${sorted_bam_file}
 
 if [ -f abund.txt ]; then
-	mv abund.txt abund_${time_point}.txt
+	mv abund.txt abund_${time_point}.${sensitivity}.txt
 fi
 
 if [ -f info.txt ]; then
-	mv info.txt info_${time_point}.txt
+	mv info.txt info_${time_point}.${sensitivity}.txt
 fi
 
 if [ -f counts.txt ]; then
-	mv counts.txt counts_${time_point}.txt
+	mv counts.txt counts_${time_point}.${sensitivity}.txt
 fi
 
 if [ -f max_ident.txt ]; then
-	mv max_ident.txt max_ident_${time_point}.txt
+	mv max_ident.txt max_ident_${time_point}.${sensitivity}.txt
 fi
