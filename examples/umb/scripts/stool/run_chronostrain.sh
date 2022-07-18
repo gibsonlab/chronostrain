16d4542ab7a0782e6c39adf554213de1eb2631cd
Youn <younhun@mit.edu>
2022-07-18 10:06:25 -0400

    Updated umb/stool runs with control group for loop.

--- examples/umb/scripts/stool/run_chronostrain.sh
+++ examples/umb/scripts/stool/run_chronostrain.sh
@@ -7,7 +7,8 @@ SEED=31415
 # =========== Run chronostrain. ==================
 echo "Running inference."
 
-for umb_id in UMB05 UMB08 UMB11 UMB12 UMB15 UMB18 UMB20 UMB23 UMB24
+#for umb_id in UMB05 UMB08 UMB11 UMB12 UMB15 UMB18 UMB20 UMB23 UMB24
+for umb_id in UMB01 UMB02 UMB03 UMB04 UMB06 UMB07 UMB09 UMB10 UMB13 UMB14 UMB16 UMB17 UMB19 UMB21 UMB22 UMB25 UMB26 UMB27 UMB28 UMB29 UMB30 UMB31
 do
     echo "Running inference on ${umb_id}."
     export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/chronostrain_${umb_id}.log"
