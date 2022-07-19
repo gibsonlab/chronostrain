f372d8137b8b619d902c96c3dd99fb8c01b0906d
Younhun Kim <younhun@mit.edu>
2022-07-19 11:41:32 -0400

    Added sensitivity specification to strainest (synthetic).

--- examples/synthetic/scripts/read_depths/run_strainest_all.sh
+++ examples/synthetic/scripts/read_depths/run_strainest_all.sh
@@ -2,7 +2,7 @@
 set -e
 source settings.sh
 
-
+sensitivity=$1
 mkdir -p ${STRAINEST_DB_DIR}
 cd ${STRAINEST_DB_DIR}
 cp ${CHRONOSTRAIN_DB_DIR}/assemblies/CP009273.1_Original/CP009273.1_Original.fasta CP009273.1_Original.fasta
@@ -17,10 +17,11 @@ for n_reads in 10000 50000 100000 500000 1000000
 do
 	for (( trial = 1; trial < ${N_TRIALS}+1; trial++ ));
 	do
-		bash run_strainest.sh $n_reads $trial 0
-		bash run_strainest.sh $n_reads $trial 1
-		bash run_strainest.sh $n_reads $trial 2
-		bash run_strainest.sh $n_reads $trial 3
-		bash run_strainest.sh $n_reads $trial 4
+		bash run_strainest.sh $n_reads $trial 0 $sensitivity &
+		bash run_strainest.sh $n_reads $trial 1 $sensitivity &
+		bash run_strainest.sh $n_reads $trial 2 $sensitivity &
+		bash run_strainest.sh $n_reads $trial 3 $sensitivity &
+		bash run_strainest.sh $n_reads $trial 4 $sensitivity &
+		wait
 	done
 done
