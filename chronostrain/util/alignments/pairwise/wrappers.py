72fffe769127f06ea7008ba51ac3dc57c60ae934
Youn <younhun@mit.edu>
2022-07-27 15:42:45 -0400

    Added option to specify aligner for filtering.

--- chronostrain/util/alignments/pairwise/wrappers.py
+++ chronostrain/util/alignments/pairwise/wrappers.py
@@ -194,8 +194,8 @@ class BowtieAligner(AbstractPairwiseAligner):
             ))
 
     def align(self, query_path: Path, output_path: Path, read_type: ReadType):
-        return self.align_end_to_end(query_path, output_path)
-        # return self.align_local(query_path, output_path)
+        # return self.align_end_to_end(query_path, output_path)
+        return self.align_local(query_path, output_path)
 
     def align_end_to_end(self, query_path: Path, output_path: Path):
         bowtie2(
@@ -234,13 +234,13 @@ class BowtieAligner(AbstractPairwiseAligner):
             report_k_alignments=self.num_report_alignments,
             report_all_alignments=self.report_all_alignments,
             num_threads=self.num_threads,
-            aln_seed_num_mismatches=0,
-            aln_seed_len=self.seed_length,  # -L 17
+            aln_seed_num_mismatches=0,  # -N
+            aln_seed_len=self.seed_length,  # -L
             aln_seed_interval_fn=bt2_func_constant(7),
             aln_gbar=1,
             effort_seed_ext_failures=self.seed_extend_failures,  # -D
             local=True,
-            effort_num_reseeds=self.num_reseeds,  # -R 3
+            effort_num_reseeds=self.num_reseeds,  # -R
             score_min_fn=self.score_min_fn,
             score_match_bonus=self.score_match_bonus,
             score_mismatch_penalty=self.score_mismatch_penalty,
