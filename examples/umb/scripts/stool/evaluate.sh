33e28c6768544e4ad7896644ff78a3f4dba570ae
Youn <younhun@mit.edu>
2022-07-29 15:16:06 -0400

    Added progress print statements to evaluation.

--- examples/umb/scripts/stool/evaluate.sh
+++ examples/umb/scripts/stool/evaluate.sh
@@ -5,7 +5,8 @@ source settings.sh
 SEED=31415
 
 # =========== Run chronostrain. ==================
-echo "Running evaluation."
-
+echo "Running ChronoStrain evaluation."
 python ${BASE_DIR}/helpers/eval_chronostrain.py -d $CHRONOSTRAIN_OUTPUT_DIR -o $OUTPUT_DIR/chronostrain.csv
+
+echo "Running StrainGE evaluation."
 python ${BASE_DIR}/helpers/eval_strainge.py -d $STRAINGE_OUTPUT_DIR -o $OUTPUT_DIR/strainge.csv -m $SRA_CSV_PATH
