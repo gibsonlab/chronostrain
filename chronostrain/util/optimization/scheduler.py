4b058454e7d668f516214b798113b7b8f8c8cb10
Youn <younhun@mit.edu>
2022-08-02 11:15:40 -0400

    changed median across timepoints to max in evaluation.

--- chronostrain/util/optimization/scheduler.py
+++ chronostrain/util/optimization/scheduler.py
@@ -63,8 +63,10 @@ class ReduceLROnPlateauLast(object):
             self.cooldown_counter -= 1
         else:
             if self.is_better(current, self.prev):
+                print("IS BETTER")
                 self.bad_epochs.push(0)
             else:
+                print("IS WORSE")
                 self.bad_epochs.push(1)
 
         if self.bad_epochs.size == self.bad_epochs.capacity and self.bad_epochs.mean() >= self.patience_ratio:
