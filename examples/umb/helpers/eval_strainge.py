5a654247235fb0f5dd29f6a43e7577860af3c80e
Youn <younhun@mit.edu>
2022-08-02 10:47:06 -0400

    added strainGE normalization.

--- examples/umb/helpers/eval_strainge.py
+++ examples/umb/helpers/eval_strainge.py
@@ -151,6 +151,10 @@ def convert_to_numpy(timeseries_df: pd.DataFrame, patient: str, metadata: pd.Dat
         tidx = time_indexes[day]
         sidx = strain_indexes[strain]
         timeseries[tidx, sidx] = row['RelAbund']
+
+    # Normalize.
+    row_sums = timeseries.sum(axis=1)
+    timeseries[row_sums > 0] = timeseries[row_sums > 0] / row_sums[row_sums > 0]
     return timeseries, strains
 
 
