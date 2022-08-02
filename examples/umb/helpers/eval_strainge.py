8f8c136524e67b68d3211ddd7d45c208abb76271
Youn <younhun@mit.edu>
2022-08-02 09:18:12 -0400

    evaluate now also groups by clades.

--- examples/umb/helpers/eval_strainge.py
+++ examples/umb/helpers/eval_strainge.py
@@ -44,7 +44,7 @@ def fetch_strain_id(strain_name: str, ref_df: pd.DataFrame) -> str:
 
     strain_names_to_try = {strain_name, strain_name.replace("_", ".")}
     if strain_name.startswith("str"):
-        strain_names_to_try.add(strain_name.split("_")[1:])
+        strain_names_to_try.add("_".join(strain_name.split("_")[1:]))
 
     for s in strain_names_to_try:
         try:
