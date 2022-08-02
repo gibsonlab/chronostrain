951a14a83ad5e8b2056640419c6575986187f613
Youn <younhun@mit.edu>
2022-08-02 09:21:53 -0400

    evaluate now also groups by clades.

--- examples/umb/helpers/eval_strainge.py
+++ examples/umb/helpers/eval_strainge.py
@@ -196,8 +196,9 @@ def dominance_switch_ratio(abundance_est: np.ndarray) -> float:
         return np.sum(r == 0).item() == r.shape[0]
 
     for i in range(len(dom) - 1):
-        switched = (dom[i] != dom[i+1]) or row_is_zeros(abundance_est[i]) or row_is_zeros(abundance_est[i+1])
-        if switched:
+        if row_is_zeros(abundance_est[i]) and row_is_zeros(abundance_est[i + 1]):
+            pass
+        elif (dom[i] != dom[i+1]) or row_is_zeros(abundance_est[i]) or row_is_zeros(abundance_est[i+1]):
             num_switches += 1
     return num_switches / (len(dom) - 1)
 
