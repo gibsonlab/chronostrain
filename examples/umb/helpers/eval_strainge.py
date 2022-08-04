eca4deafa169fd8f0c860cba0a4e6e84da0f54a3
Youn <younhun@mit.edu>
2022-08-04 12:01:09 -0400

    eval_strainge now gracefully handles empty dataframes.

--- examples/umb/helpers/eval_strainge.py
+++ examples/umb/helpers/eval_strainge.py
@@ -215,7 +215,6 @@ def divide_into_timeseries(timeseries: np.ndarray, strain_ids: List[str], clades
         # Note: if "s" is not in "clades", then it might not be ecoli.
         matching_strains = [i for i, s in enumerate(strain_ids) if (s in clades and clades[s] == this_clade)]
         if len(matching_strains) == 0:
-            print(f"Phylogroup {this_clade} was empty.")
             yield this_clade, np.empty((timeseries.shape[0], 0))
         yield this_clade, timeseries[:, matching_strains]
 
