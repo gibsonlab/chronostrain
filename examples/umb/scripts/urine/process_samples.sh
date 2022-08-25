7558d35a839b7e92f29c9d8ee3b26b070f532dd9
Youn <younhun@mit.edu>
2022-08-25 16:29:22 -0400

    umb scripts: chronostrain now by default runs for all patients.

--- examples/umb/scripts/urine/process_samples.sh
+++ examples/umb/scripts/urine/process_samples.sh
@@ -53,11 +53,6 @@ mkdir -p "${SAMPLES_DIR}/kneaddata"
 			continue
 		fi
 
-		if [[ "${exp_group}" != "Test" ]]; then
-			echo "Skipping ${sample_name}. (is not test group)"
-			continue
-		fi
-
 		echo "[*] -=-=-=-=-=-=-=-= Handling ${sra_id} (${sample_name}). =-=-=-=-=-=-=-=-"
 
 		# Obtained fastq files.
