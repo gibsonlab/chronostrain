0a7f6ebe144011a0a9564a0bee66f12d62c63a66
Youn <younhun@mit.edu>
2022-08-25 18:16:38 -0400

    wip

--- examples/umb/scripts/urine/download_samples.sh
+++ examples/umb/scripts/urine/download_samples.sh
@@ -49,7 +49,8 @@ mkdir -p ${FASTERQ_TMP_DIR}
 		--progress \
 		--outdir $SAMPLES_DIR \
 		--skip-technical \
-		--print-read-nr \
+		--seq-defline $ac.$sn.$sr.$ri \
+		--defline-qual '+' \
 		--force \
 		-t ${FASTERQ_TMP_DIR} \
 		"${SRA_PREFETCH_DIR}/${sra_id}/${sra_id}.sra"
