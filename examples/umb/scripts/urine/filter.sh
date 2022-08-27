91ed638c2a0a809e8e8ea2bb3767897824331cfc
Youn <younhun@mit.edu>
2022-08-27 19:53:42 -0400

    fixed erorr in urine/filter.sh

--- examples/umb/scripts/urine/filter.sh
+++ examples/umb/scripts/urine/filter.sh
@@ -10,6 +10,10 @@ SEED=31415
 for umb_id in UMB01 UMB02 UMB03 UMB04 UMB05 UMB06 UMB07 UMB08 UMB09 UMB10 UMB11 UMB12 UMB13 UMB14 UMB15 UMB16 UMB17 UMB18 UMB19 UMB20 UMB21 UMB22 UMB23 UMB24 UMB25 UMB26 UMB27 UMB28 UMB29 UMB30 UMB31
 do
 	export CHRONOSTRAIN_LOG_FILEPATH="${LOGDIR}/filter_${umb_id}.log"
+	reads_file="${READS_DIR}/${umb_id}_inputs.csv"
+	if [ ! -f $reads_file ]; then
+		continue
+	fi
 
 	echo "Filtering reads for ${umb_id}"
 	chronostrain filter \
