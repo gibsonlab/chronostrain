11c15377002a9c6c1ecaf5d1215650300a2f3b66
Youn <younhun@mit.edu>
2022-08-28 08:52:01 -0400

    umb/phylogeny: added a feature to generate multiple different trees using different collections of genes.

--- examples/umb/scripts/phylogeny/create_tree.sh
+++ examples/umb/scripts/phylogeny/create_tree.sh
@@ -7,8 +7,10 @@ mkdir -p ${PHYLOGENY_OUTPUT_DIR}/tree
 
 
 for choice in "all" "metaphlan" "mlst" "clermont"; do
+	echo "[*] Starting pipeline for generating phylogenetic tree (marker choice: \"${choice}\")"
 	marker_multi_align=${MULTI_ALIGN_DIR}/${choice}_markers.fasta
 
+	echo "[*] Obtaining multiple alignments."
 	python ${BASE_DIR}/helpers/concatenated_multiple_alignments.py \
 			--raw_json ${CHRONOSTRAIN_DB_JSON_ALL} \
 			--align_path ${marker_multi_align} \
@@ -17,9 +19,13 @@ for choice in "all" "metaphlan" "mlst" "clermont"; do
 			--genes_fasta ${BASE_DIR}/files/clermont_genes.fasta \
 			--metaphlan_db ${METAPHLAN_PKL_PATH}
 
+	echo "[*] Running fasttree multiple alignments."
+
 	tree_output_dir=${PHYLOGENY_OUTPUT_DIR}/tree_${choice}
+	mkdir -p ${tree_output_dir}
 	fasttree -gtr -nt ${marker_multi_align} > ${tree_output_dir}/tree.nwk
 
+	echo "[*] Generating tree annotations."
 	python ${BASE_DIR}/scripts/phylogeny/tree_annotations.py \
 			-o ${tree_output_dir}/tree \
 			-i ${NCBI_REFSEQ_DIR}/index.tsv \
