8a25485f006f9c5ed51a438e9f78fc89ef38f08b
Youn <younhun@mit.edu>
2022-08-28 08:50:32 -0400

    umb/phylogeny: added a feature to generate multiple different trees using different collections of genes.

--- examples/umb/scripts/phylogeny/create_tree.sh
+++ examples/umb/scripts/phylogeny/create_tree.sh
@@ -17,10 +17,11 @@ for choice in "all" "metaphlan" "mlst" "clermont"; do
 			--genes_fasta ${BASE_DIR}/files/clermont_genes.fasta \
 			--metaphlan_db ${METAPHLAN_PKL_PATH}
 
-	fasttree -gtr -nt ${marker_multi_align} > ${PHYLOGENY_OUTPUT_DIR}/tree/tree.nwk
+	tree_output_dir=${PHYLOGENY_OUTPUT_DIR}/tree_${choice}
+	fasttree -gtr -nt ${marker_multi_align} > ${tree_output_dir}/tree.nwk
 
 	python ${BASE_DIR}/scripts/phylogeny/tree_annotations.py \
-			-o ${PHYLOGENY_OUTPUT_DIR}/tree \
+			-o ${tree_output_dir}/tree \
 			-i ${NCBI_REFSEQ_DIR}/index.tsv \
 			-p ${PHYLOGENY_OUTPUT_DIR}/ClermonTyping/umb_phylogroups_complete.txt
 done
