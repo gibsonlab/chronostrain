79cb3bdefa04fc47541cb74485cf0084304aa45c
Youn <younhun@mit.edu>
2022-08-28 23:32:25 -0400

    updated training animation GIF rendering.

--- chronostrain/visualizations/bbvi_visualizations.py
+++ chronostrain/visualizations/bbvi_visualizations.py
@@ -4,13 +4,11 @@ from typing import List, Optional, Union
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
 
 from chronostrain import logger
-from chronostrain.algs import AbstractPosterior, ADVIGaussianSolver
+from chronostrain.algs import AbstractPosterior
 from chronostrain.model import Population
 from chronostrain.model.generative import GenerativeModel
-from chronostrain.model.io import TimeSeriesReads
 from chronostrain.util import filesystem
 
 from .plot_abundances import plot_posterior_abundances
@@ -41,32 +39,6 @@ def plot_training_animation(
         elbo_history: List[float],
         backend_writer: str = 'imagemagick'
 ):
-    import matplotlib.animation
-
-    def _blit_draw(self, artists, bg_cache):
-        # Handles blitted drawing, which renders only the artists given instead
-        # of the entire figure.
-        updated_ax = []
-        for a in artists:
-            # If we haven't cached the background for this axes object, do
-            # so now. This might not always be reliable, but it's an attempt
-            # to automate the process.
-            if a.axes not in bg_cache:
-                # bg_cache[a.axes] = a.figure.canvas.copy_from_bbox(a.axes.bbox)
-                # change here
-                bg_cache[a.axes] = a.figure.canvas.copy_from_bbox(a.axes.figure.bbox)
-            a.axes.draw_artist(a)
-            updated_ax.append(a.axes)
-
-        # After rendering all the needed artists, blit each axes individually.
-        for ax in set(updated_ax):
-            # and here
-            # ax.figure.canvas.blit(ax.bbox)
-            ax.figure.canvas.blit(ax.figure.bbox)
-
-    # MONKEY PATCH!!
-    matplotlib.animation.Animation._blit_draw = _blit_draw
-
     fig, axes = plt.subplots(1, 2, figsize=(15, 10), dpi=100)
 
     # ========== First axis (samples)
