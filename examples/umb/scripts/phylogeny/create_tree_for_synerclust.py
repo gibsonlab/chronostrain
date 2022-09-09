0fa1f83cd0673b7893041febf9dbcfe818e1a476
Youn <younhun@mit.edu>
2022-09-08 22:24:15 -0400

    reimplemented create_tree_for_synerclust.py

--- examples/umb/scripts/phylogeny/create_tree_for_synerclust.py
+++ examples/umb/scripts/phylogeny/create_tree_for_synerclust.py
@@ -1,86 +1,13 @@
-# compatible with python 2
-
-import csv
 import argparse
 import itertools
+import json
+import subprocess
 from pathlib import Path
+from typing import Iterator, Tuple, Dict, List
+
+import pandas as pd
 from Bio import Phylo
 from Bio.Phylo.TreeConstruction import DistanceTreeConstructor, DistanceMatrix
-import pandas as pd
-
-
-class SpeciesNotIncluded(BaseException):
-    def __init__(self, name_str: str):
-        self.name_str = name_str
-
-
-def fetch_strain_id_from_straingst(strain_name, index_df):
-    fasta_path = Path("/mnt/e/strainge/strainge_db") / strain_name
-    gcf_id = '_'.join(fasta_path.resolve().stem.split('_')[:2])
-    hit = index_df.loc[index_df['Assembly'] == gcf_id, :].head(1)
-    if hit.shape[0] == 0:
-        raise ValueError(
-            "Couldn't find strain from StrainGST identifier `{}`.".format(strain_name)
-        )
-
-    if hit['Species'].item().lower() != 'coli':
-        raise SpeciesNotIncluded(strain_name)
-
-    return hit['Accession'].item(), hit['Strain'].item()
-
-
-def parse_distances(similarities_path, index_df):
-    print("Parsing distances from {}".format(similarities_path))
-    names = set()
-    dists = dict()
-    names_to_ids = dict()
-
-    def fetch_id(strain_name):
-        if strain_name not in names_to_ids:
-            acc, _ = fetch_strain_id_from_straingst(strain_name, index_df)
-            names_to_ids[strain_name] = acc
-        return names_to_ids[strain_name]
-
-    names_to_skip = set()
-
-    with open(similarities_path, "rt") as f:
-        reader = csv.DictReader(f, delimiter='\t')
-        for row in reader:
-            u_name = row['kmerset1']
-            v_name = row['kmerset2']
-            jaccard_dist = 1 - float(row['jaccard'])
-
-            if u_name in names_to_skip:
-                continue
-            if v_name in names_to_skip:
-                continue
-
-            try:
-                u_id = fetch_id(u_name)
-                v_id = fetch_id(v_name)
-                names.add(u_id)
-                names.add(v_id)
-
-                if u_id < v_id:
-                    dists[(u_id, v_id)] = jaccard_dist
-                else:
-                    dists[(v_id, u_id)] = jaccard_dist
-            except SpeciesNotIncluded as e:
-                names_to_skip.add(e.name_str)
-                continue
-
-    print("found {} records.".format(len(names)))
-    names = sorted(names)
-    matrix = [
-        [0.] * (u_idx + 1)
-        for u_idx, _ in enumerate(names)
-    ]
-    for (u_idx, u), (v_idx, v) in itertools.combinations(enumerate(names), r=2):
-        if u_idx > v_idx:
-            matrix[u_idx][v_idx] = dists[(v, u)]
-        else:
-            matrix[v_idx][u_idx] = dists[(u, v)]
-    return DistanceMatrix(names, matrix)
 
 
 def create_tree(d):
@@ -91,25 +18,164 @@ def create_tree(d):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--sim_tsv_path', type=str, required=True)
-    parser.add_argument('-o', '--output_path', type=str, required=True)
-    parser.add_argument('-f', '--tree_format', type=str, required=False, default='newick')
+    parser.add_argument('-s', '--strainge_strains', type=str, required=True)
+    parser.add_argument('-sdb', '--strainge_db_dir', type=str, required=True)
+    parser.add_argument('-j', '--chronostrain_json', type=str, required=True)
     parser.add_argument('-i', '--refseq_index', type=str, required=True)
+    parser.add_argument('-o', '--output_dir', type=str, required=True)
+    parser.add_argument('-f', '--tree_format', type=str, required=False, default='newick')
     return parser.parse_args()
+
+
+def retrieve_straingst_strains(strain_list_path: Path, strainge_db_dir: Path, index_df: pd.DataFrame) -> Iterator[Tuple[str, Path]]:
+    print(f"Parsing IDs from strainGE database strains {strain_list_path}.")
+    with open(strain_list_path, 'rt') as f:
+        for line in f:
+            relative_path = Path(line.strip())
+            if relative_path.suffix == ".hdf5":
+                relative_path = relative_path.with_suffix('')
+
+            fasta_name = relative_path.name
+            if not fasta_name.startswith("Esch_coli"):
+                continue
+
+            # Resolve symlinks to retrieve GCF id
+            full_fasta_path = (strainge_db_dir / fasta_name).resolve()
+            assert full_fasta_path.name.startswith("GCF")
+            gcf_id = 'GCF_{}'.format(full_fasta_path.name.split("_")[1])
+
+            # Use this project's refseq index.
+            result = index_df.loc[index_df['Assembly'] == gcf_id].head(1)
+            strain_id = result['Accession'].item()
+            seq_path = result['SeqPath'].item()
+            yield strain_id, seq_path
+
+
+def retrieve_chronostrain_strains(json_path: Path, index_df: pd.DataFrame) -> Iterator[Tuple[str, Path]]:
+    print(f"Parsing IDs from chronostrain database file {json_path}.")
+    with open(json_path, 'r') as f:
+        strain_entries = json.load(f)
+        for strain_entry in strain_entries:
+            strain_species = strain_entry['species']
+            if strain_species != 'coli':
+                continue
+
+            strain_id = strain_entry['id']
+
+            # Use this project's refseq index.
+            result = index_df.loc[index_df['Accession'] == strain_id].head(1)
+            seq_path = result['SeqPath'].item()
+            yield strain_id, seq_path
+
+
+def compute_distances(
+        strain_records: Iterator[Tuple[str, Path]],
+        work_dir: Path
+) -> Tuple[DistanceMatrix, List[str]]:
+    work_dir.mkdir(exist_ok=True, parents=True)
+
+    strain_sketches = {}
+    for strain_id, strain_path in strain_records:
+        if strain_id in strain_sketches:
+            continue
+
+        print(f"Sketching {strain_id}...")
+        out_prefix = work_dir / f'{strain_id}'
+        sketch_path = invoke_mash_sketch(strain_path, out_prefix)
+        strain_sketches[strain_id] = sketch_path
+
+    indices: Dict[str, int] = {strain_id: i for i, strain_id in enumerate(strain_sketches.keys())}
+    matrix = [
+        [0.] * k
+        for k in range(1, len(indices) + 1)
+    ]
+
+    for x, y in itertools.combinations(strain_sketches.keys(), r=2):
+        d = invoke_mash_dist(strain_sketches[x], strain_sketches[y])
+        x_idx = indices[x]
+        y_idx = indices[y]
+        if x_idx > y_idx:
+            matrix[x_idx][y_idx] = d
+        else:
+            matrix[y_idx][x_idx] = d
+
+    return DistanceMatrix(matrix), list(strain_sketches.keys())
+
+
+def invoke_mash_sketch(fasta_path: Path, out_prefix: Path) -> Path:
+    subprocess.run([
+        'mash', 'sketch',
+        str(fasta_path),
+        '-o', str(out_prefix)
+    ], capture_output=False)
+    return out_prefix.parent / f'{out_prefix.name}.msh'
+
+
+def invoke_mash_dist(sketch1: Path, sketch2: Path) -> float:
+    completed = subprocess.run(['mash', 'dist', str(sketch1), str(sketch2)], capture_output=True)
+    completed.check_returncode()
+
+    answer = completed.stdout.decode('utf-8')
+    ref_id, query_id, mash_dist, p_val, matching_hashes = answer.split('\t')
+    return float(mash_dist)
+
+
+def create_synerclust_input(target_path: Path, strain_ids: List[str], index_df: pd.DataFrame):
+    # Search for fasta path.
+    with open(target_path, "w") as out_f:
+        print("//", file=out_f)
+        for strain_id in strain_ids:
+            res = index_df.loc[index_df['Accession'] == strain_id, :]
+            if res.shape[0] > 1:
+                raise RuntimeError("Ambiguous accession -> gcf mapping.")
+
+            gcf = res.head(1)['Assembly'].item()
+            seq_path = Path(res.head(1)['SeqPath'].item())
+            gff_path = next(iter(seq_path.parent.glob(f'{gcf}_*genomic.chrom.gff')))
+
+        print(f"Genome\t{strain_id}", file=out_f)
+        print(f"Sequence\t{seq_path}", file=out_f)
+        print(f"Annotation\t{gff_path}", file=out_f)
+        print("//", file=out_f)
 
 
 def main():
     args = parse_args()
+    output_dir = Path(args.output_dir)
+    temp_dir = output_dir / '__dist_tmp'
     index_df = pd.read_csv(args.refseq_index, sep='\t')
-    tree = create_tree(parse_distances(args.sim_tsv_path, index_df))
+
+    straingst_strains = retrieve_straingst_strains(
+        args.strainge_strains,
+        args.strainge_db_dir,
+        index_df
+    )
+    chronostrain_strains = retrieve_chronostrain_strains(
+        args.chronostrain_json,
+        index_df
+    )
+    distances, strain_ids = compute_distances(
+        itertools.chain(straingst_strains, chronostrain_strains),
+        temp_dir
+    )
+
+    print("Constructing tree from distance matrix.")
+    tree = DistanceTreeConstructor().nj(distances)
 
     # erase internal node names. Necessary for SynerClust?
     for clade in tree.find_clades():
         clade.name = ""
 
-    Phylo.write([tree], str(args.output_path), args.tree_format)
-    print("Created tree {}".format(args.output_path))
+    # Save the tree.
+    tree_path = output_dir / "tree.nwk"
+    Phylo.write([tree], tree_path, args.tree_format)
+    print("Created tree {}".format(tree_path))
     print("To run SynerClust, the user might need to manually delete the root node distance (`:0.000`).")
+
+    # Generate synerclust input.
+    synerclust_input = output_dir / "synerclust_input.txt"
+    create_synerclust_input(synerclust_input, strain_ids, index_df)
+    print("Created synerclust input file {}".format(synerclust_input))
 
 
 if __name__ == "__main__":
--- examples/umb/scripts/phylogeny/run_synerclust.sh
+++ examples/umb/scripts/phylogeny/run_synerclust.sh
@@ -3,36 +3,27 @@ set -e
 source settings.sh
 
 synerclust_dir=/home/lactis/SynerClust/bin
-sim_file=/mnt/e/strainge/similarities.tsv
-
 out_dir=${PHYLOGENY_OUTPUT_DIR}/synerclust
 mkdir -p $out_dir
 
-tree_path=${out_dir}/newick_jaccard.nwk  # precomputed
 
 if [ ! -f "${tree_path}" ]; then
 	echo "[*] Creating tree for SynerClust."
 	python create_tree_for_synerclust.py \
+		-s /mnt/e/strainge/references_to_keep.txt \
+		-sdb /mnt/e/strainge/strainge_db \
+		-j /mnt/e/chronostrain/umb_database/database_pruned_resolved.json \
 		-i ${NCBI_REFSEQ_DIR}/index.tsv \
-		-o ${tree_path} \
+		-o ${out_dir} \
 		-f 'newick' \
-		-s ${sim_file}
-fi
-
-input_file=${out_dir}/input.txt
-if [ ! -f "${input_file}" ]; then
-	echo "[*] Generating input file for SynerClust."
-	python create_synerclust_input.py \
-		-i ${NCBI_REFSEQ_DIR}/index.tsv \
-		-o $input_file
 fi
 
 
 echo "[*] Running Synerclust (PATH: ${synerclust_dir})"
 python $synerclust_dir/synerclust.py \
-	-r ${out_dir}/input.txt \
 	-w ${out_dir} \
-	-t ${tree_path} \
+	-r ${out_dir}/synerclust_input.txt \
+	-t ${out_dir}/tree.nwk \
 	--run single
 
 echo "[*] Done."
