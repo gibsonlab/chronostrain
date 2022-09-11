9daef851a4a0eac731f208a2df83f13af392c931
Youn <younhun@mit.edu>
2022-09-10 21:06:41 -0400

    wip

--- chronostrain/algs/subroutines/alignments/cached_pairwise_alignment.py
+++ chronostrain/algs/subroutines/alignments/cached_pairwise_alignment.py
@@ -73,7 +73,7 @@ class CachedReadPairwiseAlignments(object):
                     int(-deletion_ll / np.log(2)),
                     int(-insertion_ll / np.log(2))
                 ),
-                clip_penalty=1,
+                clip_penalty=5,
                 score_threshold=50,
                 bwa_command='bwa'
             )
