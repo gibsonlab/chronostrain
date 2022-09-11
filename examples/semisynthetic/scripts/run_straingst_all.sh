262589e2ff54033394e7eebca7cafe1c5aad9c4e
Youn <younhun@mit.edu>
2022-09-11 11:00:23 -0400

    wip

--- examples/semisynthetic/scripts/run_straingst_all.sh
+++ examples/semisynthetic/scripts/run_straingst_all.sh
@@ -8,7 +8,7 @@ echo "[*] Running StrainGST batch on mode \"${mode}\""
 
 cd ${BASE_DIR}/scripts
 for (( trial = 1; trial < ${N_TRIALS}+1; trial++ )); do
-	for n_reads in 10000 25000 50000 75000 100000; do
+	for n_reads in "${SYNTHETIC_COVERAGES[@]}"; do
 		bash run_straingst.sh $n_reads $trial 0 $mode
 		bash run_straingst.sh $n_reads $trial 1 $mode
 		bash run_straingst.sh $n_reads $trial 2 $mode
