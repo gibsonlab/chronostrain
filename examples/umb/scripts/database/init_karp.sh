bbd4084de0b2a54f9c2f843ff5f7b009b28b663d
Youn <younhun@mit.edu>
2022-10-06 11:04:10 -0400

    wip

--- examples/umb/scripts/database/init_karp.sh
+++ examples/umb/scripts/database/init_karp.sh
@@ -5,7 +5,7 @@ source settings.sh
 mkdir -p ${KARP_DB_DIR}
 python helpers/prepare_karp.py \
 -i ${NCBI_REFSEQ_DIR}/index.tsv \
--s ${STRAINGE_DB_DIR}/references_to_keep.tsv \
+-s ${STRAINGE_DB_DIR}/references_to_keep.txt \
 -m ${STRAINGE_DB_DIR}/strainge_db/references_meta.tsv \
 -o ${KARP_DB_DIR}
 
