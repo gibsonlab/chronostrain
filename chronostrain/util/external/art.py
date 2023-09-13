b2fe4817ade861f0dba211d1031e826f8f318459
Younhun Kim <youn.kim.3181@gmail.com>
2023-09-13 19:49:32 -0400

    added -ss option support for art_illumina.

--- chronostrain/util/external/art.py
+++ chronostrain/util/external/art.py
@@ -7,14 +7,15 @@ def art_illumina(reference_path: Path,
                  num_reads: int,
                  output_dir: Path,
                  output_prefix: str,
-                 profile_first: Path,
-                 profile_second: Path,
                  read_length: int,
                  seed: int,
                  paired_end_frag_mean_len: int = 10000,
                  paired_end_frag_stdev_len: int = 200,
                  output_sam: bool = False,
                  output_aln: bool = True,
+                 profile_first: Optional[Path] = None,
+                 profile_second: Optional[Path] = None,
+                 sequencing_sys: Optional[str] = None,
                  quality_shift: Optional[int] = None,
                  quality_shift_2: Optional[int] = None,
                  stdout_path: Optional[Path] = None,
@@ -41,8 +42,6 @@ def art_illumina(reference_path: Path,
     """
 
     cmd_args = [
-        '--qprof1', str(profile_first),
-        '--qprof2', str(profile_second),
         '-i', reference_path,
         '-l', read_length,
         '-c', num_reads,
@@ -53,6 +52,12 @@ def art_illumina(reference_path: Path,
         '-rs', str(seed)
     ]
 
+    if profile_first is not None:
+        cmd_args += ['--qprof1', str(profile_first)]
+    if profile_second is not None:
+        cmd_args += ['--qprof2', str(profile_second)]
+    if sequencing_sys is not None:
+        cmd_args += ['-ss', sequencing_sys]
     if output_sam:
         cmd_args.append('-sam')
 
