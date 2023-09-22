b2b3e46e6353738ad06c74f682c1eb66eb56238d
Younhun Kim <youn.kim.3181@gmail.com>
2023-09-22 10:47:05 -0400

    bugfix

--- examples/semisynthetic/scripts/strainge/run_straingst_all.sh
+++ examples/semisynthetic/scripts/strainge/run_straingst_all.sh
@@ -3,15 +3,16 @@ set -e
 source settings.sh
 
 
-cd ${BASE_DIR}/scripts
-for (( replicate = 1; replicate < ${N_GENOME_REPLICATES}+1; replicate++ )); do
-  for (( trial = 1; trial < ${N_TRIALS}+1; trial++ )); do
+for (( trial = 1; trial < ${N_TRIALS}+1; trial++ )); do
+  for (( replicate = 1; replicate < ${N_GENOME_REPLICATES}+1; replicate++ )); do
     for n_reads in "${SYNTHETIC_COVERAGES[@]}"; do
-      bash strainge/run_straingst.sh $replicate $n_reads $trial 0
-      bash strainge/run_straingst.sh $replicate $n_reads $trial 1
-      bash strainge/run_straingst.sh $replicate $n_reads $trial 2
-      bash strainge/run_straingst.sh $replicate $n_reads $trial 3
-      bash strainge/run_straingst.sh $replicate $n_reads $trial 4
+      for mutation_ratio in "${MUTATION_RATIOS[@]}"; do
+        bash strainge/run_straingst.sh $replicate $n_reads $trial 0
+        bash strainge/run_straingst.sh $replicate $n_reads $trial 1
+        bash strainge/run_straingst.sh $replicate $n_reads $trial 2
+        bash strainge/run_straingst.sh $replicate $n_reads $trial 3
+        bash strainge/run_straingst.sh $replicate $n_reads $trial 4
+      done
     done
 	done
 done
