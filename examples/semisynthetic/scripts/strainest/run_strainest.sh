1cf8ebfd7af93451541d16da6345310f71cc7560
Younhun Kim <youn.kim.3181@gmail.com>
2023-09-27 17:12:42 -0400

    updated strainest scripts.

--- examples/semisynthetic/scripts/strainest/run_strainest.sh
+++ examples/semisynthetic/scripts/strainest/run_strainest.sh
@@ -84,17 +84,17 @@ rm ${bam_file}
 rm ${sorted_bam_file}
 
 if [ -f abund.txt ]; then
-	mv abund.txt abund_${time_point}.${sensitivity}.txt
+	mv abund.txt abund_${time_point}.txt
 fi
 
 if [ -f info.txt ]; then
-	mv info.txt info_${time_point}.${sensitivity}.txt
+	mv info.txt info_${time_point}.txt
 fi
 
 if [ -f counts.txt ]; then
-	mv counts.txt counts_${time_point}.${sensitivity}.txt
+	mv counts.txt counts_${time_point}.txt
 fi
 
 if [ -f max_ident.txt ]; then
-	mv max_ident.txt max_ident_${time_point}.${sensitivity}.txt
+	mv max_ident.txt max_ident_${time_point}.txt
 fi
