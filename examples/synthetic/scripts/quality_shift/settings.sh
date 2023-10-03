59847a656fe16bec3b885cb9b4b5e3d59d3c5d1c
Younhun Kim <youn.kim.3181@gmail.com>
2023-10-03 11:31:10 -0400

    updated synthetic scripts.

--- examples/synthetic/scripts/quality_shift/settings.sh
+++ examples/synthetic/scripts/quality_shift/settings.sh
@@ -35,7 +35,6 @@ export CHRONOSTRAIN_READ_BATCH_SZ=2500
 export CHRONOSTRAIN_NUM_EPOCHS=150
 export CHRONOSTRAIN_DECAY_LR=0.25
 export CHRONOSTRAIN_LR=0.01
-export CHRONOSTRAIN_OUTPUT_FILENAME="abundances.out"
 
 # ========= (Example-specific configuration. No need to modify below this line, unless you really want it changed.)
 export CHRONOSTRAIN_DB_JSON="${BASE_DIR}/files/database.json"
--- examples/synthetic/scripts/read_depths/settings.sh
+++ examples/synthetic/scripts/read_depths/settings.sh
@@ -38,7 +38,6 @@ export CHRONOSTRAIN_DECAY_LR=0.25
 export CHRONOSTRAIN_LR=0.001
 export CHRONOSTRAIN_LR_PATIENCE=5
 export CHRONOSTRAIN_MIN_LR=1e-5
-export CHRONOSTRAIN_OUTPUT_FILENAME="abundances.out"
 
 # ========= (Example-specific configuration. No need to modify below this line, unless you really want it changed.)
 export CHRONOSTRAIN_DB_JSON="${BASE_DIR}/files/database.json"
