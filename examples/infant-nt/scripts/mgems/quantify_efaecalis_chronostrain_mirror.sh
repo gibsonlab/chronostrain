76470eac98a21d015ba3355ea138f20a554a1f44
Younhun Kim <youn.kim.3181@gmail.com>
2024-03-15 10:00:55 -0400

    infant-nt example: - separated run_pipeline_explicit.sh into two scripts (species binning + efaecalis quantification) - added efaecalis (chronostrain-mirrored) script.

---
+++ examples/infant-nt/scripts/mgems/quantify_efaecalis_chronostrain_mirror.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+set -e
+source settings.sh
+source mgems/settings.sh
+shopt -s nullglob
+
+# demix_check should point to demix_check.py (https://github.com/harry-thorpe/demix_check).
+# To pass this first check, create a bash executable called "demix_check" that invokes `python demix_check.py` and add to PATH environment var.
+require_program demix_check
+require_program themisto
+require_program mSWEEP
+require_program mGEMS
+require_program gzip
+
+# ============ Requires arguments:
+participant=$1
+sample_id=$2
+require_variable 'participant' $participant
+require_variable 'sample_id' $sample_id
+
+workdir=$(pwd)
+participant_dir=${DATA_DIR}/${participant}
+output_dir=${participant_dir}/mgems/${sample_id}
+species_breadcrumb=${output_dir}/mgems.species.DONE
+breadcrumb=${output_dir}/mgems.efaecalis_mirror.DONE
+if ! [ -f ${species_breadcrumb} ]; then
+  echo "[*] mGEMS species-binning for ${participant} [Sample ${sample_id}] not yet done."
+  exit 1
+fi
+if [ -f ${breadcrumb} ]; then
+  echo "[*] mGEMS efaecalis quantification (chronostrain mirror) for ${participant} [Sample ${sample_id}] already done."
+  exit 0
+fi
+
+
+# ====================================================== script begins here
+echo "[*] Running mGEMS efaecalis quantification for ${participant}, sample ${sample_id}"
+species_outdir=${output_dir}/species
+strain_fq_1=${species_outdir}/binned_reads/Enterococcus_faecalis_1.fastq.gz
+strain_fq_2=${species_outdir}/binned_reads/Enterococcus_faecalis_2.fastq.gz
+
+strain_outdir=${output_dir}/Efaecalis_chrono_mirror
+strain_aln_1=${strain_outdir}/ali_1.aln
+strain_aln_2=${strain_outdir}/ali_2.aln
+mkdir -p ${strain_outdir}
+
+cd ${EFAECALIS_CHRONO_MIRROR_REF_DIR}
+echo "[**] Aligning fwd+rev reads"
+aln_and_compress ${strain_fq_1} ${strain_fq_2} ${strain_aln_1} ${strain_aln_2} ${EFAECALIS_CHRONO_MIRROR_REF_INDEX} ${EFAECALIS_CHRONO_N_COLORS} ${strain_outdir}/tmp
+
+echo "[**] Cleaning up alignment tmpdir."
+rm -rf ${strain_outdir}/tmp
+
+echo "[**] Running mSWEEP abundance estimation (min abundance = 0.0)."
+mSWEEP \
+  -t ${N_CORES} \
+  --themisto-1 ${strain_aln_1}  \
+  --themisto-2 ${strain_aln_2}  \
+  -o ${strain_outdir}/msweep \
+  -i ${EFAECALIS_CHRONO_MIRROR_CLUSTER} \
+  --bin-reads \
+  --min-abundance 0.0 \
+  --verbose
+
+
+echo "[**] Extracting reads (for demix_check)."
+mkdir -p ${strain_outdir}/binned_reads
+for bin_file in ${strain_outdir}/*.bin; do
+  mv ${bin_file} ${strain_outdir}/binned_reads
+done
+
+for bin_file in ${strain_outdir}/binned_reads/*.bin; do
+  mGEMS extract --bins ${bin_file} -r ${strain_fq_1},${strain_fq_2} -o ${strain_outdir}/binned_reads
+done
+
+echo "[**] Compressing extracted reads."
+for f in ${strain_outdir}/binned_reads/*.fastq; do gzip "$f"; done
+
+echo "[**] Running demix_check."
+demix_check --mode_check \
+  --binned_reads_dir ${strain_outdir}/binned_reads \
+  --msweep_abun ${strain_outdir}/msweep_abundances.txt \
+  --out_dir ${strain_outdir}/demix_check \
+  --ref . \
+  --min_abun 0.0
+
+cd ${workdir}
+touch "${breadcrumb}"
--- examples/infant-nt/scripts/mgems/run_pipeline_infant.sh
+++ examples/infant-nt/scripts/mgems/run_pipeline_infant.sh
@@ -3,6 +3,13 @@ set -e
 source settings.sh
 source mgems/settings.sh
 
+# demix_check should point to demix_check.py (https://github.com/harry-thorpe/demix_check).
+# To pass this first check, create a bash executable called "demix_check" that invokes `python demix_check.py` and add to PATH environment var.
+require_program demix_check
+require_program themisto
+require_program mSWEEP
+require_program mGEMS
+require_program gzip
 
 # ============ Requires arguments:
 participant=$1
@@ -22,6 +29,7 @@ mkdir -p "${output_dir}"
 while IFS=$'\t' read part_id time_point sample_id read1_raw_fq read2_raw_fq
 do
     if [ "${part_id}" == "Participant" ]; then continue; fi
-    bash mgems/run_pipeline_explicit.sh "${part_id}" "${sample_id}"
+    bash mgems/bin_sample_species.sh "${part_id}" "${sample_id}"
+    bash mgems/quantify_efaecalis.sh "${part_id}" "${sample_id}"
 done < "${participant_dir}/dataset.tsv"
 touch "${breadcrumb}"
