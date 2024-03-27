bbb1a403e9f6384657a7b12b369a15bc3fd688e0
Younhun Kim <youn.kim.3181@gmail.com>
2024-03-27 19:13:19 -0400

    changed organization of mutation scripts.

--- examples/infant-nt/scripts/mutations/run_chronostrain.mutation.sh
+++ examples/infant-nt/scripts/mutations/run_chronostrain.mutation.sh
@@ -17,7 +17,7 @@ cd ${BASE_DIR}
 
 
 export CHRONOSTRAIN_LOG_FILEPATH="${run_dir}/inference.log"
-export CHRONOSTRAIN_CACHE_DIR="${DATA_DIR}/.cache_mut"
+export CHRONOSTRAIN_CACHE_DIR="${DATA_DIR}/.cache_mut_${mutation_rate}"
 export CHRONOSTRAIN_DB_JSON=${DATA_DIR}/database/mutated_dbs/${mutation_rate}/chronostrain/efaecalis.json
 export CHRONOSTRAIN_CLUSTERS=${DATA_DIR}/database/mutated_dbs/${mutation_rate}/chronostrain/efaecalis.clusters.txt
 
